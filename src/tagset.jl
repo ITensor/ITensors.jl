a553d7be11805e0788be28fac27884d3a5c2e065
Katharine Hyatt <khyatt@flatironinstitute.org>
2019-01-22 16:53:34 -0500

    Add an in method to check for tags in tagsets

--- src/tagset.jl
+++ src/tagset.jl
@@ -2,13 +2,14 @@
 struct TagSet
     tags::Vector{String}
     TagSet(tags::String) = new(sort(split(tags,",")))
-    TagSet(ts::TagSet) = new(copy(ts.tags))
+    TagSet(tags::Vector{String}) = new(sort(tags))
 end
 
-copy(ts::TagSet) = TagSet(ts)
+copy(ts::TagSet) = TagSet(ts.tags)
 
 convert(::Type{TagSet},x::String) = TagSet(x)
 convert(::Type{TagSet},x::TagSet) = TagSet(x)
 
 ==(ts1::TagSet,ts2::TagSet) = (ts1.tags==ts2.tags)
 
+in(tag::String, ts::TagSet) == in(tag, ts.tags)
