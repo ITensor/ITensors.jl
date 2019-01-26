0aa3f09c52d1039c1d300299e22666d5c48aaa1a
Miles Stoudenmire <miles.stoudenmire@gmail.com>
2019-01-26 11:59:34 -0500

    Changed Dense storage to be initialized to zero

--- src/storage/dense.jl
+++ src/storage/dense.jl
@@ -2,7 +2,7 @@
 struct Dense{T} <: TensorStorage
   data::Vector{T}
   Dense{T}(data::Vector) where {T} = new{T}(data)
-  Dense{T}(size::Integer) where {T} = new{T}(Vector{T}(undef,size))
+  Dense{T}(size::Integer) where {T} = new{T}(zeros(size))
   Dense{T}(x::Number,size::Integer) where {T} = new{T}(fill(x,size))
   Dense{T}() where {T} = new{T}(Vector{T}())
 end
