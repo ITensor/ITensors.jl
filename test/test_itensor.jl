972a69be42e6fb790a0fa59ceb8fcbf5dc865bda
Miles Stoudenmire <miles.stoudenmire@gmail.com>
2019-01-27 15:35:09 -0800

    Updated ITensor unit tests and added new SVD truncation test

--- test/test_itensor.jl
+++ test/test_itensor.jl
@@ -5,7 +5,7 @@ using ITensors,
 
 digits(::Type{T},i,j,k) where {T} = T(i*10^2+j*10+k)
 
-@testset "ITensor, Dense{$T} storage" for T ∈ (Float64,ComplexF64)
+@testset "ITensor, Dense{$SType} storage" for SType ∈ (Float64,)#,ComplexF64)
   mi,mj,mk,ml,mα = 2,3,4,5,6,7
   i = Index(mi,"i")
   j = Index(mj,"j")
@@ -13,16 +13,16 @@ digits(::Type{T},i,j,k) where {T} = T(i*10^2+j*10+k)
   l = Index(ml,"l")
   α = Index(mα,"α") 
   @testset "Set and get values with IndexVals" begin
-    A = ITensor(T,i,j,k)
+    A = ITensor(SType,i,j,k)
     for ii ∈ 1:dim(i), jj ∈ 1:dim(j), kk ∈ 1:dim(k)
-      A[k(kk),j(jj),i(ii)] = digits(T,ii,jj,kk)
+      A[k(kk),j(jj),i(ii)] = digits(SType,ii,jj,kk)
     end
     for ii ∈ 1:dim(i), jj ∈ 1:dim(j), kk ∈ 1:dim(k)
-      @test A[j(jj),k(kk),i(ii)]==digits(T,ii,jj,kk)
+      @test A[j(jj),k(kk),i(ii)]==digits(SType,ii,jj,kk)
     end
   end
   @testset "Test permute(ITensor,Index...)" begin
-    A = randomITensor(T,i,k,j)
+    A = randomITensor(SType,i,k,j)
     permA = permute(A,k,j,i)
     @test k==inds(permA)[1]
     @test j==inds(permA)[2]
@@ -32,28 +32,28 @@ digits(::Type{T},i,j,k) where {T} = T(i*10^2+j*10+k)
     end
   end
   @testset "Set and get values with Ints" begin
-    A = ITensor(T,i,j,k)
+    A = ITensor(SType,i,j,k)
     A = permute(A,k,i,j)
     for ii ∈ 1:dim(i), jj ∈ 1:dim(j), kk ∈ 1:dim(k)
-      A[kk,ii,jj] = digits(T,ii,jj,kk)
+      A[kk,ii,jj] = digits(SType,ii,jj,kk)
     end
     A = permute(A,i,j,k)
     for ii ∈ 1:dim(i), jj ∈ 1:dim(j), kk ∈ 1:dim(k)
-      @test A[ii,jj,kk]==digits(T,ii,jj,kk)
+      @test A[ii,jj,kk]==digits(SType,ii,jj,kk)
     end
   end
   @testset "Test scalar(ITensor)" begin
-    x = T(34)
+    x = SType(34)
     A = ITensor(x)
     @test x==scalar(A)
   end
   @testset "Test norm(ITensor)" begin
-    A = randomITensor(T,i,j,k)
+    A = randomITensor(SType,i,j,k)
     @test norm(A)≈sqrt(scalar(dag(A)*A))
   end
   @testset "Test add ITensors" begin
-    A = randomITensor(T,i,j,k)
-    B = randomITensor(T,k,i,j)
+    A = randomITensor(SType,i,j,k)
+    B = randomITensor(SType,k,i,j)
     C = A+B
     for ii ∈ 1:dim(i), jj ∈ 1:dim(j), kk ∈ 1:dim(k)
       @test C[i(ii),j(jj),k(kk)]==A[j(jj),i(ii),k(kk)]+B[i(ii),k(kk),j(jj)]
@@ -62,29 +62,44 @@ digits(::Type{T},i,j,k) where {T} = T(i*10^2+j*10+k)
   end
 
   @testset "Test factorizations of an ITensor" begin
-    A = randomITensor(T,i,j,k,l)
+
+    A = randomITensor(SType,i,j,k,l)
+
     @testset "Test SVD of an ITensor" begin
       U,S,V = svd(A,j,l)
-      u = commonindex(U,S)
-      v = commonindex(S,V)
+      u = commonIndex(U,S)
+      v = commonIndex(S,V)
       @test A≈U*S*V
-      @test U*dag(prime(U,u))≈δ(T,u,prime(u)) atol=1e-14
-      @test V*dag(prime(V,v))≈δ(T,v,prime(v)) atol=1e-14
+      @test U*dag(prime(U,u))≈δ(SType,u,u') atol=1e-14
+      @test V*dag(prime(V,v))≈δ(SType,v,v') atol=1e-14
     end
+
+    @testset "Test SVD truncation" begin 
+        M = randn(4,4)
+        (U,s,V) = svd(M)
+        ii = Index(4)
+        jj = Index(4)
+        S = Diagonal(s)
+        T = ITensor(IndexSet(ii,jj),Dense{Float64}(vec(U*S*V')))
+        (U,S,V) = svd(T,ii;maxm=2)
+        @test norm(U*S*V-T)≈sqrt(s[3]^2+s[4]^2)
+    end 
+
     @testset "Test QR decomposition of an ITensor" begin
       Q,R = qr(A,i,l)
-      q = commonindex(Q,R)
+      q = commonIndex(Q,R)
       @test A≈Q*R
-      @test Q*dag(prime(Q,q))≈δ(T,q,prime(q)) atol=1e-14
+      @test Q*dag(prime(Q,q))≈δ(SType,q,q') atol=1e-14
     end
+
     @testset "Test polar decomposition of an ITensor" begin
       U,P = polar(A,k,l)
-      u = commonindex(U,P)
+      u = commonIndex(U,P)
       @test A≈U*P
       #Note: this is only satisfied when left dimensions 
       #are greater than right dimensions
-      @test U*dag(prime(U,u))≈δ(T,u,prime(u)) atol=1e-14
+      @test U*dag(prime(U,u))≈δ(SType,u,u') atol=1e-14
     end
+
   end # End ITensor factorization testset
 end # End Dense storage test
-
