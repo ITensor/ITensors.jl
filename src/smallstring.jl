c2e535cc00163957ba386a92b43d027ebdd8d14b
Miles Stoudenmire <miles.stoudenmire@gmail.com>
2019-02-05 21:26:05 -0800

    Implemented length, getindex, and show for SmallString

--- src/smallstring.jl
+++ src/smallstring.jl
@@ -6,9 +6,20 @@ end
 SmallString(s::String) = SmallString(unsafe_wrap(Vector{UInt8},s))
 String(ss::SmallString) = prod(Char.(ss.data))
 
+length(s::SmallString) = length(s.data)
+getindex(s::SmallString,n::Integer) = getindex(s.data,n)
+ 
 function cmp(a::SmallString, b::SmallString)
   return ccall(:memcmp, Int32, (Ptr{UInt8}, Ptr{UInt8}, UInt8),
                a.data, b.data, 0x08)
 end
 isless(a::SmallString,b::SmallString) = cmp(a, b) < 0
+
+function show(io::IO, s::SmallString)
+  print(io,'"')
+  for n=1:length(s)
+    print(io,convert(Char,s[n]))
+  end
+  print(io,'"')
+end
 
