927b18381f3510e67651e4595d23d85a5f03683d
Miles Stoudenmire <miles.stoudenmire@gmail.com>
2019-05-17 13:57:55 -0400

    Experimenting more with DMRG

--- src/mps/dmrg.jl
+++ src/mps/dmrg.jl
@@ -1,5 +1,41 @@
 using KrylovKit: Lanczos, eigsolve
 
+function pause() 
+  println("(paused)")
+  readline(stdin)
+end
+
+function iterEigSolve(PH,
+                      phi::ITensor;
+                      kwargs...)::Tuple{Number,ITensor}
+
+  #
+  # TODO: make version which takes krylovdim, maxiter
+  #       was giving energies which randomly go up!
+  #
+  #tol = get(kwargs,:tol,10*eps(Float64))
+  #krylovdim::Int = get(kwargs,:krylovdim,2)
+  #maxiter::Int = get(kwargs,:maxiter,1)
+
+  #actualdim = 1
+  #for i in inds(phi)
+  #  plev(i) == 0 && (actualdim *= dim(i))
+  #end
+  #if krylovdim > actualdim
+  #  @printf "krylovdim=%d > actualdim=%d, resetting" krylovdim actualdim
+  #  krylovdim = actualdim
+  #end
+  #lczos = Lanczos(tol=tol,krylovdim=krylovdim,maxiter=maxiter)
+  #vals, vecs, info = eigsolve(PH,phi,1,:SR,lczos)
+
+  vals, vecs, info = eigsolve(PH,phi,1,:SR,ishermitian=true)
+
+  #@show info.normres[1]
+  #@show info.numops
+  #@show info.numiter
+  return vals[1],vecs[1]
+end
+
 function dmrg(H::MPO,
               psi0::MPS,
               sweeps::Sweeps;
@@ -14,25 +50,33 @@ function dmrg(H::MPO,
 
   for sw=1:nsweep(sweeps)
     for (b,ha) in sweepnext(N)
-      @printf "sw=%d ha=%d b=%d\n" sw ha b
+      #@printf "sw=%d ha=%d b=%d\n" sw ha b
 
       position!(PH,psi,b)
 
       phi = psi[b]*psi[b+1]
+      #@printf "initial phi norm = %.3f\n" norm(phi)
+      #@printf "initial energy = %.8f\n" scalar(phi*PH(phi))/norm(phi)^2
 
-      #phi,energy = davidson(PH,phi;kwargs...)
-      @show scalar(phi*phi)
-      Hphi = PH(phi)
-      @show inds(Hphi)
-      @show scalar(phi*Hphi)
+      energy,phi = iterEigSolve(PH,phi;kwargs...)
+      #@printf "unnorm energy=%.8f\n" scalar(phi*PH(phi))
+      phi /= norm(phi)
+      #@printf "check phi energy = %.8f\n" scalar(phi*PH(phi))/norm(phi)^2
 
-      lczos = Lanczos(krylovdim=2,maxiter=2,tol=10*eps(Float64))
-      vals, vecs, info = eigsolve(PH,phi,1,:SR,lczos)
-      @show vals
-      exit(0)
+      dir = ha==1 ? "Fromleft" : "Fromright"
+      replaceBond!(psi,b,phi,dir;
+                   maxdim=maxdim(sweeps,sw),
+                   mindim=mindim(sweeps,sw),
+                   cutoff=cutoff(sweeps,sw))
 
-      #dir = ha==1 ? "Fromleft" : "Fromright"
-      #replaceBond!(psi,b,phi,dir;kwargs...)
+      #nphi = psi[b]*psi[b+1]
+      #@printf "final MPS norm = %.3f\n" norm(nphi)
+      #@printf "final energy = %.8f\n" scalar(nphi*PH(nphi))/norm(nphi)^2
+      #@printf "dim=%d\n" dim(linkind(psi,b))
+
+      #pause()
+
+      @printf "sw=%d ha=%d b=%d energy=%.8f dim=%d\n" sw ha b energy dim(linkind(psi,b))
     end
   end
   return (energy,psi)
