ea1e33e12dca4e768a3bcdf84212aefad7a4e807
Miles Stoudenmire <miles.stoudenmire@gmail.com>
2019-07-13 22:47:28 -0400

    Moved development/heisenberg.jl code into test_autompo.jl

--- test/test_autompo.jl
+++ test/test_autompo.jl
@@ -1,7 +1,14 @@
 using ITensors,
-      Test
+      Test,
+      Random
 
-function makeRandMPS(sites::SiteSet,
+function setElt(iv::IndexVal)::ITensor
+  T = ITensor(ind(iv))
+  T[iv] = 1.0
+  return T
+end
+
+function makeRandomMPS(sites::SiteSet,
                      chi::Int=4)::MPS
   N = length(sites)
   v = Vector{ITensor}(undef, N)
@@ -45,19 +52,61 @@ function isingMPO(sites::SiteSet)::MPO
   return H
 end
 
+function heisenbergMPO(sites::SiteSet)::MPO
+  H = MPO(sites)
+  N = length(H)
+  link = fill(Index(),N+1)
+  for n=1:N+1
+    link[n] = Index(5,"Link,Heis,l=$(n-1)")
+  end
+  for n=1:N
+    s = sites[n]
+    ll = link[n]
+    rl = link[n+1]
+    H[n] = ITensor(ll,s,s',rl)
+    H[n] += setElt(ll[1])*setElt(rl[1])*op(sites,"Id",n)
+    H[n] += setElt(ll[5])*setElt(rl[5])*op(sites,"Id",n)
+    H[n] += setElt(ll[2])*setElt(rl[1])*op(sites,"S+",n)
+    H[n] += setElt(ll[3])*setElt(rl[1])*op(sites,"S-",n)
+    H[n] += setElt(ll[4])*setElt(rl[1])*op(sites,"Sz",n)
+    H[n] += setElt(ll[5])*setElt(rl[2])*op(sites,"S-",n)*0.5
+    H[n] += setElt(ll[5])*setElt(rl[3])*op(sites,"S+",n)*0.5
+    H[n] += setElt(ll[5])*setElt(rl[4])*op(sites,"Sz",n)
+  end
+  H[1] *= setElt(link[1][5])
+  H[N] *= setElt(link[N+1][1])
+  return H
+end
+
 @testset "AutoMPO" begin
 
-  N = 4
-  sites = SiteSet(N,2)
+  N = 10
 
   @testset "Ising" begin
+    sites = spinHalfSites(N)
     ampo = AutoMPO(sites)
     for j=1:N-1
       add!(ampo,"Sz",j,"Sz",j+1)
     end
     Ha = toMPO(ampo)
     He = isingMPO(sites)
-    psi = makeRandMPS(sites)
+    psi = makeRandomMPS(sites)
+    Oa = inner(psi,Ha,psi)
+    Oe = inner(psi,He,psi)
+    @test Oa ≈ Oe
+  end
+
+  @testset "Heisenberg" begin
+    sites = spinHalfSites(N)
+    ampo = AutoMPO(sites)
+    for j=1:N-1
+      add!(ampo,"Sz",j,"Sz",j+1)
+      add!(ampo,0.5,"S+",j,"S-",j+1)
+      add!(ampo,0.5,"S-",j,"S+",j+1)
+    end
+    Ha = toMPO(ampo)
+    He = heisenbergMPO(sites)
+    psi = makeRandomMPS(sites)
     Oa = inner(psi,Ha,psi)
     Oe = inner(psi,He,psi)
     @test Oa ≈ Oe
