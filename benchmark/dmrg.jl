64d4496ef3ece604127d104a6a552477823b3704
Miles Stoudenmire <miles.stoudenmire@gmail.com>
2019-07-13 22:47:47 -0400

    Updated DMRG benchmark to use AutoMPO

--- benchmark/dmrg.jl
+++ benchmark/dmrg.jl
@@ -4,11 +4,21 @@ using Printf
 function main()
   N = 100
   sites = spinOneSites(N)
-  H = Heisenberg(sites)
+
+  ampo = AutoMPO(sites)
+  for j=1:N-1
+    add!(ampo,"Sz",j,"Sz",j+1)
+    add!(ampo,0.5,"S+",j,"S-",j+1)
+    add!(ampo,0.5,"S-",j,"S+",j+1)
+  end
+  H = toMPO(ampo)
+
   psi = randomMPS(sites)
+
   sw = Sweeps(5)
   maxdim!(sw,10,20,100,100,200)
   cutoff!(sw,1E-11)
+
   println("Starting DMRG")
   energy,psi = @time dmrg(H,psi,sw,maxiter=2)
   @printf "Final energy = %.12f\n" energy
