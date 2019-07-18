f29216833c81a0ae687dab7a7748c9cbab40c610
Miles Stoudenmire <miles.stoudenmire@gmail.com>
2019-07-18 13:13:59 -0400

    Added tests for orthog

--- test/test_svd.jl
+++ test/test_svd.jl
@@ -4,6 +4,19 @@ using ITensors,
 
 @testset "SVD Algorithms" begin
 
+  @testset "Test Orthog" begin
+    M1 = [1.0 2.0 5.0 4.0;
+         1.0 1.0 1.0 1.0;
+         0.0 0.5 0.5 1.0;
+         0.0 1.0 1.0 2.0]
+    orthog!(M1)
+    @test norm(M1'*M1-Diagonal(ones(size(M1,1)))) < 1E-13
+
+    M2 = rand(10,10)
+    orthog!(M2)
+    @test norm(M2'*M2-Diagonal(ones(size(M2,1)))) < 1E-13
+  end
+
   @testset "Matrix With Zero Sing Val" begin
     M = [1.0 2.0 5.0 4.0;
          1.0 1.0 1.0 1.0;
