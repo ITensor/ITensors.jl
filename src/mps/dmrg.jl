63849e67cb85744c79dd60b0f39e6387ad3acd68
Miles Stoudenmire <miles.stoudenmire@gmail.com>
2019-07-20 12:41:00 -0400

    Cleaned up mps/dmrg.jl and add a doc string

--- src/mps/dmrg.jl
+++ src/mps/dmrg.jl
@@ -1,43 +1,10 @@
 export dmrg
 
-#using KrylovKit: Lanczos, eigsolve
-
-
-function iterEigSolve(PH,
-                      phi::ITensor;
-                      kwargs...)::Tuple{Number,ITensor}
-
-  #
-  # TODO: make version which takes krylovdim, maxiter
-  #       was giving energies which randomly go up!
-  #
-  #tol = get(kwargs,:tol,10*eps(Float64))
-  #krylovdim::Int = get(kwargs,:krylovdim,2)
-  #maxiter::Int = get(kwargs,:maxiter,1)
-
-  #actualdim = 1
-  #for i in inds(phi)
-  #  plev(i) == 0 && (actualdim *= dim(i))
-  #end
-  #if krylovdim > actualdim
-  #  @printf "krylovdim=%d > actualdim=%d, resetting" krylovdim actualdim
-  #  krylovdim = actualdim
-  #end
-  #lczos = Lanczos(tol=tol,krylovdim=krylovdim,maxiter=maxiter)
-  #vals, vecs, info = eigsolve(PH,phi,1,:SR,lczos)
-
-  vals, vecs, info = eigsolve(PH,phi,1,:SR,ishermitian=true)
-
-  #@show info.normres[1]
-  #@show info.numops
-  #@show info.numiter
-  return vals[1],vecs[1]
-end
-
 function dmrg(H::MPO,
               psi0::MPS,
               sweeps::Sweeps;
               kwargs...)::Tuple{Float64,MPS}
+
   which_factorization::String = get(kwargs,:which_factorization,"automatic")
 
   psi = copy(psi0)
@@ -50,31 +17,18 @@ function dmrg(H::MPO,
   for sw=1:nsweep(sweeps)
     sw_time = @elapsed begin
 
-    sweep_t = 0.0
-    eigen_t = 0.0
-    svd_t = 0.0
-
-    reset!(timer)
+    @debug begin
+      reset!(timer)
+      println("\n\n~~~ This is sweep $sw ~~~\n\n")
+    end
 
     for (b,ha) in sweepnext(N)
-      #@printf "sw=%d ha=%d b=%d\n" sw ha b
-
       position!(PH,psi,b)
 
       phi = psi[b]*psi[b+1]
-      #@printf "initial phi norm = %.3f\n" norm(phi)
-      #@printf "initial energy = %.8f\n" scalar(phi*PH(phi))/norm(phi)^2
 
-      eigen_t += @elapsed begin
-        energy,phi = davidson(PH,phi;kwargs...)
-      end
+      energy,phi = davidson(PH,phi;kwargs...)
 
-      #energy,phi = iterEigSolve(PH,phi;kwargs...)
-      #@printf "unnorm energy=%.8f\n" scalar(phi*PH(phi))
-      #phi /= norm(phi)
-      #@printf "check phi energy = %.8f\n" scalar(phi*PH(phi))/norm(phi)^2
-
-      svd_t += @elapsed begin
       dir = ha==1 ? "fromleft" : "fromright"
       replaceBond!(psi,b,phi;
                    maxdim=maxdim(sweeps,sw),
@@ -82,24 +36,27 @@ function dmrg(H::MPO,
                    cutoff=cutoff(sweeps,sw),
                    dir=dir,
                    which_factorization=which_factorization)
-      end
-
-      #nphi = psi[b]*psi[b+1]
-      #@printf "final MPS norm = %.3f\n" norm(nphi)
-      #@printf "final energy = %.8f\n" scalar(nphi*PH(nphi))/norm(nphi)^2
-      #@printf "dim=%d\n" dim(linkindex(psi,b))
-
-      #@printf "sw=%d ha=%d b=%d energy=%.8f dim=%d\n" sw ha b energy dim(linkindex(psi,b))
-      #pause()
     end
-  end
+    end
     @printf "After sweep %d energy=%.12f maxDim=%d time=%.3f\n" sw energy maxDim(psi) sw_time
-    @show eigen_t
-    @show svd_t
-    @printf "sw_time = %.12f (eigen_t+svd_t = %.12f)\n" sw_time eigen_t+svd_t
-    printTimes(timer)
-    println("\n")
+    @debug printTimes(timer)
   end
   return (energy,psi)
 end
 
+@doc """ 
+Use the density matrix renormalization group (DMRG) algorithm to
+optimize a matrix product state (MPS) to be the eigenvector
+of the Hermitian matrix product operator (MPO) H with minimal 
+eigenvalue.
+
+Inputs:
+* `H::MPO` - a Hermitian MPO
+* `psi0::MPS` - MPS used to initialize the optimization
+* `sweeps::Sweeps` - `Sweeps` object used to control the algorithm
+
+Returns:
+* `energy::Float64` - eigenvalue of the optimized MPS
+* `psi::MPS` - optimized MPS
+
+""" dmrg
