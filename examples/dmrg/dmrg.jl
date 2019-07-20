7f2e2d914b2eb7aa9c6e615f9b68fbcfc4d8e4f7
Miles Stoudenmire <miles.stoudenmire@gmail.com>
2019-07-20 11:52:20 -0400

    Updated examples/dmrg

--- examples/dmrg/dmrg.jl
+++ examples/dmrg/dmrg.jl
@@ -1,19 +1,26 @@
-using ITensors, Printf, Profile, ProfileView
+using ITensors
+using Printf
 
-N = 100
-sites = spinOneSites(N)
-H = Heisenberg(sites)
-psi0 = randomMPS(sites)
-sw = Sweeps(5)
-cutoff!(sw,1E-10)
+function main()
+  N = 100
+  sites = spinOneSites(N)
 
-#G.S. energy for N=10,S=1/2 Heisenberg is -4.258035206805 
+  ampo = AutoMPO(sites)
+  for j=1:N-1
+    add!(ampo,"Sz",j,"Sz",j+1)
+    add!(ampo,0.5,"S+",j,"S-",j+1)
+    add!(ampo,0.5,"S-",j,"S+",j+1)
+  end
+  H = toMPO(ampo)
 
-maxdim!(sw,10,20,100,100,200)
-energy,psi = @time dmrg(H,psi0,sw,maxiter=3)
-@printf "Final energy = %.12f\n" energy
+  psi0 = randomMPS(sites)
 
-Profile.clear()  # in case we have any previous profiling data
-@profile dmrg(H,psi0,sw,maxiter=3)
-ProfileView.view()
+  sweeps = Sweeps(5)
+  maxdim!(sweeps,10,20,100,100,200)
+  cutoff!(sweeps,1E-10)
+  @show sweeps
 
+  energy,psi = dmrg(H,psi0,sweeps)
+  @printf("Final energy = %.12f\n",energy)
+end
+main()
