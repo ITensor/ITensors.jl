b354ebbbc01d0c79014c227dfbeb9c9f390b7298
Miles Stoudenmire <miles.stoudenmire@gmail.com>
2019-08-01 00:01:45 -0400

    Simplified IndexSet printing

--- src/indexset.jl
+++ src/indexset.jl
@@ -39,10 +39,11 @@ IndexSet(inds::NTuple{2,IndexSet}) = IndexSet(inds...)
 # Convert to an Index if there is only one
 Index(is::IndexSet) = length(is)==1 ? is[1] : error("Number of Index in IndexSet ≠ 1")
 
-function Base.show(io::IO, indset::IndexSet)
-  print(io, "IndexSet(")
-  join(io, (sprint(show, ind) for ind in indset.inds), ", ")
-  print(io, ')')
+function Base.show(io::IO, is::IndexSet)
+  for i in is.inds
+    print(io,i)
+    print(io," ")
+  end
 end
 
 getindex(is::IndexSet,n::Integer) = getindex(is.inds,n)
