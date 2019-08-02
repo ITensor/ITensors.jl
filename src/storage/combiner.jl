17637e94b87ee9d54d9d1547e1c7c2038857480f
Miles Stoudenmire <miles.stoudenmire@gmail.com>
2019-08-02 17:28:29 -0400

    Compressed some code in combiner.jl and other minor edits

--- src/storage/combiner.jl
+++ src/storage/combiner.jl
@@ -1,7 +1,7 @@
 export Combiner
 
 struct CombinerStorage <: TensorStorage
-    ci::Index
+  ci::Index
 end
 
 # use eachindex here
@@ -11,26 +11,15 @@ function storage_contract(CSstore::CombinerStorage,
                           dis::IndexSet)
     cind = Cis[1]
     if hasindex(dis, cind) # has combined index, uncombine
-        c_pos = indexpositions(dis, cind)[1]
-        Nis   = IndexSet(length(dis) + length(Cis) - 2)
-        i = 1
-        for j in 1:length(dis)
-            if j == c_pos
-                for k in 2:length(Cis)
-                    Nis[i] = Cis[k]
-                    i += 1
-                end
-            else
-                Nis[i] = dis[j]
-                i += 1
-            end
-        end
+        cpos = findindex(dis,cind)
+        ninds = vcat(inds(dis)[1:cpos-1],inds(Cis)[2:end],inds(dis)[cpos+1:end])
+        Nis = IndexSet(ninds)
         return Nis, Dstore
     else # lacks combined index, combine
         # dis doesn't have cind, replace
         # Cis[1], Cis[2], ... with cind, may need to permute
         j_1_pos = indexpositions(dis, Cis[2])[1]
-        j_1_pos == 0 && throw(ArgumentError("no contracted indices in combiner-tensor product"))
+        j_1_pos < 1 && throw(ArgumentError("no contracted indices in combiner-tensor product"))
         # Check if Cis[2], Cis[3], ... are grouped together (contiguous)
         # and in same order as on combiner
         c_pos = indexpositions(Cis, cind)[1]
@@ -44,31 +33,25 @@ function storage_contract(CSstore::CombinerStorage,
             c > length(Cis) && break
         end
         contig_sameord &= c == length(Cis) + 1
-        Nis = IndexSet(length(dis) + 2 - length(Cis))
         if contig_sameord
-            # this is nasty
-            i = 1
-            for j in 1:j_1_pos-1
-                Nis[i] = dis[j]
-                i += 1
-            end
-            Nis[i] = cind
-            i += 1
-            for j in (j_1_pos + length(Cis) - 1):length(dis)
-                Nis[i] = dis[j]
-                i += 1
-            end
+            lend = j_1_pos-1
+            rstart = j_1_pos+length(Cis)-1
+            ninds = vcat(inds(dis)[1:lend],cind,inds(dis)[rstart:end])
+            Nis = IndexSet(ninds)
         else # permutation required
+
             # Set P destination values to -1 to mark indices that need to be assigned destinations:
             P = fill(-1, length(dis))
             # permute combined indices to the front, in same order as in Cis:
             ni = 1
             for c in 2:length(Cis)
-                j = indexpositions(dis, Cis[c])[1]
-                j == 0 && throw(ArgumentError("combiner missing index"))
-                P[j] = ni
-                ni += 1
+              j = findindex(dis, Cis[c])
+              j < 1 && throw(ArgumentError("combiner missing index"))
+              P[j] = ni
+              ni += 1
             end
+
+            Nis = IndexSet(length(dis)+2-length(Cis))
             Nis[1] = cind
             i = 2
             for j in 1:length(dis)
@@ -79,10 +62,17 @@ function storage_contract(CSstore::CombinerStorage,
                     ni += 1
                 end
             end
-            Dstore = Dense{eltype(Dstore)}(vec(permutedims(reshape(data(Dstore), dims(dis)), invperm(P))))
+
+            ddata = vec(permutedims(reshape(data(Dstore), dims(dis)), invperm(P)))
+            Dstore = Dense{eltype(Dstore)}(ddata)
         end
         return Nis, Dstore
     end
 end
 
-storage_contract(Dstore::Dense, dis::IndexSet, CSstore::CombinerStorage, Cis::IndexSet) = storage_contract(CSstore, Cis, Dstore, dis)
+function storage_contract(Dstore::Dense, 
+                          dis::IndexSet, 
+                          CSstore::CombinerStorage, 
+                          Cis::IndexSet) 
+  return storage_contract(CSstore, Cis, Dstore, dis)
+end
