578cfdc8363d010107da125d923c7c53bda11fad
Miles Stoudenmire <miles.stoudenmire@gmail.com>
2019-09-15 12:10:55 -0400

    Updated test_smallstring.jl to use new interface

--- test/test_smallstring.jl
+++ test/test_smallstring.jl
@@ -3,21 +3,18 @@ using ITensors,
 
 import ITensors.SmallString, 
        ITensors.IntChar, 
-       ITensors.push,
        ITensors.isint
 
 @testset "SmallString" begin
   @testset "ctors" begin
       s = SmallString()
-      @test length(s) == 0
       @test ITensors.isNull(s)
   end
 
   @testset "setindex" begin
       s = SmallString()
       @test ITensors.isNull(s)
-      t = Base.setindex(s, IntChar(1), 1)
-      @test length(t) == 1
+      t = setindex(s, IntChar(1), 1)
       @test !ITensors.isNull(t)
   end
 
@@ -38,43 +35,40 @@ import ITensors.SmallString,
   end
 
   @testset "Convert to String" begin
-    s = SmallString()
-    s = push(s, 'a')
-    s = push(s, 'b')
-    s = push(s, 'c')
-    sg = Base.String(s)
-    for n=1:length(s)
-      @test convert(Char,s[n]) == sg[n]
+    s = SmallString("abc")
+    @test typeof(s) == SmallString
+
+    sg = String(s)
+    for n=1:length(sg)
+      @test sg[n] == convert(Char,s[n])
     end
   end
 
   @testset "isint" begin
-    i = SmallString()
-    i = push(i, '1')
-    i = push(i, '2')
-    i = push(i, '3')
+    i = SmallString("123")
     @test isint(i) == true
 
-    s = SmallString()
-    s = push(s, 'a')
-    s = push(s, 'b')
-    s = push(s, 'c')
+    s = SmallString("abc")
     @test isint(s) == false
   end
 
   @testset "isless" begin
-    s1 = SmallString() 
-    s1=push(s1,'a') 
-    s1=push(s1,'b')
-
-    s2 = SmallString() 
-    s2=push(s2,'x') 
-    s2=push(s2,'y')
-
+    s1 = SmallString("ab") 
+    s2 = SmallString("xy") 
     @test isless(s1,s2) == true
     @test isless(s2,s1) == false
     @test isless(s1,s1) == false
     @test isless(s2,s2) == false
+  end
+
+  @testset "push" begin
+    s1 = SmallString("ab")
+
+    s2 = SmallString()
+    s2 = push(s2,'a')
+    @test s2 != s1
+    s2 = push(s2,'b')
+    @test s2 == s1
   end
 
 end
