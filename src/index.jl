90d08670d066ecf6e872eb721bb8036ccdaf45c2
Miles Stoudenmire <miles.stoudenmire@gmail.com>
2019-11-22 12:58:26 -0500

    Fixed readcpp for Index

--- src/index.jl
+++ src/index.jl
@@ -279,11 +279,11 @@ adjoint(iv::IndexVal) = IndexVal(adjoint(ind(iv)),val(iv))
 
 show(io::IO,iv::IndexVal) = print(io,ind(iv),"=$(val(iv))")
 
-function Base.read(io::IO,::Type{Index}; kwargs...)
-  format = get(kwargs,:format,"hdf5")
+function readcpp(io::IO,::Type{Index}; kwargs...)
+  format = get(kwargs,:format,"v3")
   i = Index()
-  if format=="cpp"
-    tags = read(io,TagSet;kwargs...)
+  if format=="v3"
+    tags = readcpp(io,TagSet;kwargs...)
     id = read(io,IDType)
     dim = convert(Int64,read(io,Int32))
     dir_int = read(io,Int32)
