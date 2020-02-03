dec6bcaf9a2111f1bcc3469873c9e45d52e4c263
Matthew Fishman <mfishman@flatironinstitute.org>
2020-02-03 14:45:45 -0500

    Add test

--- test/itensor_blocksparse.jl
+++ test/itensor_blocksparse.jl
@@ -46,6 +46,9 @@ using ITensors,
     s = Index([QN(0)=>1,QN(1)=>1],"s")
     T = randomITensor(QN(0),s,s')
     cT = copy(T)
+    for ss in dim(s), ssp in dim(s')
+      @test T[s(ss),s'(ssp)] == cT[s(ss),s'(ssp)]
+    end
   end
 
   @testset "Permute" begin
