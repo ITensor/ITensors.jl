16c2cef794853c10524872b4e3780b6ce600c06a
Matthew Fishman <mfishman@flatironinstitute.org>
2020-03-20 09:57:02 -0400

    Parametrize spectrum object (to allow for other storage like CuVector)

--- src/Tensors/linearalgebra.jl
+++ src/Tensors/linearalgebra.jl
@@ -37,8 +37,8 @@ end
 contains the (truncated) density matrix eigenvalue spectrum which is computed during a
 decomposition done by `svd` or `eigen`. In addition stores the truncation error.
 """
-struct Spectrum
-  eigs::Vector{Float64}
+struct Spectrum{VecT<:AbstractVector}
+  eigs::VecT
   truncerr::Float64
 end
 
