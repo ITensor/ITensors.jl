1c8b9f9db9be28e800600d5e65ce3716f02cf041
Matthew Fishman <mfishman@flatironinstitute.org>
2020-04-14 15:54:29 -0400

    Rename files

--- docs/make.jl
+++ docs/make.jl
@@ -3,9 +3,9 @@ using Documenter, ITensors
 makedocs(sitename = "ITensors.jl";
          modules = [ITensors],
          pages = ["Introduction" => "index.md",
-                  "Index" => "Index.md",
-                  "IndexSet" => "IndexSet.md",
-                  "ITensor" => "ITensor.md",
+                  "Index" => "IndexType.md",
+                  "IndexSet" => "IndexSetType.md",
+                  "ITensor" => "ITensorType.md",
                   "MPS and MPO" => "MPSandMPO.md",
                   "DMRG" => "DMRG.md"],
          format = Documenter.HTML(prettyurls = false),
