4c4220c9117c8cd7ffae14b8e3d0c2aa89353365
Matthew Fishman <mfishman@flatironinstitute.org>
2020-04-22 11:06:52 -0400

    Reorganize snoop files

--- precompile/make_precompile.jl
+++
@@ -1,8 +0,0 @@
-using SnoopCompile
-
-inf_timing = @snoopi tmin=0.01 include("snoop.jl")
-pc = SnoopCompile.parcel(inf_timing)
-SnoopCompile.write("tmp", pc)
-cp("tmp/precompile_ITensors.jl",
-   "precompile.jl";
-   force=true)
