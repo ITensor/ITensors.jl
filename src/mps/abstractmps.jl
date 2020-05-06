76bcd6654ca087bd7d5a6ddd47a834fd46e7378b
Miles Stoudenmire <miles.stoudenmire@gmail.com>
2020-05-06 16:46:35 -0400

    Some docs for abstractmps.jl

--- src/mps/abstractmps.jl
+++ src/mps/abstractmps.jl
@@ -530,8 +530,32 @@ end
 
 Base.:-(M::AbstractMPS) = Base.:*(-1,M)
 
+"""
+    hasqns(M::MPS)
+
+    hasqns(M::MPO)
+
+Return true if the MPS or MPO has
+tensors which carry quantum numbers.
+"""
 hasqns(M::AbstractMPS) = hasqns(M[1])
 
+"""
+    flux(M::MPS)
+
+    flux(M::MPO)
+
+    totalqn(M::MPS)
+
+    totalqn(M::MPO)
+
+For an MPS or MPO which conserves quantum
+numbers, compute the total QN flux. For
+a tensor network such as an MPS or MPO,
+the flux is the sum of fluxes of each of
+the tensors in the network. The name
+`totalqn` is an alias for `flux`.
+"""
 function flux(M::AbstractMPS)::QN
   hasqns(M) || error("MPS or MPO does not conserve QNs")
   q = QN()
