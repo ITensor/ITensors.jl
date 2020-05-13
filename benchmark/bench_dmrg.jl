f1d2b22e8110bd0340b5ca8340c966bd3253819c
Matthew Fishman <mfishman@flatironinstitute.org>
2020-05-13 14:11:53 -0400

    Set up benchmark/benchmarks.jl to use PkgBenchmark.jl

---
+++ benchmark/bench_dmrg.jl
@@ -0,0 +1,79 @@
+module BenchDMRG
+
+using BenchmarkTools
+using ITensors
+
+suite = BenchmarkGroup()
+
+let N = 100
+  sites = siteinds("S=1", N)
+  ampo = AutoMPO()
+  for j=1:N-1
+    ampo .+= (     "Sz", j, "Sz", j + 1)
+    ampo .+= (0.5, "S+", j, "S-", j + 1)
+    ampo .+= (0.5, "S-", j, "S+", j + 1)
+  end
+  H = MPO(ampo,sites)
+  state = [isodd(n) ? "Up" : "Dn" for n in 1:N]
+  psi = productMPS(sites, state)
+  sweeps = Sweeps(5)
+  maxdim!(sweeps, 10, 20, 100, 100, 200)
+  cutoff!(sweeps, 1e-11)
+
+  # Precompile
+  dmrg(H, psi, sweeps; outputlevel = 0)
+
+  suite["1d_S=1_heisenberg"] = @benchmarkable dmrg($H, $psi, $sweeps;
+                                                   outputlevel = 0)
+end
+
+let N = 100
+  sites = siteinds("S=1", N; conserve_qns = true)
+  ampo = AutoMPO()
+  for j=1:N-1
+    ampo .+= (     "Sz", j, "Sz", j+1)
+    ampo .+= (0.5, "S+", j, "S-", j+1)
+    ampo .+= (0.5, "S-", j, "S+", j+1)
+  end
+  H = MPO(ampo,sites)
+  state = [isodd(n) ? "Up" : "Dn" for n in 1:N]
+  psi = productMPS(sites, state)
+  sweeps = Sweeps(5)
+  maxdim!(sweeps, 10, 20, 100, 100, 200)
+  cutoff!(sweeps, 1E-10)
+
+  # Precompile
+  dmrg(H, psi, sweeps; outputlevel = 0)
+
+  suite["1d_S=1_heisenberg_qn"] = @benchmarkable dmrg($H, $psi, $sweeps;
+                                                      outputlevel = 0)
+end
+
+#let Ny = 6,
+#    Nx = 12
+#  N = Nx * Ny
+#  sites = siteinds("S=1/2", N)
+#  lattice = square_lattice(Nx, Ny; yperiodic = false)
+#  ampo = AutoMPO()
+#  for b in lattice
+#    ampo .+= (0.5, "S+", b.s1, "S-", b.s2)
+#    ampo .+= (0.5, "S-", b.s1, "S+", b.s2)
+#    ampo .+= (     "Sz", b.s1, "Sz", b.s2)
+#  end
+#  H = MPO(ampo, sites)
+#  state = [isodd(n) ? "Up" : "Dn" for n=1:N]
+#  psi = productMPS(sites, state)
+#  sweeps = Sweeps(10)
+#  maxdim!(sweeps, 10, 20, 100, 100, 200, 400, 800)
+#  cutoff!(sweeps, 1e-8)
+#
+#  # Precompile
+#  dmrg(H, psi, sweeps; outputlevel = 0)
+#
+#  suite["2d_S=1/2_heisenberg"] = @benchmarkable dmrg($H, $psi, $sweeps;
+#                                                   outputlevel = 0)
+#end
+
+end
+
+BenchDMRG.suite
---
+++ benchmark/bench_inplace.jl
@@ -0,0 +1,15 @@
+module BenchInplace
+
+using BenchmarkTools
+using ITensors
+
+suite = BenchmarkGroup()
+
+i = Index(100);
+A = randomITensor(i', i)
+B = randomITensor(i, i')
+
+suite["axpy!"] = @benchmarkable axpy!(2, $A, $B)
+end
+
+BenchInplace.suite
