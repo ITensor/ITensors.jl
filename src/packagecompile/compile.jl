691d9d7bb033730f9813d7f5e9f9099234d45df6
Matthew Fishman <mfishman@flatironinstitute.org>
2020-06-25 15:07:33 -0400

    Compile QN DMRG. Improve the docstring and output message of compile command

--- src/packagecompile/compile.jl
+++ src/packagecompile/compile.jl
@@ -1,59 +1,61 @@
 
-"""
-    ITensors.compile(; path = "~/.julia/sysimages",
-                       filename = "sys_itensors.so")
+const default_compile_dir = joinpath(ENV["HOME"],
+                                     ".julia",
+                                     "sysimages")
 
-Compile ITensors.jl with [PackageCompiler](https://julialang.github.io/PackageCompiler.jl/dev/).
+const default_compile_filename = "sys_itensors.so"
 
-This will take some time, perhaps a few minutes. This will create the file `sys_itensors.so`, by default in the directory `~/.julia/sysimages/`. This is a version of Julia that is compiled with ITensors. After running this command, if you start Julia with:
-```
-~ julia --sysimage ~/.julia/sysimages/sys_itensors.so
-```
-and you should see the startup time and JIT compilation time are substantially improved when you are using ITensors.
+const default_compile_path = joinpath(default_compile_dir,
+                                      default_compile_filename)
 
-You can create an alias like:
-```
-~ alias julia_itensors="julia --sysimage ~/.julia/sysimages/sys_itensors.so"
-```
-(which you can put in your `~/.bashrc`, `~/.zshrc`, etc.). Then you can start Julia with a version of ITensors installed with the command:
-```
-~ julia_itensors
-```
 
-Note that if you update ITensors to a new version, for example with `Pkg.update("ITensors")`, you will need to run this command again to recompile the new version of ITensors.
-"""
-function compile(; path::AbstractString = joinpath(ENV["HOME"],
-                                                   ".julia",
-                                                   "sysimages"),
-                   filename::AbstractString = "sys_itensors.so")
-  if !isdir(path)
-    println("""The directory "$path" doesn't exist yet, creating it now.""")
+function compile_note(; dir = default_compile_dir,
+                        filename = default_compile_filename)
+  path = joinpath(dir, filename)
+  return """
+  You will be able to start Julia with a compiled version of ITensors using:
+  ```
+  ~ julia --sysimage $path
+  ```
+  and you should see that the startup times and JIT compilation times are substantially improved when you are using ITensors.
+
+  In unix, you can create an alias with the Bash command:
+  ```
+  ~ alias julia_itensors="julia --sysimage $path -e 'using ITensors' -i"
+  ```
+  which you can put in your `~/.bashrc`, `~/.zshrc`, etc. This also executes `using ITensors` so that ITensors is loaded and ready to use, you can leave off ` -e 'using ITensors' -i` if you don't want that. Then you can start Julia with a version of ITensors installed with the command:
+  ```
+  ~ julia_itensors
+  ```
+
+  Note that if you update ITensors to a new version, for example with `using Pkg; Pkg.update("ITensors")`, you will need to run the `ITensors.compile()` command again to recompile the new version of ITensors.
+  """
+end
+
+function compile(; dir::AbstractString = default_compile_dir,
+                   filename::AbstractString = default_compile_filename)
+  if !isdir(dir)
+    println("""The directory "$dir" doesn't exist yet, creating it now.""")
     println()
-    mkdir(path)
+    mkdir(dir)
   end
-  path_filename = joinpath(path, filename)
-  println("""Creating the system image containing the compiled version of ITensors at "$path_filename". This may take a few minutes.""")
+  path = joinpath(dir, filename)
+  println("""Creating the system image "$path" containing the compiled version of ITensors. This may take a few minutes.""")
   create_sysimage(:ITensors;
-                  sysimage_path = path_filename,
+                  sysimage_path = path,
                   precompile_execution_file = joinpath(@__DIR__,
                                                        "precompile_itensors.jl"))
-  println("""
-
-          The system image containing the compiled version of ITensors is located at "$path_filename". This is a version of Julia that is compiled with ITensors.
-
-          You can now start Julia with:
-          ```
-          ~ julia --sysimage $path_filename
-          ```
-          and you should see that the startup times and JIT compilation times are substantially improved when you are using ITensors.
-
-          You can create an alias like:
-          ```
-          ~ alias julia_itensors="julia --sysimage $path_filename"
-          ```
-          (which you can put in your `~/.bashrc`, `~/.zshrc`, etc.). Then you can start Julia with a version of ITensors installed with the command:
-          ```
-          ~ julia_itensors
-          ```
-          """)
+  println(compile_note(; dir = dir, filename = filename))
+  return path
 end
+
+@doc """
+    ITensors.compile(; dir = "$default_compile_dir",
+                       filename = "$default_compile_filename")
+
+Compile ITensors.jl with [PackageCompiler](https://julialang.github.io/PackageCompiler.jl/dev/). This will take some time, perhaps a few minutes.
+
+This will create a system image containing the compiled version of ITensors located at `dir/filename`, by default `$default_compile_path`.
+
+$(compile_note())
+""" compile
