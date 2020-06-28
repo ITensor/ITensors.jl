0d8d06ec0f80cc40c79ca224d989aa03d90fd765
Miles Stoudenmire <miles.stoudenmire@gmail.com>
2020-06-28 15:41:52 -0400

    Unit tests for custom siteind and siteinds

--- test/sitetype.jl
+++ test/sitetype.jl
@@ -96,6 +96,71 @@ using ITensors,
     @test Sp[s'(3),s(4)] ≈ sqrt(3)
   end
 
+  @testset "siteind defined by space overload" begin
+    ITensors.space(::SiteType"Test1") = 4
+    s = siteind("Test1",3)
+    @test dim(s) == 4
+    @test hastags(s,"Site,Test1,n=3")
+  end
+
+  @testset "siteind defined by siteind overload" begin
+    ITensors.siteind(::SiteType"Test2",n) = Index(4,"Test2,n=$n")
+    s = siteind("Test2",3)
+    @test dim(s) == 4
+    @test hastags(s,"Test2,n=3")
+  end
+
+  @testset "siteind defined by space overload with QN" begin
+    function ITensors.space(::SiteType"Test3") 
+      return [QN("T",0)=>2, QN("T",1)=>1, QN("T",2)=>1]
+    end
+    s = siteind("Test3",3)
+    @test dim(s) == 4
+    @test hasqns(s)
+    @test hastags(s,"Site,Test3,n=3")
+  end
+
+  @testset "siteinds defined by space overload" begin
+    function ITensors.space(::SiteType"Test4"; kwargs...) 
+      conserve_qns = get(kwargs,:conserve_qns,false)
+      if conserve_qns
+        return [QN("T",0)=>2, QN("T",1)=>1, QN("T",2)=>1]
+      end
+      return 4
+    end
+
+    # Without QNs
+    s = siteinds("Test4",6)
+    @test length(s) == 6
+    @test dim(s[1]) == 4
+    for n=1:length(s)
+      @test hastags(s[n],"Site,Test4,n=$n")
+      @test !hasqns(s[n])
+    end
+
+    # With QNs
+    s = siteinds("Test4",6;conserve_qns=true)
+    @test length(s) == 6
+    @test dim(s[1]) == 4
+    for n=1:length(s)
+      @test hastags(s[n],"Site,Test4,n=$n")
+      @test hasqns(s[n])
+    end
+
+  end
+
+  @testset "siteinds defined by siteinds overload" begin
+    function ITensors.siteinds(::SiteType"Test5",N; kwargs...) 
+      return [Index(4,"Test5,n=$n") for n=1:N]
+    end
+    s = siteinds("Test5",8)
+    @test length(s) == 8
+    @test dim(s[1]) == 4
+    for n=1:length(s)
+      @test hastags(s[n],"Test5,n=$n")
+    end
+  end
+
 end
 
 nothing
