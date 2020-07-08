dc8a43cbd0f258de8c7bb1d863ac9b226b6f48ad
Miles Stoudenmire <miles.stoudenmire@gmail.com>
2020-07-07 23:09:57 -0400

    Fix some issues with precompile_itensors.jl code

--- src/packagecompile/precompile_itensors.jl
+++ src/packagecompile/precompile_itensors.jl
@@ -9,7 +9,7 @@ using ITensors
 #                           test"),
 #                  "runtests.jl"))
 
-N = 10
+N = 6
 sites = siteinds("S=1",N)
 ampo = AutoMPO()
 for j=1:N-1
@@ -18,13 +18,13 @@ for j=1:N-1
     ampo .+= (0.5,"S-",j,"S+",j+1)
 end
 H = MPO(ampo,sites)
-psi0 = randomMPS(sites,10)
-sweeps = Sweeps(5)
-maxdim!(sweeps, 10,20,100,100,200)
-cutoff!(sweeps, 1E-11)
+psi0 = randomMPS(sites,2)
+sweeps = Sweeps(1)
+maxdim!(sweeps, 10)
+cutoff!(sweeps, 1E-13)
 energy, psi = dmrg(H,psi0, sweeps)
 
-sites = siteinds("S=1", N; conserveqns = true)
+sites = siteinds("S=1", N; conserve_qns = true)
 ampo = AutoMPO()
 for j=1:N-1
     ampo .+= ("Sz",j,"Sz",j+1)
@@ -32,8 +32,9 @@ for j=1:N-1
     ampo .+= (0.5,"S-",j,"S+",j+1)
 end
 H = MPO(ampo,sites)
-psi0 = randomMPS(sites,10)
-sweeps = Sweeps(5)
-maxdim!(sweeps, 10,20,100,100,200)
-cutoff!(sweeps, 1E-11)
+state = [isodd(n) ? "Up" : "Dn" for n=1:N]
+psi0 = randomMPS(sites,state,2)
+sweeps = Sweeps(1)
+maxdim!(sweeps, 10)
+cutoff!(sweeps, 1E-13)
 energy, psi = dmrg(H,psi0, sweeps)
