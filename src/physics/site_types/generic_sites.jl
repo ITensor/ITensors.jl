8100d13579b4af6c98a5a8975a13caf3939e64c8
Miles Stoudenmire <miles.stoudenmire@gmail.com>
2020-07-09 13:33:06 -0400

    Update generic "Id" op overload to use op!

--- src/physics/site_types/generic_sites.jl
+++ src/physics/site_types/generic_sites.jl
@@ -1,11 +1,10 @@
 
-function op(::OpName"Id",
-            ::SiteType,
-            s::Index)
-  Op = emptyITensor(s', dag(s))
+function op!(Op::ITensor,
+             ::OpName"Id",
+             ::SiteType,
+             s::Index)
   for n=1:dim(s)
     Op[n,n] = 1.0
   end
-  return Op
 end
 
