124dc4e76e1afc2d956aceadc5b9d3d70bba9226
Matthew Fishman <mfishman@flatironinstitute.org>
2020-07-09 13:54:14 -0400

    Clean up precompile_itensors.jl a bit

--- src/packagecompile/precompile_itensors.jl
+++ src/packagecompile/precompile_itensors.jl
@@ -10,33 +10,26 @@ using ITensors
 #                  "runtests.jl"))
 
 N = 6
-sites = siteinds("S=1",N)
-ampo = AutoMPO()
-for j=1:N-1
-    ampo .+= "Sz",j,"Sz",j+1
-    ampo .+= 0.5,"S+",j,"S-",j+1
-    ampo .+= 0.5,"S-",j,"S+",j+1
-end
-H = MPO(ampo,sites)
-psi0 = randomMPS(sites,2)
-sweeps = Sweeps(1)
+sweeps = Sweeps(3)
 maxdim!(sweeps, 10)
 cutoff!(sweeps, 1E-13)
-energy, psi = dmrg(H,psi0, sweeps)
 
-sites = siteinds("S=1", N; conserve_qns=true)
-if !hasqns(sites[1])
+ampo = AutoMPO()
+for j=1:N-1
+  ampo .+=     "Sz",j,"Sz",j+1
+  ampo .+= 0.5,"S+",j,"S-",j+1
+  ampo .+= 0.5,"S-",j,"S+",j+1
+end
+
+sites = siteinds("S=1", N)
+H = MPO(ampo, sites)
+psi0 = randomMPS(sites, 2)
+dmrg(H, psi0, sweeps)
+
+sites_qn = siteinds("S=1", N; conserve_qns=true)
+if !hasqns(sites_qn[1])
   throw(ErrorException("Index does not have QNs in part of precompile script"))
 end
-ampo = AutoMPO()
-for j=1:N-1
-    ampo .+= ("Sz",j,"Sz",j+1)
-    ampo .+= (0.5,"S+",j,"S-",j+1)
-    ampo .+= (0.5,"S-",j,"S+",j+1)
-end
-H = MPO(ampo,sites)
-psi0 = randomMPS(sites,[isodd(n) ? "Up" : "Dn" for n=1:N],2)
-sweeps = Sweeps(1)
-maxdim!(sweeps, 10)
-cutoff!(sweeps, 1E-13)
-energy, psi = dmrg(H,psi0, sweeps)
+H_qn = MPO(ampo, sites_qn)
+psi0_qn = randomMPS(sites_qn, [isodd(n) ? "Up" : "Dn" for n=1:N], 2)
+dmrg(H_qn, psi0_qn, sweeps)
