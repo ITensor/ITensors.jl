8e3ced45003e78db82caa53a793d026393c96fa9
mtfishman <mtfishman@users.noreply.github.com>
2020-07-14 11:49:17 -0400

    Fix productMPS docstring

--- src/mps/mps.jl
+++ src/mps/mps.jl
@@ -306,7 +306,7 @@ consist of either an array of integers or strings, as
 recognized by the `state` function defined for the relevant
 Index tag type.
 
-#Examples
+# Examples
 ```julia
 N = 10
 sites = siteinds("S=1/2",N)
