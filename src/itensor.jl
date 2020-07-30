32a5374ef4d894f7253dc2ae167365d9711280e7
Matthew Fishman <mfishman@flatironinstitute.org>
2020-07-30 17:35:10 -0400

    Extend product definition

--- src/itensor.jl
+++ src/itensor.jl
@@ -1227,18 +1227,98 @@ end
 """
     product(A::ITensor, B::ITensor)
 
-For matrix-like ITensors (ones with pairs of primed and
-unprimed indices), perform a matrix product, i.e.
-```julia
-mapprime(prime(A) * B, 2, 1)
+Get the product of ITensor `A` and ITensor `B`, which
+roughly speaking is a matrix-matrix product, a
+matrix-vector product, or a vector-matrix product,
+depending on the index structure.
+
+There are three main modes:
+
+1. Matrix-matrix product. In this case, ITensors `A`
+and `B` have shared indices that come in pairs of primed
+and unprimed indices. Then, `A` and `B` are multiplied 
+together, treating them as matrices from the unprimed
+to primed indices, resulting in an ITensor `C` that
+has the same pairs of primed and unprimed indices. 
+For example:
 ```
-In the future, more general ITensors with other tag or
-prime conventions may be supported.
+s1'-<-----<-s1            s1'-<-----<-s1   s1'-<-----<-s1
+      |C|      = product(       |A|              |B|      )
+s2'-<-----<-s2            s2'-<-----<-s2 , s2'-<-----<-s2
+```
+Essentially, this is implemented as 
+`C = mapprime(A', B, 2 => 1)`.
+If there are dangling indices that are not shared between
+`A` and `B`, a "batched" matrix multiplication is
+performed, i.e.:
+```
+       j                         j
+       |                         |
+s1'-<-----<-s1            s1'-<-----<-s1   s1'-<-----<-s1
+      |C|      = product(       |A|              |B|      )
+s2'-<-----<-s2            s2'-<-----<-s2 , s2'-<-----<-s2
+```
+In addition, if there are shared dangling indices,
+they are summed over:
+```
+                                    j                j
+                                    |                |
+s1'-<-----<-s1               s1'-<-----<-s1   s1'-<-----<-s1
+      |C|      = Σⱼ product(       |A|              |B|      )
+s2'-<-----<-s2               s2'-<-----<-s2 , s2'-<-----<-s2
+```
+where the sum is not performed as an explicitly 
+for-loop, but as part of a single tensor contraction.
+
+2. Matrix-vector product. In this case, ITensor `A`
+has pairs of primed and unprimed indices, and ITensor
+`B` has unprimed indices that are shared with `A`.
+Then, `A` and `B` are multiplied as a matrix-vector
+product, and the result `C` has unprimed indices.
+For example:
+```
+s1-<----            s1'-<-----<-s1   s1-<----
+     |C| = product(       |A|             |B| )
+s2-<----            s2'-<-----<-s2 , s2-<----
+```
+Again, like in the matrix-matrix product above, you can have
+dangling indices to do "batched" matrix-vector products, or
+sum over a batch of matrix-vector products.
+
+3. Vector-matrix product. In this case, ITensor `B`
+has pairs of primed and unprimed indices, and ITensor
+`A` has unprimed indices that are shared with `B`.
+Then, `B` and `A` are multiplied as a matrix-vector
+product, and the result `C` has unprimed indices.
+For example:
+```
+---<-s1            ----<-s1   s1'-<-----<-s1
+|C|     = product( |A|              |B|      )
+---<-s2            ----<-s2 , s2'-<-----<-s2
+```
+Again, like in the matrix-matrix product above, you can have
+dangling indices to do "batched" vector-matrix products, or
+sum over a batch of vector-matrix products.
 """
-function product(A::ITensor,
-                 B::ITensor)
-  R = prime(A) * B
-  return mapprime(R,2,1)
+function product(A::ITensor, B::ITensor)
+  commonindsAB = commoninds(A, B; plev = 0)
+  isempty(commonindsAB) && error("In product, must have common indices with prime level 0.")
+  common_paired_indsA = filterinds(i -> hasind(commonindsAB, i) &&
+                                        hasind(A, setprime(i, 1)), A)
+  common_paired_indsB = filterinds(i -> hasind(commonindsAB, i) &&
+                                        hasind(B, setprime(i, 1)), B)
+  if isempty(common_paired_indsA) && isempty(common_paired_indsB)
+    return A * B
+  elseif hassameinds(common_paired_indsA, common_paired_indsB)
+    A′ = prime(A; inds = unioninds(common_paired_indsA,
+                                   common_paired_indsA'))
+    return setprime(A′ * B, 1; inds = common_paired_indsA'')
+  elseif isempty(common_paired_indsA) && !isempty(common_paired_indsB)
+    A′ = prime(A; inds = common_paired_indsB)
+    return A′ * B
+  elseif !isempty(common_paired_indsA) && isempty(common_paired_indsB)
+    return noprime(A * B; inds = common_paired_indsA')
+  end
 end
 
 #######################################################################
