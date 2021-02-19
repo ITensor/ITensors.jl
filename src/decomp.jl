ea7d18f12491edfe81be07d060d0bdfd60e0140f
Matthew Fishman <mfishman@flatironinstitute.org>
2021-02-19 16:42:43 -0500

    Improve polar decomposition implementation

--- src/decomp.jl
+++ src/decomp.jl
@@ -280,18 +280,33 @@ end
 polar(A::ITensor; kwargs...) = error(noinds_error_message("polar"))
 
 # TODO: allow custom tags in internal indices?
+# TODO: return the new common indices?
 function polar(A::ITensor, Linds...; kwargs...)
-  Lis = commoninds(A, IndexSet(Linds...))
-  Ris = uniqueinds(A, Lis)
-  Lpos, Rpos = NDTensors.getperms(inds(A), Lis, Ris)
-  UT, PT = polar(tensor(A), Lpos, Rpos)
-  U, P = itensor(UT), itensor(PT)
-  u = commoninds(U, P)
-  p = uniqueinds(P, U)
-  replaceinds!(U, u, p')
-  replaceinds!(P, u, p')
-  return U, P, commoninds(U, P)
+  U, S, V = svd(A, Linds...; kwargs...)
+  u = commoninds(S, U)
+  v = commoninds(S, V)
+  # TODO: add constructors that accept IndexSet
+  # TODO: use
+  # Vᵤ = replaceinds(V, v => u)
+  Q = U * δ(u..., v'...) * V'
+  # TODO: use
+  P = dag(V') * δ(dag(v')..., dag(u)...) * S * V
+  return Q, P
 end
+
+## XXX: TODO: remove old implementation after testing new one.
+## function polar(A::ITensor, Linds...; kwargs...)
+##   Lis = commoninds(A, IndexSet(Linds...))
+##   Ris = uniqueinds(A, Lis)
+##   Lpos, Rpos = NDTensors.getperms(inds(A), Lis, Ris)
+##   UT, PT = polar(tensor(A), Lpos, Rpos)
+##   U, P = itensor(UT), itensor(PT)
+##   u = commoninds(U, P)
+##   p = uniqueinds(P, U)
+##   replaceinds!(U, u, p')
+##   replaceinds!(P, u, p')
+##   return U, P, commoninds(U, P)
+## end
 
 function factorize_qr(A::ITensor, Linds...; kwargs...)
   ortho::String = get(kwargs, :ortho, "left")
