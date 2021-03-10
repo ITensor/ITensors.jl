7e6bb683611fc8662f9350c7ad66d5ab4103bac3
Matthew Fishman <mfishman@flatironinstitute.org>
2021-03-10 15:23:07 -0500

    Add documentation for space function for built-in site types. Update CI workflows

--- src/physics/site_types/spinhalf.jl
+++ src/physics/site_types/spinhalf.jl
@@ -1,4 +1,16 @@
 
+"""
+    space(::SiteType"S=1/2";
+          conserve_qns = false,
+          conserve_sz = conserve_qns,
+          conserve_szparity = false,
+          qnname_sz = "Sz",
+          qnname_szparity = "SzParity")
+
+Create the Hilbert space for a site of type "S=1/2".
+
+Optionally specify the conserved symmetries and their quantum number labels.
+"""
 function space(::SiteType"S=1/2";
                conserve_qns = false,
                conserve_sz = conserve_qns,
