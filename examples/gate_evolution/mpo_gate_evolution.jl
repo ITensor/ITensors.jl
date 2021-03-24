37bf3ee4caf3d7fe98c170fe0de28454f0660e00
Kun Chen <chenkun@mail.ustc.edu.cn>
2021-03-24 17:16:25 -0400

    Backwards compatible syntax for keyword argument `cutoff` in MPO gate evolution example (#604)
    Co-authored-by: Kun Chen <chenkun0228@gmail.com>

--- examples/gate_evolution/mpo_gate_evolution.jl
+++ examples/gate_evolution/mpo_gate_evolution.jl
@@ -46,7 +46,7 @@ function main(; N = 10, cutoff = 1E-8,
   # for Nsteps steps
   psi = psi0
   for step in 1:Nsteps
-    psi = apply(gates, psi; cutoff)
+    psi = apply(gates, psi; cutoff=cutoff)
     t += δt
     Sz = measure_Sz(psi,c)
     println("$t $Sz")
@@ -56,7 +56,7 @@ function main(; N = 10, cutoff = 1E-8,
   rho0 = MPO(psi0)
   rho = rho0
   for step in 1:Nsteps
-    rho = apply(gates, rho; cutoff, apply_dag = true)
+    rho = apply(gates, rho; cutoff=cutoff, apply_dag = true)
     t += δt
   end
   @show inner(psi, rho, psi)
