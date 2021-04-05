ef3e6bcb90d0bfc2ccfef45afa5fa6987b8a889f
Miles Stoudenmire <miles.stoudenmire@gmail.com>
2021-04-05 09:53:14 -0400

    Fix QN undef constructor docs and add other versions

--- src/qn/qnitensor.jl
+++ src/qn/qnitensor.jl
@@ -52,18 +52,37 @@ ITensor(::Type{ElT}, inds::QNIndex...) where {ElT<:Number} =
 ITensor(inds::QNIndex...) = ITensor(Float64, QN(), IndexSet(inds...))
 
 """
-    ITensor([::Type{ElT} = Float64, ]::UndefInitializer, inds)
-    ITensor([::Type{ElT} = Float64, ]::UndefInitializer, inds::Index...)
+    ITensor([::Type{ElT} = Float64,] ::UndefInitializer, flux::QN, inds)
+    ITensor([::Type{ElT} = Float64,] ::UndefInitializer, flux::QN, inds::Index...)
 
 Construct an ITensor with indices `inds` and BlockSparse storage with undefined elements of type `ElT`, where the nonzero (allocated) blocks are determined by the provided QN `flux`. One purpose for using this constructor is that initializing the elements in an undefined way is faster than initializing them to a set value such as zero.
 
 The storage will have `NDTensors.BlockSparse` type.
+
+# Examples
+
+```julia
+i = Index([QN(0)=>1, QN(1)=>2], "i")
+A = ITensor(undef,QN(0),i',dag(i))
+B = ITensor(Float64,undef,QN(0),i',dag(i))
+C = ITensor(ComplexF64,undef,QN(0),i',dag(i))
+```
 """
 function ITensor(::Type{ElT}, ::UndefInitializer, 
                  flux::QN, inds::Indices) where {ElT <: Number}
   blocks = nzblocks(flux, IndexSet(inds))
   T = BlockSparseTensor(ElT,undef,blocks,inds)
   return itensor(T)
+end
+
+function ITensor(::Type{ElT}, ::UndefInitializer, 
+                 flux::QN, inds::Index...) where {ElT <: Number}
+  return ITensor(ElT,undef,flux,IndexSet(inds...))
+end
+
+function ITensor(::UndefInitializer, 
+                 flux::QN, inds::Index...)
+  return ITensor(Float64,undef,flux,IndexSet(inds...))
 end
 
 
