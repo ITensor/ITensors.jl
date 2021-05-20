b6f508e75bd1101ba2c9a82eba33c1aa2b71acf6
Miles Stoudenmire <miles.stoudenmire@gmail.com>
2021-05-20 15:34:48 -0400

    Update fermions.jl test code to use pair index values

--- test/fermions.jl
+++ test/fermions.jl
@@ -42,12 +42,12 @@ using ITensors, Test
 
   @testset "Fermionic IndexVals" begin
     sn = Index([QN("Nf", 0, -1) => 1, QN("Nf", 1, -1) => 1], "sn")
-    @test fparity(sn(1)) == 0
-    @test fparity(sn(2)) == 1
+    @test fparity(sn=>1) == 0
+    @test fparity(sn=>2) == 1
 
     sp = Index([QN("Nfp", 0, -2) => 1, QN("Nfp", 1, -2) => 1], "sp")
-    @test fparity(sp(1)) == 0
-    @test fparity(sp(2)) == 1
+    @test fparity(sp=>1) == 0
+    @test fparity(sp=>2) == 1
   end
 
   @testset "Get and Set Elements" begin
@@ -55,46 +55,46 @@ using ITensors, Test
 
     N = emptyITensor(s', dag(s))
 
-    N[s'(2), s(2)] = 1.0
-    @test N[s'(2), s(2)] ≈ +1.0
-    @test N[s(2), s'(2)] ≈ -1.0
+    N[s'=>2, s=>2] = 1.0
+    @test N[s'=>2, s=>2] ≈ +1.0
+    @test N[s=>2, s'=>2] ≈ -1.0
 
-    N[s(2), s'(2)] = 1.0
-    @test N[s'(2), s(2)] ≈ -1.0
-    @test N[s(2), s'(2)] ≈ 1.0
+    N[s=>2, s'=>2] = 1.0
+    @test N[s'=>2, s=>2] ≈ -1.0
+    @test N[s=>2, s'=>2] ≈ 1.0
 
     C = emptyITensor(s', dag(s))
 
-    C[s'(1), s(2)] = 1.0
-    @test C[s'(1), s(2)] ≈ 1.0
-    @test C[s(2), s'(1)] ≈ 1.0
+    C[s'=>1, s=>2] = 1.0
+    @test C[s'=>1, s=>2] ≈ 1.0
+    @test C[s=>2, s'=>1] ≈ 1.0
 
     I = emptyITensor(s', dag(s))
-    I[s'(1), s(1)] = 1.0
-    I[s'(2), s(2)] = 1.0
-    @test I[s'(1), s(1)] ≈ 1.0
-    @test I[s'(2), s(2)] ≈ 1.0
+    I[s'=>1, s=>1] = 1.0
+    I[s'=>2, s=>2] = 1.0
+    @test I[s'=>1, s=>1] ≈ 1.0
+    @test I[s'=>2, s=>2] ≈ 1.0
 
-    @test I[s(1), s'(1)] ≈ 1.0
-    @test I[s(2), s'(2)] ≈ -1.0
+    @test I[s=>1, s'=>1] ≈ 1.0
+    @test I[s=>2, s'=>2] ≈ -1.0
   end
 
   @testset "Making operators different ways" begin
     s = Index([QN("Nf", 0, -1) => 1, QN("Nf", 1, -1) => 1], "s")
 
     N1 = emptyITensor(s', dag(s))
-    N1[s'(2), s(2)] = +1.0
+    N1[s'=>2, s=>2] = +1.0
 
     N2 = emptyITensor(dag(s), s')
-    N2[s'(2), s(2)] = +1.0
+    N2[s'=>2, s=>2] = +1.0
     @test norm(N1 - N2) ≈ 0.0
 
     N3 = emptyITensor(s', dag(s))
-    N3[s(2), s'(2)] = -1.0
+    N3[s=>2, s'=>2] = -1.0
     @test norm(N1 - N3) ≈ 0.0
 
     N4 = emptyITensor(dag(s), s')
-    N4[s(2), s'(2)] = -1.0
+    N4[s=>2, s'=>2] = -1.0
     @test norm(N1 - N4) ≈ 0.0
   end
 
@@ -103,16 +103,16 @@ using ITensors, Test
       s = Index([QN("Nf", 0, -1) => 1, QN("Nf", 1, -1) => 1], "s")
 
       N1 = emptyITensor(s', dag(s))
-      N1[s'(2), s(2)] = 1.0
+      N1[s'=>2, s=>2] = 1.0
 
       N2 = emptyITensor(dag(s), s')
-      N2[s'(2), s(2)] = 1.0
+      N2[s'=>2, s=>2] = 1.0
 
       pN1 = permute(N1, dag(s), s')
-      @test pN1[s'(2), s(2)] ≈ 1.0
+      @test pN1[s'=>2, s=>2] ≈ 1.0
 
       pN2 = permute(N2, s', dag(s))
-      @test pN2[s'(2), s(2)] ≈ 1.0
+      @test pN2[s'=>2, s=>2] ≈ 1.0
 
       #TODO add cases resulting in minus signs
     end
@@ -121,19 +121,19 @@ using ITensors, Test
       s = Index([QN("Nf", 0, -1) => 1, QN("Nf", 1, -1) => 1], "sn")
 
       N1 = emptyITensor(s', dag(s))
-      N1[s'(2), s(2)] = 1.0
+      N1[s'=>2, s=>2] = 1.0
 
       N2 = emptyITensor(dag(s), s')
-      N2[s'(2), s(2)] = 1.0
+      N2[s'=>2, s=>2] = 1.0
 
       NN = N1 + N2
-      @test NN[s'(2), s(2)] ≈ 2.0
+      @test NN[s'=>2, s=>2] ≈ 2.0
 
       NN = N1 + N1
-      @test NN[s'(2), s(2)] ≈ 2.0
+      @test NN[s'=>2, s=>2] ≈ 2.0
 
       NN = N2 + N2
-      @test NN[s'(2), s(2)] ≈ 2.0
+      @test NN[s'=>2, s=>2] ≈ 2.0
     end
 
     @testset "Wavefunction Tests" begin
@@ -141,35 +141,35 @@ using ITensors, Test
 
       psi0 = ITensor(s...)
 
-      psi0[s[1](1), s[2](1), s[3](1), s[4](1)] = 1111
-      psi0[s[1](3), s[2](3), s[3](1), s[4](1)] = 3311
-      psi0[s[1](1), s[2](3), s[3](1), s[4](3)] = 1313
+      psi0[s[1]=>1, s[2]=>1, s[3]=>1, s[4]=>1] = 1111
+      psi0[s[1]=>3, s[2]=>3, s[3]=>1, s[4]=>1] = 3311
+      psi0[s[1]=>1, s[2]=>3, s[3]=>1, s[4]=>3] = 1313
 
       psi1 = permute(psi0, s[2], s[1], s[3], s[4])
       @test norm(psi1 - psi0) ≈ 0.0
 
-      @test psi0[s[1](1), s[2](1), s[3](1), s[4](1)] ≈ 1111
-      @test psi1[s[1](1), s[2](1), s[3](1), s[4](1)] ≈ 1111
-      @test psi0[s[2](1), s[1](1), s[3](1), s[4](1)] ≈ 1111
-      @test psi1[s[2](1), s[1](1), s[3](1), s[4](1)] ≈ 1111
+      @test psi0[s[1]=>1, s[2]=>1, s[3]=>1, s[4]=>1] ≈ 1111
+      @test psi1[s[1]=>1, s[2]=>1, s[3]=>1, s[4]=>1] ≈ 1111
+      @test psi0[s[2]=>1, s[1]=>1, s[3]=>1, s[4]=>1] ≈ 1111
+      @test psi1[s[2]=>1, s[1]=>1, s[3]=>1, s[4]=>1] ≈ 1111
 
-      @test psi0[s[1](3), s[2](3), s[3](1), s[4](1)] ≈ 3311
-      @test psi1[s[1](3), s[2](3), s[3](1), s[4](1)] ≈ 3311
-      @test psi0[s[2](3), s[1](3), s[3](1), s[4](1)] ≈ -3311
-      @test psi1[s[2](3), s[1](3), s[3](1), s[4](1)] ≈ -3311
-      @test psi0[s[4](1), s[2](3), s[1](3), s[3](1)] ≈ -3311
-      @test psi1[s[4](1), s[2](3), s[1](3), s[3](1)] ≈ -3311
+      @test psi0[s[1]=>3, s[2]=>3, s[3]=>1, s[4]=>1] ≈ 3311
+      @test psi1[s[1]=>3, s[2]=>3, s[3]=>1, s[4]=>1] ≈ 3311
+      @test psi0[s[2]=>3, s[1]=>3, s[3]=>1, s[4]=>1] ≈ -3311
+      @test psi1[s[2]=>3, s[1]=>3, s[3]=>1, s[4]=>1] ≈ -3311
+      @test psi0[s[4]=>1, s[2]=>3, s[1]=>3, s[3]=>1] ≈ -3311
+      @test psi1[s[4]=>1, s[2]=>3, s[1]=>3, s[3]=>1] ≈ -3311
 
       psi2 = permute(psi0, s[4], s[1], s[3], s[2])
       @test norm(psi2 - psi0) ≈ 0.0
       @test norm(psi2 - psi1) ≈ 0.0
 
-      @test psi0[s[1](1), s[2](3), s[3](1), s[4](3)] ≈ 1313
-      @test psi1[s[1](1), s[2](3), s[3](1), s[4](3)] ≈ 1313
-      @test psi2[s[1](1), s[2](3), s[3](1), s[4](3)] ≈ 1313
-      @test psi0[s[4](3), s[1](1), s[3](1), s[2](3)] ≈ -1313
-      @test psi1[s[4](3), s[1](1), s[3](1), s[2](3)] ≈ -1313
-      @test psi2[s[4](3), s[1](1), s[3](1), s[2](3)] ≈ -1313
+      @test psi0[s[1]=>1, s[2]=>3, s[3]=>1, s[4]=>3] ≈ 1313
+      @test psi1[s[1]=>1, s[2]=>3, s[3]=>1, s[4]=>3] ≈ 1313
+      @test psi2[s[1]=>1, s[2]=>3, s[3]=>1, s[4]=>3] ≈ 1313
+      @test psi0[s[4]=>3, s[1]=>1, s[3]=>1, s[2]=>3] ≈ -1313
+      @test psi1[s[4]=>3, s[1]=>1, s[3]=>1, s[2]=>3] ≈ -1313
+      @test psi2[s[4]=>3, s[1]=>1, s[3]=>1, s[2]=>3] ≈ -1313
     end
   end
 
@@ -230,8 +230,8 @@ using ITensors, Test
 
     g = Index(QN("Nf", 1, -1) => 1; tags="g")
 
-    Bdag3 = Cdag3 * setelt(dag(g)(1))
-    B1 = setelt(g(1)) * C1
+    Bdag3 = Cdag3 * setelt(dag(g)=>1)
+    B1 = setelt(g=>1) * C1
 
     # Commutator
     @test norm(Bdag3 * B1 - B1 * Bdag3) ≈ 0.0
