91c829f5610eca1ffe68ecdbf2eb5aca60d059db
Miles Stoudenmire <miles.stoudenmire@gmail.com>
2021-06-14 00:10:01 -0400

    Fix incorrect pass-through for S=1/2 aliases

--- src/physics/site_types/spinhalf.jl
+++ src/physics/site_types/spinhalf.jl
@@ -149,6 +149,8 @@ space(::SiteType"SpinHalf"; kwargs...) = space(SiteType("S=1/2"); kwargs...)
 
 val(name::ValName, ::SiteType"SpinHalf") = val(name, SiteType("S=1/2"))
 
+state(name::StateName, ::SiteType"SpinHalf") = state(name,SiteType("S=1/2"))
+
 op(o::OpName, ::SiteType"SpinHalf"; kwargs...) = op(o, SiteType("S=1/2"); kwargs...)
 
 # Support the tag "S=½" as equivalent to "S=1/2"
@@ -157,6 +159,6 @@ space(::SiteType"S=½"; kwargs...) = space(SiteType("S=1/2"); kwargs...)
 
 val(name::ValName, ::SiteType"S=½") = val(name, SiteType("S=1/2"))
 
-state(::SiteType"S=½", n::StateName) = state(SiteType("S=1/2"), n)
+state(name::StateName,::SiteType"S=½") = state(name,SiteType("S=1/2"))
 
 op(o::OpName, ::SiteType"S=½"; kwargs...) = op(o, SiteType("S=1/2"); kwargs...)
