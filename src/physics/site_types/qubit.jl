4ee8cc0aeb5ecb618b9b5eb4c69dd824362503e7
Miles Stoudenmire <miles.stoudenmire@gmail.com>
2021-07-15 17:04:31 -0400

    Prepend site type functions with ITensors.

--- src/physics/site_types/qubit.jl
+++ src/physics/site_types/qubit.jl
@@ -18,7 +18,7 @@ Create the Hilbert space for a site of type "Qubit".
 
 Optionally specify the conserved symmetries and their quantum number labels.
 """
-function space(
+function ITensors.space(
   ::SiteType"Qubit";
   conserve_qns=false,
   conserve_parity=conserve_qns,
@@ -39,68 +39,68 @@ function space(
   return 2
 end
 
-val(::ValName"0", st::SiteType"Qubit") = 1
-val(::ValName"1", st::SiteType"Qubit") = 2
+ITensors.val(::ValName"0", st::SiteType"Qubit") = 1
+ITensors.val(::ValName"1", st::SiteType"Qubit") = 2
 
-state(::StateName"0", ::SiteType"Qubit") = [1.0, 0.0]
-state(::StateName"1", ::SiteType"Qubit") = [0.0, 1.0]
+ITensors.state(::StateName"0", ::SiteType"Qubit") = [1.0, 0.0]
+ITensors.state(::StateName"1", ::SiteType"Qubit") = [0.0, 1.0]
 
 # Use S=1/2 definition of any operators 
 # called using Qubit SiteType
-op(o::OpName, ::SiteType"Qubit"; kwargs...) = op(o, SiteType("S=1/2"); kwargs...)
+ITensors.op(o::OpName, ::SiteType"Qubit"; kwargs...) = op(o, SiteType("S=1/2"); kwargs...)
 
 #
 # 1-Qubit gates
 #
 
-op(::OpName"σx", t::SiteType"S=1/2") = op("X", t)
+ITensors.op(::OpName"σx", t::SiteType"S=1/2") = op("X", t)
 
-op(::OpName"σ1", t::SiteType"S=1/2") = op("X", t)
+ITensors.op(::OpName"σ1", t::SiteType"S=1/2") = op("X", t)
 
-op(::OpName"√NOT", ::SiteType"S=1/2") = [
+ITensors.op(::OpName"√NOT", ::SiteType"S=1/2") = [
   (1 + im)/2 (1 - im)/2
   (1 - im)/2 (1 + im)/2
 ]
 
-op(::OpName"√X", t::SiteType"S=1/2") = op("√NOT", t)
+ITensors.op(::OpName"√X", t::SiteType"S=1/2") = op("√NOT", t)
 
-op(::OpName"σy", t::SiteType"S=1/2") = op("Y", t)
+ITensors.op(::OpName"σy", t::SiteType"S=1/2") = op("Y", t)
 
-op(::OpName"σ2", t::SiteType"S=1/2") = op("Y", t)
+ITensors.op(::OpName"σ2", t::SiteType"S=1/2") = op("Y", t)
 
-op(::OpName"iσy", t::SiteType"S=1/2") = op("iY", t)
+ITensors.op(::OpName"iσy", t::SiteType"S=1/2") = op("iY", t)
 
-op(::OpName"iσ2", t::SiteType"S=1/2") = op("iY", t)
+ITensors.op(::OpName"iσ2", t::SiteType"S=1/2") = op("iY", t)
 
-op(::OpName"σz", t::SiteType"S=1/2") = op("Z", t)
+ITensors.op(::OpName"σz", t::SiteType"S=1/2") = op("Z", t)
 
-op(::OpName"σ3", t::SiteType"S=1/2") = op("Z", t)
+ITensors.op(::OpName"σ3", t::SiteType"S=1/2") = op("Z", t)
 
-op(::OpName"H", ::SiteType"S=1/2") = [
+ITensors.op(::OpName"H", ::SiteType"S=1/2") = [
   1/sqrt(2) 1/sqrt(2)
   1/sqrt(2) -1/sqrt(2)
 ]
 
 # Rϕ with ϕ = π/2
-op(::OpName"Phase", ::SiteType"S=1/2") = [
+ITensors.op(::OpName"Phase", ::SiteType"S=1/2") = [
   1 0
   0 im
 ]
 
-op(::OpName"P", t::SiteType"S=1/2") = op("Phase", t)
+ITensors.op(::OpName"P", t::SiteType"S=1/2") = op("Phase", t)
 
-op(::OpName"S", t::SiteType"S=1/2") = op("Phase", t)
+ITensors.op(::OpName"S", t::SiteType"S=1/2") = op("Phase", t)
 
 ## Rϕ with ϕ = π/4
-op(::OpName"π/8", ::SiteType"S=1/2") = [
+ITensors.op(::OpName"π/8", ::SiteType"S=1/2") = [
   1 0
   0 1 / sqrt(2)+im / sqrt(2)
 ]
 
-op(::OpName"T", t::SiteType"S=1/2") = op("π/8", t)
+ITensors.op(::OpName"T", t::SiteType"S=1/2") = op("π/8", t)
 
 # Rotation around X-axis
-function op(::OpName"Rx", ::SiteType"S=1/2"; θ::Number)
+function ITensors.op(::OpName"Rx", ::SiteType"S=1/2"; θ::Number)
   return [
     cos(θ / 2) -im*sin(θ / 2)
     -im*sin(θ / 2) cos(θ / 2)
@@ -108,7 +108,7 @@ function op(::OpName"Rx", ::SiteType"S=1/2"; θ::Number)
 end
 
 # Rotation around Y-axis
-function op(::OpName"Ry", ::SiteType"S=1/2"; θ::Number)
+function ITensors.op(::OpName"Ry", ::SiteType"S=1/2"; θ::Number)
   return [
     cos(θ / 2) -sin(θ / 2)
     sin(θ / 2) cos(θ / 2)
@@ -116,56 +116,56 @@ function op(::OpName"Ry", ::SiteType"S=1/2"; θ::Number)
 end
 
 # Rotation around Z-axis
-op(::OpName"Rz", ::SiteType"S=1/2"; ϕ::Number) = [
+ITensors.op(::OpName"Rz", ::SiteType"S=1/2"; ϕ::Number) = [
   1 0
   0 exp(im * ϕ)
 ]
 
 # Rotation around generic axis n̂
-function op(::OpName"Rn", ::SiteType"S=1/2"; θ::Real, ϕ::Real, λ::Real)
+function ITensors.op(::OpName"Rn", ::SiteType"S=1/2"; θ::Real, ϕ::Real, λ::Real)
   return [
     cos(θ / 2) -exp(im * λ)*sin(θ / 2)
     exp(im * ϕ)*sin(θ / 2) exp(im * (ϕ + λ))*cos(θ / 2)
   ]
 end
 
-op(::OpName"Rn̂", t::SiteType"S=1/2"; kwargs...) = op("Rn", t; kwargs...)
+ITensors.op(::OpName"Rn̂", t::SiteType"S=1/2"; kwargs...) = ITensors.op("Rn", t; kwargs...)
 
 #
 # 2-Qubit gates
 #
 
-op(::OpName"CNOT", ::SiteType"S=1/2") = [
+ITensors.op(::OpName"CNOT", ::SiteType"S=1/2") = [
   1 0 0 0
   0 1 0 0
   0 0 0 1
   0 0 1 0
 ]
 
-op(::OpName"CX", t::SiteType"S=1/2") = op("CNOT", t)
+ITensors.op(::OpName"CX", t::SiteType"S=1/2") = op("CNOT", t)
 
-op(::OpName"CY", ::SiteType"S=1/2") = [
+ITensors.op(::OpName"CY", ::SiteType"S=1/2") = [
   1 0 0 0
   0 1 0 0
   0 0 0 -im
   0 0 im 0
 ]
 
-op(::OpName"CZ", ::SiteType"S=1/2") = [
+ITensors.op(::OpName"CZ", ::SiteType"S=1/2") = [
   1 0 0 0
   0 1 0 0
   0 0 1 0
   0 0 0 -1
 ]
 
-op(::OpName"SWAP", ::SiteType"S=1/2") = [
+ITensors.op(::OpName"SWAP", ::SiteType"S=1/2") = [
   1 0 0 0
   0 0 1 0
   0 1 0 0
   0 0 0 1
 ]
 
-function op(::OpName"√SWAP", ::SiteType"S=1/2")
+function ITensors.op(::OpName"√SWAP", ::SiteType"S=1/2")
   return [
     1 0 0 0
     0 (1 + im)/2 (1 - im)/2 0
@@ -175,7 +175,7 @@ function op(::OpName"√SWAP", ::SiteType"S=1/2")
 end
 
 # Ising (XX) coupling gate
-function op(::OpName"XX", ::SiteType"S=1/2"; ϕ::Number)
+function ITensors.op(::OpName"XX", ::SiteType"S=1/2"; ϕ::Number)
   return [
     cos(ϕ) 0 0 -im*sin(ϕ)
     0 cos(ϕ) -im*sin(ϕ) 0
@@ -185,7 +185,7 @@ function op(::OpName"XX", ::SiteType"S=1/2"; ϕ::Number)
 end
 
 # Ising (YY) coupling gate
-function op(::OpName"YY", ::SiteType"S=1/2"; ϕ::Number)
+function ITensors.op(::OpName"YY", ::SiteType"S=1/2"; ϕ::Number)
   return [
     cos(ϕ) 0 0 im*sin(ϕ)
     0 cos(ϕ) -im*sin(ϕ) 0
@@ -195,7 +195,7 @@ function op(::OpName"YY", ::SiteType"S=1/2"; ϕ::Number)
 end
 
 # Ising (ZZ) coupling gate
-function op(::OpName"ZZ", ::SiteType"S=1/2"; ϕ::Number)
+function ITensors.op(::OpName"ZZ", ::SiteType"S=1/2"; ϕ::Number)
   return [
     exp(-im * ϕ) 0 0 0
     0 exp(im * ϕ) 0 0
@@ -208,7 +208,7 @@ end
 # 3-Qubit gates
 #
 
-function op(::OpName"Toffoli", ::SiteType"S=1/2")
+function ITensors.op(::OpName"Toffoli", ::SiteType"S=1/2")
   return [
     1 0 0 0 0 0 0 0
     0 1 0 0 0 0 0 0
@@ -221,13 +221,13 @@ function op(::OpName"Toffoli", ::SiteType"S=1/2")
   ]
 end
 
-op(::OpName"CCNOT", t::SiteType"S=1/2") = op("Toffoli", t)
+ITensors.op(::OpName"CCNOT", t::SiteType"S=1/2") = op("Toffoli", t)
 
-op(::OpName"CCX", t::SiteType"S=1/2") = op("Toffoli", t)
+ITensors.op(::OpName"CCX", t::SiteType"S=1/2") = op("Toffoli", t)
 
-op(::OpName"TOFF", t::SiteType"S=1/2") = op("Toffoli", t)
+ITensors.op(::OpName"TOFF", t::SiteType"S=1/2") = op("Toffoli", t)
 
-function op(::OpName"Fredkin", ::SiteType"S=1/2")
+function ITensors.op(::OpName"Fredkin", ::SiteType"S=1/2")
   return [
     1 0 0 0 0 0 0 0
     0 1 0 0 0 0 0 0
@@ -240,15 +240,15 @@ function op(::OpName"Fredkin", ::SiteType"S=1/2")
   ]
 end
 
-op(::OpName"CSWAP", t::SiteType"S=1/2") = op("Fredkin", t)
+ITensors.op(::OpName"CSWAP", t::SiteType"S=1/2") = op("Fredkin", t)
 
-op(::OpName"CS", t::SiteType"S=1/2") = op("Fredkin", t)
+ITensors.op(::OpName"CS", t::SiteType"S=1/2") = op("Fredkin", t)
 
 #
 # 4-Qubit gates
 #
 
-function op(::OpName"CCCNOT", ::SiteType"S=1/2")
+function ITensors.op(::OpName"CCCNOT", ::SiteType"S=1/2")
   return [
     1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
     0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0
--- src/physics/site_types/spinhalf.jl
+++ src/physics/site_types/spinhalf.jl
@@ -11,7 +11,7 @@ Create the Hilbert space for a site of type "S=1/2".
 
 Optionally specify the conserved symmetries and their quantum number labels.
 """
-function space(
+function ITensors.space(
   ::SiteType"S=1/2";
   conserve_qns=false,
   conserve_sz=conserve_qns,
@@ -32,116 +32,116 @@ function space(
   return 2
 end
 
-val(::ValName"Up", ::SiteType"S=1/2") = 1
-val(::ValName"Dn", ::SiteType"S=1/2") = 2
+ITensors.val(::ValName"Up", ::SiteType"S=1/2") = 1
+ITensors.val(::ValName"Dn", ::SiteType"S=1/2") = 2
 
-val(::ValName"↑", st::SiteType"S=1/2") = 1
-val(::ValName"↓", st::SiteType"S=1/2") = 2
+ITensors.val(::ValName"↑", st::SiteType"S=1/2") = 1
+ITensors.val(::ValName"↓", st::SiteType"S=1/2") = 2
 
-val(::ValName"Z+", ::SiteType"S=1/2") = 1
-val(::ValName"Z-", ::SiteType"S=1/2") = 2
+ITensors.val(::ValName"Z+", ::SiteType"S=1/2") = 1
+ITensors.val(::ValName"Z-", ::SiteType"S=1/2") = 2
 
-state(::StateName"Up", ::SiteType"S=1/2") = [1.0, 0.0]
-state(::StateName"Dn", ::SiteType"S=1/2") = [0.0, 1.0]
+ITensors.state(::StateName"Up", ::SiteType"S=1/2") = [1.0, 0.0]
+ITensors.state(::StateName"Dn", ::SiteType"S=1/2") = [0.0, 1.0]
 
-state(::StateName"↑", st::SiteType"S=1/2") = [1.0, 0.0]
-state(::StateName"↓", st::SiteType"S=1/2") = [0.0, 1.0]
+ITensors.state(::StateName"↑", st::SiteType"S=1/2") = [1.0, 0.0]
+ITensors.state(::StateName"↓", st::SiteType"S=1/2") = [0.0, 1.0]
 
-state(::StateName"Z+", st::SiteType"S=1/2") = [1.0, 0.0]
-state(::StateName"Z-", st::SiteType"S=1/2") = [0.0, 1.0]
+ITensors.state(::StateName"Z+", st::SiteType"S=1/2") = [1.0, 0.0]
+ITensors.state(::StateName"Z-", st::SiteType"S=1/2") = [0.0, 1.0]
 
-state(::StateName"X+", st::SiteType"S=1/2") = [1 / sqrt(2), 1 / sqrt(2)]
-state(::StateName"X-", st::SiteType"S=1/2") = [1 / sqrt(2), -1 / sqrt(2)]
+ITensors.state(::StateName"X+", st::SiteType"S=1/2") = [1 / sqrt(2), 1 / sqrt(2)]
+ITensors.state(::StateName"X-", st::SiteType"S=1/2") = [1 / sqrt(2), -1 / sqrt(2)]
 
-state(::StateName"Y+", st::SiteType"S=1/2") = [1 / sqrt(2), im / sqrt(2)]
-state(::StateName"Y-", st::SiteType"S=1/2") = [1 / sqrt(2), -im / sqrt(2)]
+ITensors.state(::StateName"Y+", st::SiteType"S=1/2") = [1 / sqrt(2), im / sqrt(2)]
+ITensors.state(::StateName"Y-", st::SiteType"S=1/2") = [1 / sqrt(2), -im / sqrt(2)]
 
-op(::OpName"Z", ::SiteType"S=1/2") = [
+ITensors.op(::OpName"Z", ::SiteType"S=1/2") = [
   1 0
   0 -1
 ]
 
-op(::OpName"Sz", ::SiteType"S=1/2") = [
+ITensors.op(::OpName"Sz", ::SiteType"S=1/2") = [
   0.5 0.0
   0.0 -0.5
 ]
 
-op(::OpName"Sᶻ", t::SiteType"S=1/2") = op(OpName("Sz"), t)
+ITensors.op(::OpName"Sᶻ", t::SiteType"S=1/2") = op(OpName("Sz"), t)
 
-op(::OpName"S+", ::SiteType"S=1/2") = [
+ITensors.op(::OpName"S+", ::SiteType"S=1/2") = [
   0 1
   0 0
 ]
 
-op(::OpName"S⁺", t::SiteType"S=1/2") = op(OpName("S+"), t)
+ITensors.op(::OpName"S⁺", t::SiteType"S=1/2") = op(OpName("S+"), t)
 
-op(::OpName"Splus", t::SiteType"S=1/2") = op(OpName("S+"), t)
+ITensors.op(::OpName"Splus", t::SiteType"S=1/2") = op(OpName("S+"), t)
 
-op(::OpName"S-", ::SiteType"S=1/2") = [
+ITensors.op(::OpName"S-", ::SiteType"S=1/2") = [
   0 0
   1 0
 ]
 
-op(::OpName"S⁻", t::SiteType"S=1/2") = op(OpName("S-"), t)
+ITensors.op(::OpName"S⁻", t::SiteType"S=1/2") = op(OpName("S-"), t)
 
-op(::OpName"Sminus", t::SiteType"S=1/2") = op(OpName("S-"), t)
+ITensors.op(::OpName"Sminus", t::SiteType"S=1/2") = op(OpName("S-"), t)
 
-op(::OpName"X", ::SiteType"S=1/2") = [
+ITensors.op(::OpName"X", ::SiteType"S=1/2") = [
   0 1
   1 0
 ]
 
-op(::OpName"Sx", ::SiteType"S=1/2") = [
+ITensors.op(::OpName"Sx", ::SiteType"S=1/2") = [
   0.0 0.5
   0.5 0.0
 ]
 
-op(::OpName"Sˣ", t::SiteType"S=1/2") = op(OpName("Sx"), t)
+ITensors.op(::OpName"Sˣ", t::SiteType"S=1/2") = op(OpName("Sx"), t)
 
-op(::OpName"iY", ::SiteType"S=1/2") = [
+ITensors.op(::OpName"iY", ::SiteType"S=1/2") = [
   0 1
   -1 0
 ]
 
-op(::OpName"iSy", ::SiteType"S=1/2") = [
+ITensors.op(::OpName"iSy", ::SiteType"S=1/2") = [
   0.0 0.5
   -0.5 0.0
 ]
 
-op(::OpName"iSʸ", t::SiteType"S=1/2") = op(OpName("iSy"), t)
+ITensors.op(::OpName"iSʸ", t::SiteType"S=1/2") = op(OpName("iSy"), t)
 
-op(::OpName"Y", ::SiteType"S=1/2") = [
+ITensors.op(::OpName"Y", ::SiteType"S=1/2") = [
   0.0 -1.0im
   1.0im 0.0
 ]
 
-op(::OpName"Sy", ::SiteType"S=1/2") = [
+ITensors.op(::OpName"Sy", ::SiteType"S=1/2") = [
   0.0 -0.5im
   0.5im 0.0
 ]
 
-op(::OpName"Sʸ", t::SiteType"S=1/2") = op(OpName("Sy"), t)
+ITensors.op(::OpName"Sʸ", t::SiteType"S=1/2") = op(OpName("Sy"), t)
 
-op(::OpName"S2", ::SiteType"S=1/2") = [
+ITensors.op(::OpName"S2", ::SiteType"S=1/2") = [
   0.75 0.0
   0.0 0.75
 ]
 
-op(::OpName"S²", t::SiteType"S=1/2") = op(OpName("S2"), t)
+ITensors.op(::OpName"S²", t::SiteType"S=1/2") = op(OpName("S2"), t)
 
-op(::OpName"ProjUp", ::SiteType"S=1/2") = [
+ITensors.op(::OpName"ProjUp", ::SiteType"S=1/2") = [
   1 0
   0 0
 ]
 
-op(::OpName"projUp", t::SiteType"S=1/2") = op(OpName("ProjUp"), t)
+ITensors.op(::OpName"projUp", t::SiteType"S=1/2") = op(OpName("ProjUp"), t)
 
-op(::OpName"ProjDn", ::SiteType"S=1/2") = [
+ITensors.op(::OpName"ProjDn", ::SiteType"S=1/2") = [
   0 0
   0 1
 ]
 
-op(::OpName"projDn", t::SiteType"S=1/2") = op(OpName("ProjDn"), t)
+ITensors.op(::OpName"projDn", t::SiteType"S=1/2") = op(OpName("ProjDn"), t)
 
 # Support the tag "SpinHalf" as equivalent to "S=1/2"
 
@@ -149,9 +149,9 @@ space(::SiteType"SpinHalf"; kwargs...) = space(SiteType("S=1/2"); kwargs...)
 
 val(name::ValName, ::SiteType"SpinHalf") = val(name, SiteType("S=1/2"))
 
-state(name::StateName, ::SiteType"SpinHalf") = state(name, SiteType("S=1/2"))
+ITensors.state(name::StateName, ::SiteType"SpinHalf") = state(name, SiteType("S=1/2"))
 
-op(o::OpName, ::SiteType"SpinHalf"; kwargs...) = op(o, SiteType("S=1/2"); kwargs...)
+ITensors.op(o::OpName, ::SiteType"SpinHalf"; kwargs...) = op(o, SiteType("S=1/2"); kwargs...)
 
 # Support the tag "S=½" as equivalent to "S=1/2"
 
@@ -159,6 +159,6 @@ space(::SiteType"S=½"; kwargs...) = space(SiteType("S=1/2"); kwargs...)
 
 val(name::ValName, ::SiteType"S=½") = val(name, SiteType("S=1/2"))
 
-state(name::StateName, ::SiteType"S=½") = state(name, SiteType("S=1/2"))
+ITensors.state(name::StateName, ::SiteType"S=½") = state(name, SiteType("S=1/2"))
 
-op(o::OpName, ::SiteType"S=½"; kwargs...) = op(o, SiteType("S=1/2"); kwargs...)
+ITensors.op(o::OpName, ::SiteType"S=½"; kwargs...) = op(o, SiteType("S=1/2"); kwargs...)
