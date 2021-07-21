a623f32fc53539127b40ef91df1ef81bb072f954
mtfishman <mfishman@flatironinstitute.org>
2021-07-21 14:05:43 -0400

    Fix but in state function when overloading version that accepts an Index input

--- src/physics/site_types/qudit.jl
+++ src/physics/site_types/qudit.jl
@@ -1,6 +1,10 @@
 
 ITensors.space(::SiteType"Qudit"; dim=2) = dim
 
-function ITensors.op(::OpName"A",
-                     
+function ITensors.state(::StateName{N}, ::SiteType"Qudit", s::Index) where {N}
+  n = parse(Int, String(N))
+  st = zeros(dim(s))
+  st[n + 1] = 1.0
+  return st
+end
 
