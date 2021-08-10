7d64df775aaf2ee57cdd94e6bc4a58a42e203fbd
Miles Stoudenmire <miles.stoudenmire@gmail.com>
2021-08-10 19:09:05 -0400

    Fixed bug involving missing default case for state function

--- test/sitetype.jl
+++ test/sitetype.jl
@@ -358,6 +358,13 @@ using ITensors, Test
     @test v2[s => 3] == 1
     @test_throws BoundsError state(s, "3")
   end
+
+  @testset "Regression test for state overload" begin
+    ITensors.space(::SiteType"Xev") = 8
+    ITensors.state(::StateName"0", ::SiteType"Xev") = [1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+    s = siteind("Xev")
+    @test state(s,"0") ≈ ITensor([1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],s)
+  end
 end
 
 nothing
