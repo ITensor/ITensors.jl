22cf5b64967d21ab1acf7e755832c5176ce072da
Matt Fishman <mtfishman@users.noreply.github.com>
2021-09-22 17:39:02 -0400

    Fix threaded contraction, add CI tests for threading (#746)
    * Fix threaded contraction, add CI tests for threading

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,56 +1,65 @@
 using Test
 
-@testset "ITensors.jl" begin
-  @testset "$filename" for filename in [
-    "NDTensors/runtests.jl",
-    "ContractionSequenceOptimization/runtests.jl",
-    "tagset.jl",
-    "smallstring.jl",
-    "symmetrystyle.jl",
-    "index.jl",
-    "indexset.jl",
-    "ndtensors.jl",
-    "not.jl",
-    "inference.jl",
-    "itensor_scalar.jl",
-    "itensor.jl",
-    "indices.jl",
-    "itensor_slice.jl",
-    "itensor_scalar_contract.jl",
-    "itensor_combine_contract.jl",
-    "broadcast.jl",
-    "emptyitensor.jl",
-    "diagitensor.jl",
-    "contract.jl",
-    "combiner.jl",
-    "debug_checks.jl",
-    "trg.jl",
-    "ctmrg.jl",
-    "iterativesolvers.jl",
-    "dmrg.jl",
-    "sitetype.jl",
-    "phys_site_types.jl",
-    "decomp.jl",
-    "lattices.jl",
-    "mps.jl",
-    "mpo.jl",
-    "sweeps.jl",
-    "sweepnext.jl",
-    "autompo.jl",
-    "svd.jl",
-    "qn.jl",
-    "qnindex.jl",
-    "qnitensor.jl",
-    "qncombiner.jl",
-    "qndiagitensor.jl",
-    "fermions.jl",
-    "empty.jl",
-    "qnmpo.jl",
-    "readwrite.jl",
-    "readme.jl",
-    "examples.jl",
-  ]
-    println("Running $filename")
-    include(filename)
+if Threads.nthreads() == 1
+  @testset "ITensors.jl" begin
+    @testset "$filename" for filename in [
+      "NDTensors/runtests.jl",
+      "ContractionSequenceOptimization/runtests.jl",
+      "tagset.jl",
+      "smallstring.jl",
+      "symmetrystyle.jl",
+      "index.jl",
+      "indexset.jl",
+      "ndtensors.jl",
+      "not.jl",
+      "inference.jl",
+      "itensor_scalar.jl",
+      "itensor.jl",
+      "indices.jl",
+      "itensor_slice.jl",
+      "itensor_scalar_contract.jl",
+      "itensor_combine_contract.jl",
+      "broadcast.jl",
+      "emptyitensor.jl",
+      "diagitensor.jl",
+      "contract.jl",
+      "combiner.jl",
+      "debug_checks.jl",
+      "trg.jl",
+      "ctmrg.jl",
+      "iterativesolvers.jl",
+      "dmrg.jl",
+      "sitetype.jl",
+      "phys_site_types.jl",
+      "decomp.jl",
+      "lattices.jl",
+      "mps.jl",
+      "mpo.jl",
+      "sweeps.jl",
+      "sweepnext.jl",
+      "autompo.jl",
+      "svd.jl",
+      "qn.jl",
+      "qnindex.jl",
+      "qnitensor.jl",
+      "qncombiner.jl",
+      "qndiagitensor.jl",
+      "fermions.jl",
+      "empty.jl",
+      "qnmpo.jl",
+      "readwrite.jl",
+      "readme.jl",
+      "examples.jl",
+    ]
+      println("Running $filename")
+      include(filename)
+    end
+  end
+elseif Threads.nthreads() > 1
+  @testset "ITensors.jl threaded" begin
+    @testset "$filename" for filename in ["threading.jl"]
+      println("Running $filename")
+      include(filename)
+    end
   end
 end
---
+++ test/threading.jl
@@ -0,0 +1,70 @@
+using Compat
+using ITensors
+using Test
+using LinearAlgebra
+
+@testset "Threading" begin
+  blas_num_threads = Compat.get_num_threads()
+  strided_num_threads = ITensors.NDTensors.Strided.get_num_threads()
+
+  BLAS.set_num_threads(1)
+  ITensors.NDTensors.Strided.set_num_threads(1)
+
+  @testset "Threaded contraction" begin
+    i = Index([QN(0) => 500, QN(1) => 500])
+    A = randomITensor(i', dag(i))
+
+    using_threaded_blocksparse = ITensors.disable_threaded_blocksparse()
+
+    R = A' * A
+
+    ITensors.enable_threaded_blocksparse()
+
+    Rthreaded = A' * A
+
+    @test R ≈ Rthreaded
+
+    ITensors.disable_threaded_blocksparse()
+
+    time = @elapsed B = A' * A
+
+    ITensors.enable_threaded_blocksparse()
+
+    time_threaded = @elapsed B = A' * A
+
+    @test time > time_threaded
+
+    if using_threaded_blocksparse
+      ITensors.enable_threaded_blocksparse()
+    else
+      ITensors.disable_threaded_blocksparse()
+    end
+  end
+
+  @testset "Contraction resulting in no blocks with threading bug" begin
+    i = Index([QN(0) => 1, QN(1) => 1])
+    A = emptyITensor(i', dag(i))
+    B = emptyITensor(i', dag(i))
+    A[i' => 1, i => 1] = 11.0
+    B[i' => 2, i => 2] = 22.0
+
+    using_threaded_blocksparse = ITensors.disable_threaded_blocksparse()
+    C1 = A' * B
+    ITensors.enable_threaded_blocksparse()
+    C2 = A' * B
+    if using_threaded_blocksparse
+      ITensors.enable_threaded_blocksparse()
+    else
+      ITensors.disable_threaded_blocksparse()
+    end
+
+    @test nnzblocks(C1) == 0
+    @test nnzblocks(C2) == 0
+    @test nnz(C1) == 0
+    @test nnz(C2) == 0
+    @test C1 ≈ C2
+  end
+
+  BLAS.set_num_threads(blas_num_threads)
+  ITensors.NDTensors.Strided.set_num_threads(strided_num_threads)
+end
