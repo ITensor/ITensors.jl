5be5c6914de7e5d45422f3b56d9cdbcb37cdb53b
Matthew Fishman <mfishman@flatironinstitute.org>
2021-11-10 18:36:15 -0500

    Add example for making an MPO density matrix from a sum of MPS outer products

---
+++ examples/mps_mpo_algebra/mps_density_matrix.jl
@@ -0,0 +1,14 @@
+using ITensors
+
+N = 4
+nmps = 3
+cutoff = 1e-8
+s = siteinds("S=1/2", N)
+ψs = [randomMPS(s; linkdims=2) for _ in 1:nmps]
+ρs = [outer(ψ, ψ; cutoff=cutoff) for ψ in ψs]
+ρ = sum(ρs; cutoff=cutoff)
+
+ψs_full = prod.(ψs)
+ρs_full = [ψ'dag(ψ) for ψ in ψs_full]
+ρ_full = sum(ρs_full)
+@show norm(ρ_full - prod(ρ))
