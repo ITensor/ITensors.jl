58abe4ae9e7853f0cbd8238a954d267c9419f33d
Matt Fishman <mtfishman@users.noreply.github.com>
2022-02-28 00:24:57 -0500

    Format test/qnitensor.jl
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- test/qnitensor.jl
+++ test/qnitensor.jl
@@ -1701,7 +1701,7 @@ Random.seed!(1234)
     @testset "diag" for ElType in (Float64, ComplexF64)
       χ = [QN(0) => 1, QN(1) => 2]
       i, j = Index.((χ,), ("i", "j"))
-      A = randomITensor(ElType, i, j) 
+      A = randomITensor(ElType, i, j)
       _, S, _ = svd(A, i)
       d = diag(S)
       @test d isa DenseTensor{real(ElType),1}
