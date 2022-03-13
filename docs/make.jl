0778ccd0852788b0d1d17d7ee7f30fbdb2eff856
Miles <miles.stoudenmire@gmail.com>
2022-03-13 17:19:27 -0400

    Pass object instead of type to deploydocs [no ci]

--- docs/make.jl
+++ docs/make.jl
@@ -2,4 +2,4 @@ include("settings.jl")
 
 makedocs(; sitename=sitename, settings...)
 
-deploydocs(; repo="github.com/ITensor/ITensors.jl.git", devbranch="main", push_preview=true, deploy_config=Documenter.GitHubActions)
+deploydocs(; repo="github.com/ITensor/ITensors.jl.git", devbranch="main", push_preview=true, deploy_config=Documenter.GitHubActions())
