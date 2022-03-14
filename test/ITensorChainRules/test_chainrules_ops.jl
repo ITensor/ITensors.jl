c6ea3d724a69364b92f1c97cd8d3c6af6dce2363
Matt Fishman <mtfishman@users.noreply.github.com>
2022-03-14 12:44:40 -0400

    Update test/ITensorChainRules/test_chainrules_ops.jl
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- test/ITensorChainRules/test_chainrules_ops.jl
+++ test/ITensorChainRules/test_chainrules_ops.jl
@@ -33,7 +33,15 @@ using Zygote: ZygoteRuleConfig, gradient
     return y[1, 1]
   end
   args = (x,)
-  test_rrule(ZygoteRuleConfig(), f, args...; rrule_f=rrule_via_ad, check_inferred=false, rtol = 1.0e-7, atol = 1.0e-7)
+  test_rrule(
+    ZygoteRuleConfig(),
+    f,
+    args...;
+    rrule_f=rrule_via_ad,
+    check_inferred=false,
+    rtol=1.0e-7,
+    atol=1.0e-7,
+  )
 
   f = function (x)
     y = Op("Ry", 1; θ=x) + Op("Ry", 1; θ=x)
