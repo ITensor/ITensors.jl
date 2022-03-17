685525fe2084a8407d2f68eb9df5b1bd5c73d97b
Matthias Thamm <50720616+mthamm@users.noreply.github.com>
2022-03-17 21:52:08 +0100

    [ITensorGPU] [BUG] Fix bugs in complex SVD on GPU (with and without truncations) (#871)

--- ITensorGPU/test/test_cutruncate.jl
+++ ITensorGPU/test/test_cutruncate.jl
@@ -8,13 +8,21 @@ using ITensors,
 @testset "cutrunctate" begin
   @test ITensorGPU.truncate!(CUDA.zeros(Float64, 10)) == (0.0, 0.0, CUDA.zeros(Float64, 1))
   trunc = ITensorGPU.truncate!(
-    CuArray([1.0, 0.5, 0.1, 0.05]); absoluteCutoff=true, cutoff=0.2
+    CuArray([1.0, 0.5, 0.4, 0.1, 0.05]); absoluteCutoff=true, cutoff=0.2
   )
   @test trunc[1] ≈ 0.15
-  @test trunc[2] ≈ 0.3
-  @test trunc[3] == CuArray([1.0, 0.5])
-  trunc = ITensorGPU.truncate!(CuArray([0.5, 0.4, 0.1]); relativeCutoff=true, cutoff=0.2)
-  @test trunc[1] ≈ 0.1
-  @test trunc[2] ≈ 0.45
-  @test trunc[3] == CuArray([0.5])
+  @test trunc[2] ≈ 0.25
+  @test Array(trunc[3]) == [1.0, 0.5, 0.4]
+  trunc = ITensorGPU.truncate!(
+    CuArray([0.4, 0.26, 0.19, 0.1, 0.05]); relativeCutoff=true, cutoff=0.2
+  )
+  @test trunc[1] ≈ 0.15
+  @test trunc[2] ≈ 0.145
+  @test Array(trunc[3]) == [0.4, 0.26, 0.19]
+  trunc = ITensorGPU.truncate!(
+    CuArray([0.4, 0.26, 0.19, 0.1, 0.05] / 2); relativeCutoff=true, cutoff=0.2
+  )
+  @test trunc[1] ≈ 0.15
+  @test trunc[2] ≈ 0.145 / 2
+  @test Array(trunc[3]) == [0.4, 0.26, 0.19] / 2
 end # End truncate test
