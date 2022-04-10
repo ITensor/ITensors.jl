34f0a7032b5e0df48f7a90adf0700a0e7b07cbb8
Katharine Hyatt <kshyatt@users.noreply.github.com>
2022-04-10 18:25:26 -0400

    [ITensorGPU] [ENHANCEMENT] Allow cutruncate to work for Float32s

--- ITensorGPU/test/test_cutruncate.jl
+++ ITensorGPU/test/test_cutruncate.jl
@@ -6,23 +6,27 @@ using ITensors,
 
 # gpu tests!
 @testset "cutrunctate" begin
-  @test ITensorGPU.truncate!(CUDA.zeros(Float64, 10)) == (0.0, 0.0, CUDA.zeros(Float64, 1))
-  trunc = ITensorGPU.truncate!(
-    CuArray([1.0, 0.5, 0.4, 0.1, 0.05]); absoluteCutoff=true, cutoff=0.2
-  )
-  @test trunc[1] ≈ 0.15
-  @test trunc[2] ≈ 0.25
-  @test Array(trunc[3]) == [1.0, 0.5, 0.4]
-  trunc = ITensorGPU.truncate!(
-    CuArray([0.4, 0.26, 0.19, 0.1, 0.05]); relativeCutoff=true, cutoff=0.2
-  )
-  @test trunc[1] ≈ 0.15
-  @test trunc[2] ≈ 0.145
-  @test Array(trunc[3]) == [0.4, 0.26, 0.19]
-  trunc = ITensorGPU.truncate!(
-    CuArray([0.4, 0.26, 0.19, 0.1, 0.05] / 2); relativeCutoff=true, cutoff=0.2
-  )
-  @test trunc[1] ≈ 0.15
-  @test trunc[2] ≈ 0.145 / 2
-  @test Array(trunc[3]) == [0.4, 0.26, 0.19] / 2
+  @testset for T in (Float32, Float64)
+    @test ITensorGPU.truncate!(CUDA.zeros(T, 10)) == (zero(T), zero(T), CUDA.zeros(T, 1))
+    trunc = ITensorGPU.truncate!(
+      CuArray(T[1.0, 0.5, 0.4, 0.1, 0.05]); absoluteCutoff=true, cutoff=T(0.2)
+    )
+    @test trunc[1] ≈ T(0.15)
+    @test trunc[2] ≈ T(0.25)
+    @test Array(trunc[3]) == T[1.0, 0.5, 0.4]
+    trunc = ITensorGPU.truncate!(
+      CuArray(T[0.4, 0.26, 0.19, 0.1, 0.05]); relativeCutoff=true, cutoff=T(0.2)
+    )
+    @test trunc[1] ≈ T(0.15)
+    @test trunc[2] ≈ T(0.145)
+    @test Array(trunc[3]) == T[0.4, 0.26, 0.19]
+    trunc = ITensorGPU.truncate!(
+      CuArray(convert(Vector{T}, [0.4, 0.26, 0.19, 0.1, 0.05] / 2));
+      relativeCutoff=true,
+      cutoff=T(0.2),
+    )
+    @test trunc[1] ≈ T(0.15)
+    @test trunc[2] ≈ T(0.145 / 2)
+    @test Array(trunc[3]) == convert(Vector{T}, [0.4, 0.26, 0.19] / 2)
+  end
 end # End truncate test
