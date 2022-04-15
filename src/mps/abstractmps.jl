eaeaa2067d65052a53ed3742f43e757a74ea48c4
Matt Fishman <mtfishman@users.noreply.github.com>
2022-04-15 13:35:18 -0400

    [ITensors] [BUG] Fix for performance issues when applying gates to every other site (#900)

--- src/mps/abstractmps.jl
+++ src/mps/abstractmps.jl
@@ -2087,13 +2087,17 @@ expτH = ops(os, s)
 ```
 """
 function product(
-  As::Vector{<:ITensor}, ψ::AbstractMPS; move_sites_back::Bool=true, kwargs...
+  As::Vector{<:ITensor},
+  ψ::AbstractMPS;
+  move_sites_back_between_gates::Bool=true,
+  move_sites_back::Bool=true,
+  kwargs...,
 )
   Aψ = ψ
   for A in As
-    Aψ = product(A, Aψ; move_sites_back=false, kwargs...)
+    Aψ = product(A, Aψ; move_sites_back=move_sites_back_between_gates, kwargs...)
   end
-  if move_sites_back
+  if !move_sites_back_between_gates && move_sites_back
     s = siteinds(Aψ)
     ns = 1:length(ψ)
     ñs = [findsite(ψ, i) for i in s]
