596e41613bb69c48174eca3a37e395ada951b53b
Matthew Fishman <mfishman@flatironinstitute.org>
2022-04-17 19:59:08 -0400

    New issue templates

---
+++ .github/ISSUE_TEMPLATE/04_ITensorGaussianMPS_bug_report.md
@@ -0,0 +1,54 @@
+---
+name: ITensorGaussianMPS.jl bug report
+about: Create a bug report to help us improve ITensorGaussianMPS.jl
+title: "[ITensorGaussianMPS] [BUG]"
+labels: ["ITensorGaussianMPS", "bug"]
+assignees: ''
+
+---
+
+**Minimal code demonstrating the bug or unexpected behavior**
+
+If applicable, provide a minimal code that can be run to demonstrate the bug or unexpected behavior.
+
+If you are unable to construct a minimal code that demonstrates the bug or unexpected behavior, provide detailed steps for how to reproduce the behavior you are seeing.
+
+<details><summary>Minimal runnable code</summary><p>
+
+```julia
+[YOUR MINIMAL RUNNABLE CODE HERE]
+```
+</p></details>
+
+
+**Expected output or behavior**
+Describe what you expected to happen.
+
+If you provided a minimal code that can be run to demonstrate the bug or unexpected behavior, describe what you expected the output would be.
+
+
+**Actual output or behavior**
+Describe what actually happened.
+
+If you provided a minimal code that demonstrates the bug or unexpected behavior, provide the output you from that code. If the code leads to an error or warning, include the full error or warning below.
+
+<details><summary>Output of minimal runnable code</summary><p>
+
+```julia
+[OUTPUT OF YOUR MINIMAL RUNNABLE CODE HERE]
+```
+</p></details>
+
+
+**Version information**
+
+ - Output from `versioninfo()`:
+```julia
+julia> versioninfo()
+[YOUR OUTPUT HERE]
+```
+ - Output from `using Pkg; Pkg.status("ITensors")`:
+```julia
+julia> using Pkg; Pkg.status("ITensors")
+[YOUR OUTPUT HERE]
+```
--- .github/ISSUE_TEMPLATE/generate_issue_templates/generate_issue_templates.jl
+++ .github/ISSUE_TEMPLATE/generate_issue_templates/generate_issue_templates.jl
@@ -4,23 +4,40 @@ template_package_name = "PACKAGE"
 
 package_names = [
   "ITensors",
-  "ITensorGaussianMPS",
-  "ITensorGLMakie",
-  "ITensorGPU",
-  "ITensorMakie",
-  "ITensorUnicodePlots",
-  "ITensorVisualizationBase",
   "NDTensors",
+  "ITensorGPU",
+  "ITensorGaussianMPS",
+  "ITensorVisualizationBase",
+  "ITensorUnicodePlots",
+  "ITensorMakie",
+  "ITensorGLMakie",
 ]
 
-bug_report_file(package_name::String) = "$(package_name)_bug_report.md"
-feature_request_file(package_name::String) = "$(package_name)_feature_request.md"
+package_ordering = Dict([
+  "ITensors" => 1,
+  "NDTensors" => 2,
+  "ITensorGPU" => 3,
+  "ITensorGaussianMPS" => 4,
+  "ITensorVisualizationBase" => 5,
+  "ITensorUnicodePlots" => 6,
+  "ITensorMakie" => 7,
+  "ITensorGLMakie" => 8,
+])
+
+function bug_report_file(package_name::String)
+  return "$(package_name)_bug_report.md"
+end
+function feature_request_file(package_name::String)
+  return "$(package_name)_feature_request.md"
+end
 
 for package_name in package_names
   @show package_name
 
+  order = lpad(package_ordering[package_name], 2, "0")
+
   template_bug_report = bug_report_file(template_package_name)
-  new_bug_report = bug_report_file(package_name)
+  new_bug_report = order * "_" * bug_report_file(package_name)
 
   if isfile(new_bug_report)
     println("File $new_bug_report already exists, skipping")
@@ -33,7 +50,7 @@ for package_name in package_names
   end
 
   template_feature_request = feature_request_file(template_package_name)
-  new_feature_request = feature_request_file(package_name)
+  new_feature_request = order * "_" * feature_request_file(package_name)
 
   if isfile(new_feature_request)
     println("File $new_feature_request already exists, skipping")
