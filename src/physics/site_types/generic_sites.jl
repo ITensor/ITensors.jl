36c0ce5d5109b456a3f59c58df5984c1ae4ffba7
Matthew Fishman <mfishman@flatironinstitute.org>
2022-04-20 14:03:18 -0400

    [ITensors] [ENHANCMENT] Haar random unitary gate

--- src/physics/site_types/generic_sites.jl
+++ src/physics/site_types/generic_sites.jl
@@ -1,11 +1,38 @@
-function op!(Op::ITensor, ::OpName"Id", ::SiteType"Generic", s::Index)
-  for n in 1:dim(s)
-    Op[n, n] = 1.0
-  end
+function op(::OpName"Id", ::SiteType"Generic", s1::Index, sn::Index...; eltype=Float64)
+  s = (s1, sn...)
+  n = prod(dim.(s))
+  return itensor(Matrix(one(eltype) * I, n, n), prime.(s)..., dag.(s)...)
 end
 
-op(::OpName"I", t::SiteType"Generic", s::Index) = op("Id", s)
+op(::OpName"I", st::SiteType"Generic", s::Index...; kwargs...) = op(OpName("Id"), st, s...; kwargs...)
 
-function op!(Op::ITensor, ::OpName"F", st::SiteType"Generic", s::Index)
-  return op!(Op, OpName("Id"), st, s)
+function op(::OpName"F", st::SiteType"Generic", s::Index; kwargs...)
+  return op(OpName("Id"), st, s; kwargs...)
+end
+
+function default_random_matrix(eltype::Type, s::Index...)
+  n = prod(dim.(s))
+  return randn(eltype, n, n)
+end
+
+# Haar-random unitary
+#
+# Reference:
+# Section 4.6
+# http://math.mit.edu/~edelman/publications/random_matrix_theory.pdf
+function op(
+  ::OpName"RandomUnitary",
+  ::SiteType"Generic",
+  s1::Index,
+  sn::Index...;
+  eltype=ComplexF64,
+  random_matrix=default_random_matrix(eltype, s1, sn...),
+)
+  s = (s1, sn...)
+  Q, _ = NDTensors.qr_positive(random_matrix)
+  return itensor(Q, prime.(s)..., dag.(s)...)
+end
+
+function op(::OpName"randU", st::SiteType"Generic", s::Index...; kwargs...)
+  return op(OpName("RandomUnitary"), st, s...; kwargs...)
 end
