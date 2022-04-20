9cb594d667ea6507488c1ca620f05506b697032e
Matthew Fishman <mfishman@flatironinstitute.org>
2022-04-20 15:00:07 -0400

    Change to in-place op overloads for generic op types

--- src/physics/site_types/generic_sites.jl
+++ src/physics/site_types/generic_sites.jl
@@ -1,15 +1,16 @@
-function op(::OpName"Id", ::SiteType"Generic", s1::Index, sn::Index...; eltype=Float64)
+function op!(o::ITensors, ::OpName"Id", ::SiteType"Generic", s1::Index, sn::Index...; eltype=Float64)
   s = (s1, sn...)
   n = prod(dim.(s))
-  return itensor(Matrix(one(eltype) * I, n, n), prime.(s)..., dag.(s)...)
+  t = itensor(Matrix(one(eltype) * I, n, n), prime.(s)..., dag.(s)...)
+  return settensor!(o, tensor(t))
 end
 
-function op(::OpName"I", st::SiteType"Generic", s::Index...; kwargs...)
-  return op(OpName("Id"), st, s...; kwargs...)
+function op!(o::ITensor, ::OpName"I", st::SiteType"Generic", s::Index...; kwargs...)
+  return op!(o, OpName("Id"), st, s...; kwargs...)
 end
 
-function op(::OpName"F", st::SiteType"Generic", s::Index; kwargs...)
-  return op(OpName("Id"), st, s; kwargs...)
+function op!(o::ITensor, ::OpName"F", st::SiteType"Generic", s::Index; kwargs...)
+  return op!(o, OpName("Id"), st, s; kwargs...)
 end
 
 function default_random_matrix(eltype::Type, s::Index...)
@@ -22,7 +23,8 @@ end
 # Reference:
 # Section 4.6
 # http://math.mit.edu/~edelman/publications/random_matrix_theory.pdf
-function op(
+function op!(
+  o::ITensor,
   ::OpName"RandomUnitary",
   ::SiteType"Generic",
   s1::Index,
@@ -32,9 +34,10 @@ function op(
 )
   s = (s1, sn...)
   Q, _ = NDTensors.qr_positive(random_matrix)
-  return itensor(Q, prime.(s)..., dag.(s)...)
+  t = itensor(Q, prime.(s)..., dag.(s)...)
+  return settensor!(o, tensor(t))
 end
 
-function op(::OpName"randU", st::SiteType"Generic", s::Index...; kwargs...)
-  return op(OpName("RandomUnitary"), st, s...; kwargs...)
+function op!(o::ITensor, ::OpName"randU", st::SiteType"Generic", s::Index...; kwargs...)
+  return op!(o, OpName("RandomUnitary"), st, s...; kwargs...)
 end
