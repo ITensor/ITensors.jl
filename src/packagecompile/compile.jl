1e95dc8168b35e550805961fd9152cc35e03fe4c
Jan Schneider <j-t-s@outlook.de>
2022-05-05 18:43:28 +0200

    stash changes as precompilation doesnt work

--- src/packagecompile/compile.jl
+++ src/packagecompile/compile.jl
@@ -5,6 +5,14 @@ default_compile_filename() = "sys_itensors.so"
 
 default_compile_path() = joinpath(default_compile_dir(), default_compile_filename())
 
+# SOURCE: https://github.com/JuliaLang/PackageCompiler.jl/blob/adbf1df36aadc245f95d1282dda02d8bbeccb32f/src/PackageCompiler.jl#L166
+function get_julia_cmd()
+  julia_path = joinpath(Sys.BINDIR, Base.julia_exename())
+  color = Base.have_color === nothing ? "auto" : Base.have_color ? "yes" : "no"
+  return `$julia_path --color=$color --startup-file=no`
+end
+
+
 function compile_note(; dir=default_compile_dir(), filename=default_compile_filename())
   path = joinpath(dir, filename)
   return """
@@ -30,6 +38,7 @@ end
 function compile(;
   dir::AbstractString=default_compile_dir(),
   filename::AbstractString=default_compile_filename(),
+  kwargs...
 )
   if !isdir(dir)
     println("""The directory "$dir" doesn't exist yet, creating it now.""")
@@ -40,10 +49,71 @@ function compile(;
   println(
     """Creating the system image "$path" containing the compiled version of ITensors. This may take a few minutes.""",
   )
+
+  include_MKL::Bool = get(kwargs,:include_MKL,false)
+  num_threads::Int = get(kwargs,:num_threads,1)
+  blocksparse_multithreading::Bool = get(kwargs,:blocksparse_multithreading,false)
+  contraction_sequence_optimization::Bool = get(kwargs,:contraction_sequence_optimization,true)
+
+
+
+  project = dirname(Base.active_project())
+
+  # define tracefile that contains the objects the PackageCompiler has to compile
+  tracefile = joinpath(@__DIR__, "tracefile.jl")
+  # delete any preexisting tracefile
+  isfile(tracefile) && rm(tracefile)
+
+  # Define script from which a julia process will watch which objects are to be compiled in a new system image
+  # key feature here is that we control the process itself that generates the tracefile,
+  # which allows us to let the process be multithreaded (or not)
+  precompile_file = joinpath(@__DIR__, "script_to_watch.jl")
+
+  # write the dynamically generated precompilation file to disk
+  # reflecting the user choice of multithreading enabled/disabled and or MKL, etc.
+  # First convert Expr object of script_to_watch() to a string that can be written to disk
+  open(precompile_file, "w") do fout
+    write(fout,string(script_to_watch(;
+                      include_MKL=include_MKL,
+                      num_threads=num_threads,
+                      blocksparse_multithreading=blocksparse_multithreading,
+                      contraction_sequence_optimization=contraction_sequence_optimization,
+                      )
+                    )
+    )
+  end
+
+
+
+  # Cruical advantage here: controlling the process that produces the tracefile and possibly enable multithreading
+  # adaption from https://github.com/JuliaLang/PackageCompiler.jl/ 
+  if num_threads > 1
+    precompile_cmd = `$(get_julia_cmd()) -t $(num_threads) --compile=all --trace-compile=$tracefile $(precompile_file)`
+    build_args = `$(get(kwargs, :Cmd, ``)) -t $(num_threads)`
+  else
+    precompile_cmd = `$(get_julia_cmd()) --compile=all --trace-compile=$tracefile $(precompile_file)`
+    build_args = get(kwargs, :Cmd,``)
+  end
+
+  splitter = Sys.iswindows() ? ';' : ':'
+
+  precompile_cmd = addenv(precompile_cmd, "JULIA_LOAD_PATH" => "$project$(splitter)@stdlib")
+
+  run(precompile_cmd)
+
+
+  package_list = include_MKL == true ? [:ITensors, :MKL] : [:ITensors]
+
+  
+
+
+
+
   create_sysimage(
-    :ITensors;
+    package_list;
     sysimage_path=path,
     precompile_execution_file=joinpath(@__DIR__, "precompile_itensors.jl"),
+    sysimage_build_args=build_args,
   )
   println(compile_note(; dir=dir, filename=filename))
   return path
@@ -51,11 +121,75 @@ end
 
 @doc """
     ITensors.compile(; dir = "$(default_compile_dir())",
-                       filename = "$(default_compile_filename())")
+                       filename = "$(default_compile_filename())",
+                       build_args::Cmd=``
+                       )
 
 Compile ITensors.jl with [PackageCompiler](https://julialang.github.io/PackageCompiler.jl/dev/). This will take some time, perhaps a few minutes.
 
 This will create a system image containing the compiled version of ITensors located at `dir/filename`, by default `$(default_compile_path())`.
 
+ - `sysimage_build_args::Cmd`: A set of command line options that is used in the Julia process building the sysimage, for example `-O1 -t 4 `.
+ Consider using multithreading argument `-t <nthreads>` especially when using multithreading with BlockSparse ITensors.
+
 $(compile_note())
 """ compile
+
+# this function is needed as we dynamically need to adapt the execution file
+# to reflect the multithreading features (or not)
+# script_to_watch() returns an `Expr` object
+function script_to_watch(;include_MKL::Bool=false, num_threads::Int=1, blocksparse_multithreading::Bool=false, contraction_sequence_optimization::Bool=true)
+
+  quote
+    using ITensors, LinearAlgebra
+    $(include_MKL)==true && using MKL
+
+    # check if num_threads is a positive integer
+    if $(num_threads) <=0
+      throw(ArgumentError("num_threads must be positive integer but is $(num_threads)!"))
+    end
+
+    # consider two cases for multithreading:
+    # 1. BlockSparse multithreading with QN conservation
+    if $(num_threads) > 1 && blocksparse_multithreading
+      BLAS.set_num_threads(1)
+      ITensors.Strided.set_num_threads(1)
+      ITensors.enable_threaded_blocksparse()
+    # 2. BLAS multithreading
+    elseif $(num_threads) > 1 && !blocksparse_multithreading
+      BLAS.set_num_threads(num_threads)
+      ITensors.Strided.set_num_threads(1)
+      ITensors.disable_threaded_blocksparse()
+    end
+    
+    contraction_sequence_optimization && ITensors.enable_contraction_sequence_optimization()
+
+    # what follows is the basis script to be watched for compilation
+    N = 6
+    sweeps = Sweeps(3)
+    maxdim!(sweeps, 10)
+    cutoff!(sweeps, 1E-13)
+
+    ampo = OpSum()
+    for j in 1:(N - 1)
+      ampo .+= "Sz", j, "Sz", j + 1
+      ampo .+= 0.5, "S+", j, "S-", j + 1
+      ampo .+= 0.5, "S-", j, "S+", j + 1
+    end
+
+    sites = siteinds("S=1", N)
+    H = MPO(ampo, sites)
+    psi0 = randomMPS(sites; linkdims=2)
+    dmrg(H, psi0, sweeps)
+
+    sites_qn = siteinds("S=1", N; conserve_qns=true)
+    if !hasqns(sites_qn[1])
+      throw(ErrorException("Index does not have QNs in part of precompile script"))
+    end
+    H_qn = MPO(ampo, sites_qn)
+    psi0_qn = randomMPS(sites_qn, [isodd(n) ? "Up" : "Dn" for n in 1:N]; linkdims=2)
+    dmrg(H_qn, psi0_qn, sweeps)
+  end
+
+end
+
