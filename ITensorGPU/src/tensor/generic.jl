b0bad4023b5f3b88162823fe580a6b5a1615ff85
Matthew Fishman <mfishman@flatironinstitute.org>
2022-07-14 22:09:09 -0400

    Simplify cpu adapt wrapper a bit

--- ITensorGPU/src/tensor/generic.jl
+++ ITensorGPU/src/tensor/generic.jl
@@ -1,5 +1,6 @@
-cpu(x::Vector) = x
-cpu(x::CuVector) = adapt(Vector{eltype(x)}, x)
+# TODO: Define `Adapt` overloads for ITensor/Tensor types
+cpu(x::Array) = x
+cpu(x::CuArray) = adapt(Array, x)
 cpu(x::TensorStorage) = setdata(x, cpu(data(x)))
 cpu(x::Tensor) = setstorage(x, cpu(storage(x)))
 
