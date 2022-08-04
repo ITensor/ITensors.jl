3151c49e1f9386e45d07c6f23ec6871452a40c5d
Matthew Fishman <mfishman@flatironinstitute.org>
2022-08-04 09:27:24 -0400

    Fix tests

--- src/mps/mps.jl
+++ src/mps/mps.jl
@@ -682,7 +682,6 @@ function correlation_matrix(psi::MPS, _Op1, _Op2; kwargs...)
     # Get j == i diagonal correlations
     rind = commonind(psi[i], psi[i + 1])
     oᵢ = adapt(datatype(Li), op(onsiteOp, s, i))
-    # C[ni, ni] = (prime(dag(psi[i]), !rind) * oᵢ * Li)[] / norm2_psi
     C[ni, ni] =
       scalar((Li * oᵢ) * prime(dag(psi[i]), not(rind))) / norm2_psi
 
