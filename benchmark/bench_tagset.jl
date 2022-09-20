694300f8f4f3893dff715ae1d4c83902107c3a4e
Karl Pierce <kmp5@vt.edu>
2022-09-20 15:54:29 -0400

    Format `benchmark/`

--- benchmark/bench_tagset.jl
+++ benchmark/bench_tagset.jl
@@ -7,9 +7,7 @@ suite = BenchmarkGroup()
 
 suite["tagset"] = @benchmarkable TagSet("abcdefgh,ijklmnop,qrstuvwx,ABCDEFGH")
 suite["tagset_unicode"] = @benchmarkable TagSet("αβγδϵζηθ,ijklmnop,qrstuvwx,ΑΒΓΔΕΖΗΘ")
-suite["tagset_long"] = @benchmarkable TagSet(
-  "abcdefghijklm,nopqrstuvwxyz,ABCDEFGHIJKLM,NOPQRSTUVWXYZ"
-)
+suite["tagset_long"] = @benchmarkable TagSet("abcdefghijklm,nopqrstuvwxyz,ABCDEFGHIJKLM,NOPQRSTUVWXYZ")
 end
 
 BenchTagSet.suite
