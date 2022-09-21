b39bcb981def2819823ff459f1ee1fbad233afd4
Karl Pierce <kmp5@vt.edu>
2022-09-21 15:35:45 -0400

    Update ITensorGPU/test/test_cuitensor.jl
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- ITensorGPU/test/test_cuitensor.jl
+++ ITensorGPU/test/test_cuitensor.jl
@@ -96,7 +96,7 @@ using ITensors,
       CuArray(permute(A, i, j, k)) + CuArray(permute(B, i, j, k))
     for ii in 1:dim(i), jj in 1:dim(j), kk in 1:dim(k)
       @test C[i => ii, j => jj, k => kk] ==
-          A[j => jj, i => ii, k => kk] + B[i => ii, k => kk, j => jj]
+        A[j => jj, i => ii, k => kk] + B[i => ii, k => kk, j => jj]
     end
   end
 
