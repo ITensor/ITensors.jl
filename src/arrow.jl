4a527dcd999ff34ffd23045332ebbecdcb383605
Karl Pierce <kmp5@vt.edu>
2022-09-21 14:28:40 -0400

    Format docstring

--- src/arrow.jl
+++ src/arrow.jl
@@ -1,6 +1,7 @@
 
 """
-   Arrow
+Arrow
+
 `enum` type that can take three values: `In`, `Out`, or `Neither`, representing a directionality
 associated with an index, i.e. the index leg is directed into or out of a given tensor
 """
