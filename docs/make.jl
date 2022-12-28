0511999e052f0dfb7024aa1d6cc546f177254f1b
Miles <miles.stoudenmire@gmail.com>
2022-12-27 21:41:04 -0500

    Try redefining Github workflow env variable [no ci]

--- docs/make.jl
+++ docs/make.jl
@@ -2,6 +2,10 @@ include("settings.jl")
 
 makedocs(; sitename=sitename, settings...)
 
+if get(ENV, "GITHUB_EVENT_NAME", nothing) == "workflow_dispatch"
+  ENV["GITHUB_EVENT_NAME"] = "push"
+end
+
 deploydocs(;
   repo="github.com/ITensor/ITensors.jl.git",
   devbranch="main",
