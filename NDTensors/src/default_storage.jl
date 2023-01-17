fc80f192476609ec74faeb448e6f3921c669f9ae
Karl Pierce <kmp5@vt.edu>
2023-01-17 09:42:44 -0500

    Julia Formatting

--- NDTensors/src/default_storage.jl
+++ NDTensors/src/default_storage.jl
@@ -1,10 +1,15 @@
 ## This is a fil which specifies the default storage type provided some set of parameters
 ## The parameters are the element type and storage type
-function default_storage_type(storetype::Type{<:TensorStorage} = Dense, eltype::Type = Float64, datatype::Type{<:AbstractArray} = Vector, index = Tuple{Int,})
+function default_storage_type(
+  storetype::Type{<:TensorStorage}=Dense,
+  eltype::Type=Float64,
+  datatype::Type{<:AbstractArray}=Vector,
+  index=Tuple{Int},
+)
   #TODO check if indices are block sparse or not
   type = nothing
   try
-    type = storetype{eltype, datatype{eltype}}
+    type = storetype{eltype,datatype{eltype}}
   catch e
     println("Cannot construct storetype", storetype)
     throw(e)
@@ -12,16 +17,23 @@ function default_storage_type(storetype::Type{<:TensorStorage} = Dense, eltype::
   return type
 end
 
-default_storage_type(eltype::Type{<:Number}) =  default_storage_type(Dense, eltype)
+default_storage_type(eltype::Type{<:Number}) = default_storage_type(Dense, eltype)
 
-default_storage_type(datatype::Type{<:AbstractArray}) =
-  default_storage_type(Dense, Float64, datatype)
+function default_storage_type(datatype::Type{<:AbstractArray})
+  return default_storage_type(Dense, Float64, datatype)
+end
 
 ## This function needs to be defined for each vector type
-default_storage_type(datatype::Type{<:Vector{ElT}}) where {ElT} = default_storage_type(Dense, ElT, Vector)
+function default_storage_type(datatype::Type{<:Vector{ElT}}) where {ElT}
+  return default_storage_type(Dense, ElT, Vector)
+end
 
-default_storage_type(eltype::Type, datatype::Type{<:AbstractArray}) = default_storage_type(Dense, eltype, datatype)
+function default_storage_type(eltype::Type, datatype::Type{<:AbstractArray})
+  return default_storage_type(Dense, eltype, datatype)
+end
 
-default_storage_type(datatype::Type{<:AbstractArray}, eltype::Type) = default_storage_type(Dense, eltype, datatype)
+function default_storage_type(datatype::Type{<:AbstractArray}, eltype::Type)
+  return default_storage_type(Dense, eltype, datatype)
+end
 
 default_storage_type(index) = default_storage_type(Dense, Float64, Vector, index)
