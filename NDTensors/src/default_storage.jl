09efbd68305a61c05ac0b1bb1b6bb5c5149d155b
kmp5VT <kmp5@vt.edu>
2023-01-20 10:32:19 -0500

    Update default_storagetypes per comments

--- NDTensors/src/default_storage.jl
+++ NDTensors/src/default_storage.jl
@@ -6,14 +6,5 @@ default_eltype() = Float64
 default_storagetype(datatype::Type{<:AbstractArray{ElT}}) where {ElT} = Dense{ElT, datatype}
 
 default_storagetype(datatype::Type{<:AbstractArray}) = default_storagetype(datatype{default_eltype()})
-default_storagetype(eltype::Type{<:Number}) = default_storage_type(default_datatype(eltype))
-default_storagetype() = default_storagetype(default_datatype(default_eltype()))
-default_storage_type(storagetype::Type{<:TensorStorage}) = 
-  try
-    storagetype{default_eltype(), default_datatype(default_eltype())}
-  catch e
-    println("Provided storage type cannot be constructed like a default TensorStorage object")
-    throw(e)
-  end
-
-#default_storage_type(index) = default_storage_type(Dense, Float64, Vector, index)
+default_storagetype(eltype::Type{<:Number}) = default_storagetype(default_datatype(eltype))
+default_storagetype() = default_storagetype(default_eltype())
