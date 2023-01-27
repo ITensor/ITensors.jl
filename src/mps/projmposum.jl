10a648775f39fb9c0efe2808b7635d99ca0c5c06
Miles <miles.stoudenmire@gmail.com>
2023-01-27 23:52:12 +0800

    Implement terms and set_terms and generic code fixes

--- src/mps/projmposum.jl
+++ src/mps/projmposum.jl
@@ -1,18 +1,24 @@
 
 abstract type AbstractSum end
 
-copy(P::AbstractSum) = AbstractSum(copy.(P.terms))
+terms(sum::AbstractSum) = sum.terms
 
-nsite(P::AbstractSum) = nsite(P.terms[1])
-
-function set_nsite!(Ps::AbstractSum, nsite)
-  for P in Ps.terms
-    set_nsite!(P, nsite)
-  end
-  return Ps
+function set_terms(sum::AbstractSum, terms)
+  return error("Please implement `set_terms` for the `AbstractSum` type `$(typeof(sum))`.")
 end
 
-Base.length(P::AbstractSum) = length(P.terms[1])
+copy(P::AbstractSum) = typeof(P)(copy.(terms(P)))
+
+nsite(P::AbstractSum) = nsite(first(terms(P)))
+
+function set_nsite!(A::AbstractSum, nsite)
+  for t in terms(A)
+    set_nsite!(t, nsite)
+  end
+  return A
+end
+
+Base.length(A::AbstractSum) = length(terms(A)[1])
 
 """
     product(P::ProjMPOSum,v::ITensor)
@@ -28,12 +34,12 @@ returned ITensor will have the same indices
 as `v`. The operator overload `P(v)` is
 shorthand for `product(P,v)`.
 """
-function product(P::AbstractSum, v::ITensor)::ITensor
-  Pv = product(P.terms[1], v)
-  for n in 2:length(P.terms)
-    Pv += product(P.terms[n], v)
+function product(A::AbstractSum, v::ITensor)::ITensor
+  Av = product(first(terms(A)), v)
+  for n in 2:length(terms(A))
+    Av += product(terms(A)[n], v)
   end
-  return Pv
+  return Av
 end
 
 """
@@ -43,15 +49,15 @@ Deduce the element type (such as Float64
 or ComplexF64) of the tensors in the ProjMPOSum
 `P`.
 """
-function Base.eltype(P::AbstractSum)
-  elT = eltype(P.terms[1])
-  for n in 2:length(P.terms)
-    elT = promote_type(elT, eltype(P.terms[n]))
+function Base.eltype(A::AbstractSum)
+  elT = eltype(first(terms(A)))
+  for n in 2:length(terms(A))
+    elT = promote_type(elT, eltype(terms(A)[n]))
   end
   return elT
 end
 
-(P::AbstractSum)(v::ITensor) = product(P, v)
+(A::AbstractSum)(v::ITensor) = product(A, v)
 
 """
     size(P::ProjMPOSum)
@@ -65,7 +71,7 @@ indices `(a,s1,s2,b)` to the space `(a',s1',s2',b')`
 then the size is `(d,d)` where
 `d = dim(a)*dim(s1)*dim(s1)*dim(b)`
 """
-Base.size(P::AbstractSum) = size(P.terms[1])
+Base.size(A::AbstractSum) = size(first(terms(A)))
 
 """
     position!(P::ProjMPOSum, psi::MPS, pos::Int)
@@ -79,9 +85,9 @@ The MPS `psi` must have compatible bond indices with
 the previous projected MPO tensors for this
 operation to succeed.
 """
-function position!(P::AbstractSum, psi::MPS, pos::Int)
-  for M in P.terms
-    position!(M, psi, pos)
+function position!(A::AbstractSum, psi::MPS, pos::Int)
+  for t in terms(A)
+    position!(t, psi, pos)
   end
 end
 
@@ -98,25 +104,37 @@ ProjMPOSum `P`, and `ortho` is a String which can take
 the values `"left"` or `"right"` depending on the
 sweeping direction of the DMRG calculation.
 """
-function noiseterm(P::AbstractSum, phi::ITensor, dir::String)
-  nt = noiseterm(P.terms[1], phi, dir)
-  for n in 2:length(P.terms)
-    nt += noiseterm(P.terms[n], phi, dir)
+function noiseterm(A::AbstractSum, phi::ITensor, dir::String)
+  nt = noiseterm(terms(A)[1], phi, dir)
+  for n in 2:length(terms(A))
+    nt += noiseterm(terms(A)[n], phi, dir)
   end
   return nt
 end
 
+"""
+    disk(ps::AbstractSum; kwargs...)
+
+Call `disk` on each term of an AbstractSum, to enable
+saving of cached data to hard disk.
+"""
+function disk(sum::AbstractSum; disk_kwargs...)
+  return set_terms(sum, [disk(term; disk_kwargs...) for term in terms(sum)])
+end
+
 #
-# Definition of concrete, generic ProjSum type
+# Definition of concrete, generic SequentialSum type
 #
 
-mutable struct ProjSum{T} <: AbstractSum
+struct SequentialSum{T} <: AbstractSum
   terms::Vector{T}
 end
 
-ProjSum{T}(mpos::Vector{MPO}) where {T} = ProjSum([T(M) for M in mpos])
+SequentialSum{T}(mpos::Vector{MPO}) where {T} = SequentialSum([T(M) for M in mpos])
 
-ProjSum{T}(Ms::MPO...) where {T} = ProjSum{T}([Ms...])
+SequentialSum{T}(Ms::MPO...) where {T} = SequentialSum{T}([Ms...])
+
+set_terms(sum::SequentialSum, terms) = SequentialSum(terms)
 
 """
 A ProjMPOSum computes and stores the projection of an
@@ -142,24 +160,4 @@ sum Σⱼ on the left):
      o--o--o-      -o--o--o--o--o--o |psi>
 ```
 """
-const ProjMPOSum = ProjSum{ProjMPO}
-
-"""
-A `DiskProjMPOSum` functions the same as a `ProjMPOSum`
-(see the docstring for `ProjMPOSum`) but automatically caches most of
-the tensors it stores onto the hard drive to save RAM memory.
-"""
-const DiskProjMPOSum = ProjSum{DiskProjMPO}
-
-"""
-    disk(ps::ProjMPOSum; kwargs...)
-
-Convert a ProjMPOSum into a DiskProjMPOSum,
-which will automatically start caching most 
-stored tensors onto the hard drive.
-"""
-function disk(ps::ProjMPOSum; kwargs...)
-  return DiskProjMPOSum([disk(pm; kwargs...) for pm in ps.terms])
-end
-
-disk(P::DiskProjMPOSum; kwargs...) = P
+const ProjMPOSum = SequentialSum{ProjMPO}
