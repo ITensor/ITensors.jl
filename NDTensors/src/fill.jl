d37c353bb21256fe73d9783d5bf0aa22b58bece5
Karl Pierce <kmp5@vt.edu>
2023-02-03 13:44:35 -0400

    Update fills to error check but don't change types

--- NDTensors/src/fill.jl
+++ NDTensors/src/fill.jl
@@ -2,18 +2,10 @@
 #  This includes random fills, zeros, ...
 
 function generic_randn(
-  StoreT::Type{<:Dense{<:Number,DataT}}, dim::Integer=0
-) where {DataT<:AbstractArray}
-  ElT = eltype(StoreT)
-  ElTD = eltype(DataT)
-  if ElTD != Any && ElTD != ElT
-    println(
-      "Warning, Element provided to Dense does not match the datatype. Defaulting to Dense eltype.",
-    )
-  end
-  typedDataT = set_eltype_if_unspecified(DataT, ElT)
-  data = generic_randn(typedDataT, dim)
-  StoreT = NDTensors.similartype(StoreT, ElT)
+  StoreT::Type{<:Dense{ElT,DataT}}, dim::Integer=0
+) where {DataT<:AbstractArray, ElT}
+  @assert ElT == eltype(DataT)
+  data = generic_randn(DataT, dim)
   return StoreT(data)
 end
 
@@ -22,7 +14,7 @@ function generic_randn(StoreT::Type{<:Dense{ElT}}, dim::Integer=0) where {ElT}
 end
 
 function generic_randn(StoreT::Type{<:Dense}, dim::Integer=0)
-  return generic_randn(StoreT{default_eltype(),default_datatype(default_eltype())}, dim)
+  return generic_randn(default_storagetype(), dim)
 end
 
 function generic_randn(DataT::Type{<:AbstractArray}, dim::Integer=0)
@@ -38,24 +30,15 @@ end
 ##TODO replace randn in ITensors with generic_randn
 ## and replace zeros with generic_zeros
 function generic_zeros(
-  StoreT::Type{<:Dense{<:Number,DataT}}, dim::Integer=0
-) where {DataT<:AbstractArray}
-  ElT = eltype(StoreT)
-  ElTD = eltype(DataT)
-  if ElTD != Any && ElTD != ElT
-    println(
-      "Warning, Element provided to Dense does not match the datatype. Defaulting to Dense eltype.",
-    )
-  end
-  typedDataT = set_eltype_if_unspecified(DataT, ElT)
-  data = generic_zeros(typedDataT, dim)
-  StoreT = NDTensors.similartype(StoreT, ElT)
+  StoreT::Type{<:Dense{ElT,DataT}}, dim::Integer=0
+) where {DataT<:AbstractArray, ElT}
+  @assert ElT == eltype(DataT)
+  data = generic_zeros(DataT, dim)
   return StoreT(data)
 end
 
 function generic_zeros(StoreT::Type{<:Dense{ElT}}, dim::Integer=0) where {ElT}
-  @show default_storagetype(eltype(StoreT))
-  return generic_zeros(default_storagetype(eltype(StoreT)), dim)
+  return generic_zeros(default_storagetype(ElT), dim)
 end
 
 function generic_zeros(StoreT::Type{<:Dense}, dim::Integer=0)
