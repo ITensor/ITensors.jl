f4606f8d91cd9ad529276894e265a51755397bae
mtfishman <mfishman@flatironinstitute.org>
2023-02-06 12:00:36 -0500

    Remove similartype overloads for Array in favor of set_eltype, set_ndims

--- NDTensors/src/array/similar.jl
+++
@@ -1,8 +0,0 @@
-## Overloads needed for `Array`
-function similartype(arraytype::Type{<:Array}, eltype::Type)
-  return Array{eltype,ndims(arraytype)}
-end
-
-function similartype(arraytype::Type{<:Array}, dims::Tuple)
-  return Array{eltype(arraytype),length(dims)}
-end
