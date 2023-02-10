896a91dbd77548912f13e7ab2495b8d24f0c417c
Karl Pierce <kmp5@vt.edu>
2023-02-10 11:21:58 -0500

    [NDTensors] [ITensorGPU] Introduce `default_storagetype`, `default_datatype`, etc. for more generic `Tensor`/`ITensor` constructors (#1040)

--- ITensorGPU/src/cuarray/set_types.jl
+++ ITensorGPU/src/cuarray/set_types.jl
@@ -1,4 +1,12 @@
-buffertype(::Type{<:CuArray{<:Any,<:Any,B}}) where {B} = B
+buffertype(datatype::Type{<:CuArray{<:Any,<:Any,B}}) where {B} = B
+function buffertype(datatype::Type{<:CuArray})
+  println(
+    "CuArray definitions require a CUDA.Mem buffer try $(datatype{default_buffertype()})"
+  )
+  throw(TypeError)
+end
+
+default_buffertype() = CUDA.Mem.DeviceBuffer
 
 function set_eltype(arraytype::Type{<:CuArray}, eltype::Type)
   return CuArray{eltype,ndims(arraytype),buffertype(arraytype)}
--- NDTensors/src/dense/dense.jl
+++ NDTensors/src/dense/dense.jl
@@ -3,25 +3,113 @@
 #
 using LinearAlgebra: BlasFloat
 
-struct Dense{ElT,VecT<:AbstractArray} <: TensorStorage{ElT}
-  data::VecT
-  function Dense{ElT,VecT}(data::AbstractArray) where {ElT,VecT<:AbstractArray{ElT}}
-    return new{ElT,VecT}(data)
+struct Dense{ElT,DataT<:AbstractArray} <: TensorStorage{ElT}
+  data::DataT
+  function Dense{ElT,DataT}(data::DataT) where {ElT,DataT<:AbstractVector}
+    @assert ElT == eltype(DataT)
+    return new{ElT,DataT}(data)
   end
 
-  # Special case of Vector
-  function Dense{ElT,VecT}(data::Vector) where {ElT,VecT<:AbstractArray{ElT}}
-    return new{ElT,VecT}(data)
-  end
-
-  # Special case of Array which gets turned into a Vector
-  function Dense{ElT,VecT}(data::Array) where {ElT,VecT<:AbstractArray{ElT}}
-    return new{ElT,VecT}(vec(data))
+  function Dense{ElT,DataT}(data::DataT) where {ElT,DataT<:AbstractArray}
+    println("Only Vector-based datatypes are currently supported.")
+    throw(TypeError)
   end
 end
 
-# TODO: make these more general, move to tensorstorage.jl
-datatype(::Type{<:Dense{<:Any,DataT}}) where {DataT} = DataT
+#Start with high information constructors and move to low information constructors
+function Dense{ElT,DataT}() where {ElT,DataT<:AbstractArray}
+  return Dense{ElT,DataT}(DataT())
+end
+
+# Construct from a set of indices
+# This will fail if zero(ElT) is not defined for the ElT
+function Dense{ElT,DataT}(inds::Tuple) where {ElT,DataT<:AbstractArray}
+  return Dense{ElT,DataT}(generic_zeros(DataT, dim(inds)))
+end
+
+function Dense{ElT,DataT}(dim::Integer) where {ElT,DataT<:AbstractArray}
+  return Dense{ElT,DataT}(generic_zeros(DataT, dim))
+end
+
+function Dense{ElT,DataT}(::UndefInitializer, inds::Tuple) where {ElT,DataT<:AbstractArray}
+  return Dense{ElT,DataT}(similar(DataT, dim(inds)))
+end
+
+function Dense{ElT,DataT}(x::ElT, dim::Integer) where {ElT,DataT<:AbstractVector}
+  return Dense{ElT,DataT}(fill!(similar(DataT, dim), x))
+end
+
+function Dense{ElR,DataT}(data::AbstractArray) where {ElR,DataT<:AbstractArray}
+  data = convert(DataT, data)
+  return Dense{ElR,DataT}(data)
+end
+
+# This function is ill-defined. It cannot transform a complex type to real...
+function Dense{ElR}(data::AbstractArray{ElT}) where {ElR,ElT}
+  return Dense{ElR}(convert(similartype(typeof(data), ElR), data))
+end
+
+function Dense{ElT}(data::AbstractArray{ElT}) where {ElT}
+  return Dense{ElT,typeof(data)}(data)
+end
+
+function Dense{ElT}(inds::Tuple) where {ElT}
+  return Dense{ElT}(dim(inds))
+end
+
+function Dense{ElT}(dim::Integer) where {ElT}
+  return Dense{ElT,default_datatype(ElT)}(dim)
+end
+
+Dense{ElT}() where {ElT} = Dense{ElT,default_datatype(ElT)}()
+
+function Dense(data::AbstractVector)
+  return Dense{eltype(data)}(data)
+end
+
+function Dense(data::DataT) where {DataT<:AbstractArray{<:Any,N}} where {N}
+  #println("Warning: Only vector based datatypes are currenlty supported by Dense. The data structure provided will be vectorized.")
+  return Dense(vec(data))
+end
+
+function Dense(DataT::Type{<:AbstractArray}, dim::Integer)
+  ElT = eltype(DataT)
+  return Dense{ElT,DataT}(dim)
+end
+
+Dense(ElT::Type{<:Number}, dim::Integer) = Dense{ElT}(dim)
+
+function Dense(ElT::Type{<:Number}, ::UndefInitializer, dim::Integer)
+  return Dense{ElT,default_datatype(ElT)}(undef, (dim,))
+end
+
+function Dense(::UndefInitializer, dim::Integer)
+  datatype = default_datatype()
+  return Dense{eltype(datatype),datatype}(undef, (dim,))
+end
+
+Dense(x::Number, dim::Integer) = Dense(fill!(similar(default_datatype(typeof(x)), dim), x))
+
+Dense(dim::Integer) = Dense(default_eltype(), dim)
+
+Dense(::Type{ElT}) where {ElT} = Dense{ElT}()
+
+## End Dense initializers
+
+setdata(D::Dense, ndata) = Dense(ndata)
+setdata(storagetype::Type{<:Dense}, data) = Dense(data)
+
+copy(D::Dense) = Dense(copy(data(D)))
+
+#This is getting closer but is still broken...
+function Base.real(T::Type{<:Dense})
+  return set_datatype(T, similartype(datatype(T), real(eltype(T))))
+end
+
+## TODO this currently works only for vector not cuvector because similartype fails
+function complex(T::Type{<:Dense})
+  return set_datatype(T, similartype(datatype(T), complex(eltype(T))))
+end
 
 function set_datatype(storagetype::Type{<:Dense}, datatype::Type{<:AbstractVector})
   return Dense{eltype(datatype),datatype}
@@ -33,74 +121,21 @@ function set_datatype(storagetype::Type{<:Dense}, datatype::Type{<:AbstractArray
   )
 end
 
-# TODO: Define a generic `dense` for `Tensor`, `TensorStorage`.
-dense(storagetype::Type{<:Dense}) = storagetype
-
-function Dense(data::VecT) where {VecT<:AbstractArray{ElT}} where {ElT}
-  return Dense{ElT,VecT}(data)
-end
-
-function Dense(data::Array{ElT}) where {ElT}
-  return Dense{ElT,Vector{ElT}}(vec(data))
-end
-
-function Dense{ElR}(data::AbstractArray{ElT}) where {ElR,ElT}
-  return ElT == ElR ? Dense(data) : Dense(ElR.(data))
-end
-
-# Construct from a set of indices
-function Dense{ElT,VecT}(inds) where {ElT,VecT<:AbstractArray{ElT}}
-  return Dense(VecT(dim(inds)))
-end
-
-Dense{ElT}(dim::Integer) where {ElT} = Dense(zeros(ElT, dim))
-
-Dense{ElT}(::UndefInitializer, dim::Integer) where {ElT} = Dense(Vector{ElT}(undef, dim))
-
-Dense(::Type{ElT}, dim::Integer) where {ElT} = Dense{ElT}(dim)
-
-Dense(x::ElT, dim::Integer) where {ElT<:Number} = Dense(fill(x, dim))
-
-Dense(dim::Integer) = Dense(Float64, dim)
-
-Dense(::Type{ElT}, ::UndefInitializer, dim::Integer) where {ElT} = Dense{ElT}(undef, dim)
-
-Dense(::UndefInitializer, dim::Integer) = Dense(Float64, undef, dim)
-
-Dense{ElT}() where {ElT} = Dense(ElT[])
-Dense(::Type{ElT}) where {ElT} = Dense{ElT}()
-
-setdata(D::Dense, ndata) = Dense(ndata)
-setdata(storagetype::Type{<:Dense}, data) = Dense(data)
-
-#
-# Random constructors
-#
-
-function randn(::Type{StoreT}, dim::Integer) where {StoreT<:Dense}
-  return Dense(randn(eltype(StoreT), dim))
-end
-
-copy(D::Dense) = Dense(copy(data(D)))
-
-Base.real(::Type{Dense{ElT,Vector{ElT}}}) where {ElT} = Dense{real(ElT),Vector{real(ElT)}}
-
-function complex(::Type{Dense{ElT,Vector{ElT}}}) where {ElT}
-  return Dense{complex(ElT),Vector{complex(ElT)}}
-end
+# TODO: make these more general, move to tensorstorage.jl
+datatype(::Type{<:Dense{<:Any,DataT}}) where {DataT} = DataT
 
 zeros(DenseT::Type{<:Dense}, inds) = zeros(DenseT, dim(inds))
 
-# Generic for handling `Vector` and `CuVector`
-function zeros(storagetype::Type{<:Dense}, dim::Int)
-  return fill!(NDTensors.similar(storagetype, dim), zero(eltype(storagetype)))
-end
+# # Generic for handling `Vector` and `CuVector`
+# function zeros(storagetype::Type{<:Dense}, dim::Int)
+#   return fill!(NDTensors.similar(storagetype, dim), zero(eltype(storagetype)))
+# end
 
 function promote_rule(
-  ::Type{<:Dense{ElT1,VecT1}}, ::Type{<:Dense{ElT2,VecT2}}
-) where {ElT1,VecT1,ElT2,VecT2}
+  ::Type{<:Dense{ElT1,DataT1}}, ::Type{<:Dense{ElT2,DataT2}}
+) where {ElT1,DataT1,ElT2,DataT2}
   ElR = promote_type(ElT1, ElT2)
-  VecR = promote_type(VecT1, VecT2)
+  VecR = promote_type(DataT1, DataT2)
   return Dense{ElR,VecR}
 end
 
@@ -136,7 +171,7 @@ const DenseTensor{ElT,N,StoreT,IndsT} = Tensor{ElT,N,StoreT,IndsT} where {StoreT
 DenseTensor(::Type{ElT}, inds) where {ElT} = tensor(Dense(ElT, dim(inds)), inds)
 
 # TODO: Define a generic `dense` for `Tensor`, `TensorStorage`.
-dense(tensortype::Type{<:DenseTensor}) = tensortype
+dense(storagetype::Type{<:Dense}) = storagetype
 
 # Special convenience function for Int
 # dimensions
@@ -176,7 +211,7 @@ Tensor(A::Array{<:Number,N}, inds::Dims{N}) where {N} = tensor(Dense(vec(A)), in
 #
 
 function randomDenseTensor(::Type{ElT}, inds) where {ElT}
-  return tensor(randn(Dense{ElT}, dim(inds)), inds)
+  return tensor(generic_randn(Dense{ElT}, dim(inds)), inds)
 end
 
 function randomDenseTensor(::Type{ElT}, inds::Int...) where {ElT}
@@ -207,7 +242,7 @@ IndexStyle(::Type{<:DenseTensor}) = IndexLinear()
 iterate(T::DenseTensor, args...) = iterate(storage(T), args...)
 
 function _zeros(TensorT::Type{<:DenseTensor}, inds)
-  return tensor(zeros(storagetype(TensorT), dim(inds)), inds)
+  return tensor(generic_zeros(storagetype(TensorT), dim(inds)), inds)
 end
 
 function zeros(TensorT::Type{<:DenseTensor}, inds)
