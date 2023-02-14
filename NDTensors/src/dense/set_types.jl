1da62f9fc3cd0418715691b35d5b0e48c9e254e7
Karl Pierce <kmp5@vt.edu>
2023-02-14 10:52:22 -0500

    Fix name error

--- NDTensors/src/dense/set_types.jl
+++ NDTensors/src/dense/set_types.jl
@@ -6,12 +6,12 @@ end
 function set_parameters_if_unspecified(
   storetype::Type{<:Dense{ElT,DataT}}, datatype::Type{<:AbstractArray}=default_datatype(ElT)
 ) where {ElT,DataT<:AbstractArray}
-  return set_datatype(storetype, set_properties_if_unspecified(DataT, ElT))
+  return set_datatype(storetype, set_parameters_if_unspecified(DataT, ElT))
 end
 function set_parameters_if_unspecified(
   storetype::Type{<:Dense{ElT}}, datatype::Type{<:AbstractArray}=default_datatype(ElT)
 ) where {ElT}
-  return default_storagetype(set_properties_if_unspecified(datatype, ElT), ())
+  return default_storagetype(set_parameters_if_unspecified(datatype, ElT), ())
 end
 
 function set_parameters_if_unspecified(
