61fce12af6dc5a013d544f527da2f60e9a747c37
mtfishman <mfishman@flatironinstitute.org>
2023-02-17 12:23:06 -0500

    Fix test selection args logic

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,10 +6,13 @@ ITensors.BLAS.set_num_threads(1)
 ITensors.disable_threaded_blocksparse()
 
 @testset "ITensors tests" begin
-  println("Passed arguments ARGS = $(ARGS) to tests.")
-  if isempty(ARGS) || "all" in ARGS || "base" in ARGS
+  # Make a copy, since it seems that one of the ITensors base
+  # tests modifies `ARGS` and makes them empty.
+  test_args = copy(ARGS)
+  println("Passed arguments ARGS = $(test_args) to tests.")
+  if isempty(test_args) || "all" in test_args || "base" in test_args
     println(
-      """\nArguments ARGS = $(ARGS) are empty, or contain `"all"` or `"base"`. Running base (non-MPS/MPO) ITensors tests.""",
+      """\nArguments ARGS = $(test_args) are empty, or contain `"all"` or `"base"`. Running base (non-MPS/MPO) ITensors tests.""",
     )
     dirs = [
       "LazyApply",
@@ -20,14 +23,14 @@ ITensors.disable_threaded_blocksparse()
       "ITensorChainRules",
       "ITensorNetworkMaps",
     ]
-    for dir in dirs
+    @time for dir in dirs
       println("\nTest $(@__DIR__)/$(dir)")
       @time include(joinpath(@__DIR__, dir, "runtests.jl"))
     end
   end
-  if isempty(ARGS) || "all" in ARGS || "mps" in ARGS
+  if isempty(test_args) || "all" in test_args || "mps" in test_args
     println(
-      """\nArguments ARGS = $(ARGS) are empty, or contain `"all"` or `"mps"`. Running MPS/MPO ITensors tests.""",
+      """\nArguments ARGS = $(test_args) are empty, or contain `"all"` or `"mps"`. Running MPS/MPO ITensors tests.""",
     )
     dir = "ITensorLegacyMPS"
     println("\nTest $(@__DIR__)/$(dir)")
