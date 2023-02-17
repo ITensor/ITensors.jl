f2b2c9f84b32663daec0f409371c980ce22a8691
mtfishman <mfishman@flatironinstitute.org>
2023-02-17 12:08:14 -0500

    Remove non-test for non-Hermitian DMRG from tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,7 +6,9 @@ ITensors.BLAS.set_num_threads(1)
 ITensors.disable_threaded_blocksparse()
 
 @testset "ITensors tests" begin
+  println("Passed arguments ARGS = $(ARGS) to tests.")
   if isempty(ARGS) || "all" in ARGS || "base" in ARGS
+    println("""\nArguments ARGS = $(ARGS) are empty, or contain `"all"` or `"base"`. Running base (non-MPS/MPO) ITensors tests.""")
     dirs = [
       "LazyApply",
       "Ops",
@@ -22,6 +24,7 @@ ITensors.disable_threaded_blocksparse()
     end
   end
   if isempty(ARGS) || "all" in ARGS || "mps" in ARGS
+    println("""\nArguments ARGS = $(ARGS) are empty, or contain `"all"` or `"mps"`. Running MPS/MPO ITensors tests.""")
     dir = "ITensorLegacyMPS"
     println("\nTest $(@__DIR__)/$(dir)")
     @time include(joinpath(@__DIR__, dir, "runtests.jl"))
