96eb5e9fe1f3e84015a1e37ca7e837652088e570
Karl Pierce <kmp5@vt.edu>
2023-03-08 12:50:44 -0500

    format

--- NDTensors/src/abstractarray/ndims.jl
+++ NDTensors/src/abstractarray/ndims.jl
@@ -3,5 +3,5 @@ ndims(x) = Base.ndims(x)
 if VERSION < v"1.7"
   ndims(x::AbstractArray) = ndims(typeof(x))
 
-  ndims(x::Type{<:AbstractArray{<:Any, N::Number}}) = N
+  ndims(x::Type{<:AbstractArray{<:Any,N::Number}}) = N
 end
