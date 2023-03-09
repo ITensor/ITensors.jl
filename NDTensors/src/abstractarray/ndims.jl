549ad4a48149d46f97f4133f581e6d859da16267
Karl Pierce <kmp5@vt.edu>
2023-03-09 11:06:57 -0500

    Naming

--- NDTensors/src/abstractarray/ndims.jl
+++ NDTensors/src/abstractarray/ndims.jl
@@ -6,5 +6,5 @@ ndims(arrayT::Type{<:AbstractArray}) = Base.ndims(arrayT)
 ## In house patch to deal issue of calling ndims with an Array of unspecified eltype
 ## https://github.com/JuliaLang/julia/pull/40682
 if VERSION < v"1.7"
-  ndims(array::Type{<:AbstractArray{<:Any,N}}) where {N} = N
+  ndims(arrayT::Type{<:AbstractArray{<:Any,N}}) where {N} = N
 end
