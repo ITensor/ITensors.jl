c3a0cac88b828f1600b21899dc4b554bdf52dd35
Karl Pierce <kmp5@vt.edu>
2023-03-09 16:18:57 -0500

    arraytype not necessary.

--- NDTensors/src/abstractarray/ndims.jl
+++ NDTensors/src/abstractarray/ndims.jl
@@ -6,5 +6,5 @@ ndims(arraytype::Type{<:AbstractArray}) = Base.ndims(arraytype)
 ## In house patch to deal issue of calling ndims with an Array of unspecified eltype
 ## https://github.com/JuliaLang/julia/pull/40682
 if VERSION < v"1.7"
-  ndims(arraytype::Type{<:AbstractArray{<:Any,N}}) where {N} = N
+  ndims(::Type{<:AbstractArray{<:Any,N}}) where {N} = N
 end
