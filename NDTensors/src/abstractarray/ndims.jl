944a396e0ec41081c48bf503c3da8116a3f2773a
Karl Pierce <kmp5@vt.edu>
2023-03-09 11:06:27 -0500

    More specifically call ndims and add comments

--- NDTensors/src/abstractarray/ndims.jl
+++ NDTensors/src/abstractarray/ndims.jl
@@ -1,7 +1,10 @@
-ndims(x) = Base.ndims(x)
+## NDTensors.ndims (not imported from Base)
 
+ndims(array::AbstractArray) = Base.ndims(array)
+ndims(arrayT::Type{<:AbstractArray}) = Base.ndims(arrayT)
+
+## In house patch to deal issue of calling ndims with an Array of unspecified eltype
+## https://github.com/JuliaLang/julia/pull/40682
 if VERSION < v"1.7"
-  ndims(x::AbstractArray) = ndims(typeof(x))
-
-  ndims(x::Type{<:AbstractArray{<:Any,N}}) where {N} = N
+  ndims(array::Type{<:AbstractArray{<:Any,N}}) where {N} = N
 end
