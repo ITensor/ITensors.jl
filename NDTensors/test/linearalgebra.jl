3b5072d092ecd9db6b03c4450cc90a53ae2c9829
Jan Reimers <janreimers@gmail.com>
2023-03-11 20:46:38 -0600

    Format

--- NDTensors/test/linearalgebra.jl
+++ NDTensors/test/linearalgebra.jl
@@ -20,43 +20,44 @@ end
   @test norm(U2 * U2' - Diagonal(fill(1.0, m))) < 1E-14
 end
 
+Base.eps(::Type{Complex{T}}) where {T<:AbstractFloat} = eps(T)
 
-Base.eps(::Type{Complex{T}}) where T <: AbstractFloat = eps(T)
+@testset "Dense $qx decomposition, elt=$elt, positve=$positive" for qx in [qr, ql],
+  elt in [Float64, ComplexF64, Float32, ComplexF32],
+  positive in [false, true]
 
-@testset "Dense $qx decomposition, elt=$elt, positve=$positive" for qx in [qr,ql], elt in [ Float64, ComplexF64, Float32, ComplexF32], positive in [false,true]
-  eps=Base.eps(elt)*30 #this is set rather tight, so if you increase/change m,n you may have open up the tolerance on eps.
+  eps = Base.eps(elt) * 30 #this is set rather tight, so if you increase/change m,n you may have open up the tolerance on eps.
   n, m = 4, 8
-  Id=Diagonal(fill(1.0, min(n, m)))
+  Id = Diagonal(fill(1.0, min(n, m)))
   #
   # Wide matrix (more columns than rows)
   #
   A = randomTensor(elt, n, m)
-  Q, X = qx(A,positive=positive) #X is R or L.
+  Q, X = qx(A; positive=positive) #X is R or L.
   @test A ≈ Q * X atol = eps
   @test array(Q)' * array(Q) ≈ Id atol = eps
   @test array(Q) * array(Q)' ≈ Id atol = eps
-  if positive 
-    nr,nc=size(X)
-    dr=qx==ql ? Base.max(0,nc-nr) : 0
-    diagX=diag(X[:,1+dr:end]) #location of diag(L) is shifted dr columns over the right.
-    @test all(real(diagX).>=0.0) 
-    @test all(imag(diagX).==0.0)
+  if positive
+    nr, nc = size(X)
+    dr = qx == ql ? Base.max(0, nc - nr) : 0
+    diagX = diag(X[:, (1 + dr):end]) #location of diag(L) is shifted dr columns over the right.
+    @test all(real(diagX) .>= 0.0)
+    @test all(imag(diagX) .== 0.0)
   end
   #
   # Tall matrix (more rows than cols)
   #
   A = randomTensor(elt, m, n) #Tall array
-  Q, X = qx(A,positive=positive)
+  Q, X = qx(A; positive=positive)
   @test A ≈ Q * X atol = eps
   @test array(Q)' * array(Q) ≈ Id atol = eps
   if positive
-    nr,nc=size(X)
-    dr=qx==ql ? Base.max(0,nc-nr) : 0
-    diagX=diag(X[:,1+dr:end]) #location of diag(L) is shifted dr columns over the right.
-    @test all(real(diagX).>=0.0) 
-    @test all(imag(diagX).==0.0)
+    nr, nc = size(X)
+    dr = qx == ql ? Base.max(0, nc - nr) : 0
+    diagX = diag(X[:, (1 + dr):end]) #location of diag(L) is shifted dr columns over the right.
+    @test all(real(diagX) .>= 0.0)
+    @test all(imag(diagX) .== 0.0)
   end
 end
-
 
 nothing
