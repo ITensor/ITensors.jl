994b551ff98dcc04d72f1f42fde6159fd837c95a
kmp5VT <kmp5@vt.edu>
2023-03-17 14:38:39 -0400

    Revert ITensors.jl to main

--- src/ITensors.jl
+++ src/ITensors.jl
@@ -67,7 +67,6 @@ using PackageCompiler
 using Pkg
 using Printf
 using Random
-using Requires
 using SerializedElementArrays
 using StaticArrays
 using TimerOutputs
@@ -242,12 +241,6 @@ include("packagecompile/compile.jl")
 include("developer_tools.jl")
 
 function __init__()
-  @require CUDA = "052768ef-5323-5732-b1bb-66c8b64840ba" begin
-    include("../ext/NDTensorsGPU/CUDA/NDTensorsCUDA.jl")
-  end
-  @require Metal = "dde4c033-4e86-420c-a63e-0dd931031962" begin
-    #include("../ext/NDTensorsGPU/Metal/NDTensorsMetal.jl")
-  end
   return resize!(empty!(INDEX_ID_RNGs), Threads.nthreads()) # ensures that we didn't save a bad object
 end
 
