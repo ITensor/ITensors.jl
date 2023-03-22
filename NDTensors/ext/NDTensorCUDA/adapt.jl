0061442ce98dce41beb21b8821a4269c0fd21d6a
kmp5VT <kmp5@vt.edu>
2023-03-22 16:13:55 -0400

    Overwrite CUDA.jl adapt_storage to overwrite eager Float32

--- NDTensors/ext/NDTensorCUDA/adapt.jl
+++ NDTensors/ext/NDTensorCUDA/adapt.jl
@@ -4,3 +4,9 @@
 
 to_vector_type(arraytype::Type{CuArray}) = CuVector
 to_vector_type(arraytype::Type{CuArray{T}}) where {T} = CuVector{T}
+
+Adapt.adapt_storage(::CuArrayAdaptor{B}, xs::AbstractArray{T,N}) where {T<:AbstractFloat,N,B} =
+  CUDA.isbits(xs) ? xs : CuArray{T,N,B}(xs)
+
+Adapt.adapt_storage(::CuArrayAdaptor{B}, xs::AbstractArray{T,N}) where {T<:Complex{<:AbstractFloat},N,B} =
+  CUDA.isbits(xs) ? xs : CuArray{T,N,B}(xs)
