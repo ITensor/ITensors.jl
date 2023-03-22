ccaac1122feabfb99fb4b0956a2e02cdab66eaaf
kmp5VT <kmp5@vt.edu>
2023-03-22 16:37:41 -0400

    Format

--- NDTensors/ext/NDTensorCUDA/adapt.jl
+++ NDTensors/ext/NDTensorCUDA/adapt.jl
@@ -5,8 +5,14 @@
 to_vector_type(arraytype::Type{CuArray}) = CuVector
 to_vector_type(arraytype::Type{CuArray{T}}) where {T} = CuVector{T}
 
-Adapt.adapt_storage(::CuArrayAdaptor{B}, xs::AbstractArray{T,N}) where {T<:AbstractFloat,N,B} =
-  CUDA.isbits(xs) ? xs : CuArray{T,N,B}(xs)
+function Adapt.adapt_storage(
+  ::CuArrayAdaptor{B}, xs::AbstractArray{T,N}
+) where {T<:AbstractFloat,N,B}
+  return CUDA.isbits(xs) ? xs : CuArray{T,N,B}(xs)
+end
 
-Adapt.adapt_storage(::CuArrayAdaptor{B}, xs::AbstractArray{T,N}) where {T<:Complex{<:AbstractFloat},N,B} =
-  CUDA.isbits(xs) ? xs : CuArray{T,N,B}(xs)
+function Adapt.adapt_storage(
+  ::CuArrayAdaptor{B}, xs::AbstractArray{T,N}
+) where {T<:Complex{<:AbstractFloat},N,B}
+  return CUDA.isbits(xs) ? xs : CuArray{T,N,B}(xs)
+end
--- NDTensors/ext/NDTensorCUDA/imports.jl
+++ NDTensors/ext/NDTensorCUDA/imports.jl
@@ -1,4 +1,5 @@
 import NDTensors: set_ndims, set_eltype, to_vector_type, set_eltype_if_unspecified
-import NDTensors: ContractionProperties,_contract!,GemmBackend, auto_select_backend, _gemm!
+import NDTensors:
+  ContractionProperties, _contract!, GemmBackend, auto_select_backend, _gemm!
 
 import .CUDA: CuArrayAdaptor
