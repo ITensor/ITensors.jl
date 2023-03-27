f44bf0837315e62cd77211cf43a49cccd783ddfc
kmp5VT <kmp5@vt.edu>
2023-03-27 11:59:22 -0400

    Add more examples to NDTensorCUDA

--- NDTensors/ext/examples/NDTensorCUDA.jl
+++ NDTensors/ext/examples/NDTensorCUDA.jl
@@ -12,6 +12,37 @@ l = Index(62)
 dim1 = (i, j, l)
 dim2 = (j, k)
 
+NDTensors.generic_randn(CuVector, 20)
+@which ITensor(NDTensors.generic_randn(CuVector, dim(dim1)), dim1)
+Array{Float64}(NDTensors.NeverAlias(), NDTensors.generic_randn(CuVector, 20))
 A = ITensor(NDTensors.generic_randn(CuVector, dim(dim1)), dim1)
 B = ITensor(NDTensors.generic_randn(CuVector, dim(dim2)), dim2)
+C = A * B
+
+A = ITensor(Float32, dim1)
+B = ITensor(Float64, dim2)
+
+fill!(A, randn())
+fill!(B, randn())
+
+A = cu(A)
+B = cu(B)
+
 A * B
+
+dim3 = (l,k);
+dim4 = (i,)
+C = ITensor(NDTensors.generic_randn(CuVector, dim(dim3)), dim3)
+D = ITensor(Tensor(CuVector, dim4))
+fill!(D, randn())
+E = A * B * C
+storage(E)
+storage(D)
+D * E
+
+f(A, B, C) = (A * B * C)[]
+using Zygote
+gradient(f, A, B, C)
+
+@show data(storage(B))
+svd(B, (j))
