449a4bfe740493b576861b47d26a387de4be622c
Karl Pierce <kmp5@vt.edu>
2023-03-28 17:03:46 -0400

    Add broken * function

--- NDTensors/ext/NDTensorMetal/array.jl
+++ NDTensors/ext/NDTensorMetal/array.jl
@@ -3,7 +3,6 @@ using LinearAlgebra
 
 function LinearAlgebra.norm(x::Metal.MtlArray)
   v = real(eltype(x))(0.0)
-  @show typeof(v)
   for i in x
     v += real(conj(i) * i)
   end
@@ -31,4 +30,12 @@ end
 function Base.conj(x::Metal.MtlArray)
   new_vec = NDTensors.similartype(x, eltype(x))(undef, length(x))
   return transform(x, new_vec, conj)
+end
+
+function Base.:*(x::Number, S::Metal.MtlArray)
+  newV = similar(S)
+  for i in 1:length(S)
+     newV[i] = S[i] * (x)
+  end
+  return newV
 end
