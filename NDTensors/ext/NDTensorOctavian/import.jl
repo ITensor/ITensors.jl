77feb692192ca5da2c533488d7eced5904663501
kmp5VT <kmp5@vt.edu>
2023-03-29 15:24:22 -0400

    Move octavian to NDTensorOctavian

---
+++ NDTensors/ext/NDTensorOctavian/import.jl
@@ -0,0 +1,2 @@
+import NDTensors:
+    _gemm!
---
+++ NDTensors/ext/NDTensorOctavian/octavian.jl
@@ -0,0 +1,18 @@
+function backend_octavian()
+  return NDTensors.gemm_backend[] = :Octavian
+end
+
+function _gemm!(
+  ::NDTensors.GemmBackend{:Octavian},
+  tA,
+  tB,
+  alpha,
+  A::AbstractVecOrMat,
+  B::AbstractVecOrMat,
+  beta,
+  C::AbstractVecOrMat,
+)
+  return Octavian.matmul!(
+    C, tA == 'T' ? transpose(A) : A, tB == 'T' ? transpose(B) : B, alpha, beta
+  )
+end
