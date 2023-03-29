5a6c0503a0e80929f95070e170e66d1f08cea842
kmp5VT <kmp5@vt.edu>
2023-03-29 15:45:26 -0400

    Make cu system easier but doesn't currently work with CUDA.Mem.unifiedbuffer

--- src/adapt.jl
+++ src/adapt.jl
@@ -1,5 +1,5 @@
 adapt_structure(to, x::ITensor) = itensor(adapt(to, tensor(x)))
 
-@inline function NDTensors.cu(x::ITensor; unified::Bool=false)
-  return itensor(NDTensors.cu(storage(x); unified=unified), inds(x))
-end
+# @inline function NDTensors.cu(x::ITensor; unified::Bool=false)
+#   return itensor(NDTensors.cu(storage(x); unified=unified), inds(x))
+# end
