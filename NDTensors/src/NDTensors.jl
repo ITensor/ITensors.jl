4db50868c966ca70d032353522d1a0a98aa7b477
kmp5VT <kmp5@vt.edu>
2023-04-07 15:28:57 -0400

    Format

--- NDTensors/src/NDTensors.jl
+++ NDTensors/src/NDTensors.jl
@@ -241,10 +241,10 @@ function __init__()
     end
 
     @require CUDA = "052768ef-5323-5732-b1bb-66c8b64840ba" begin
-        println("NDTensors has CUDA")
-        if CUDA.functional()
-          include("../ext/NDTensorCUDA/NDTensorCUDA.jl")
-        end
+      println("NDTensors has CUDA")
+      if CUDA.functional()
+        include("../ext/NDTensorCUDA/NDTensorCUDA.jl")
+      end
     end
 
     @require Metal = "dde4c033-4e86-420c-a63e-0dd931031962" begin
