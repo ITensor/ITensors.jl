d1afc8a3c496ba2565ded88854c3c1e18bd8926a
kmp5VT <kmp5@vt.edu>
2023-04-11 17:05:53 -0400

    Remove preferences.jl

--- NDTensors/src/preferences.jl
+++
@@ -1,7 +0,0 @@
-using Preferences
-
-function allow_ndtensorcuda(allow::Bool)
-  return @set_preferences!("allow_ndtensorcuda" => allow)
-end
-
-const allow_ndtensorcuda() = @load_preference("allow_ndtensorcuda", false)
