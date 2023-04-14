421b76b7641849f0ca812a6f16c2bee01b17383d
kmp5VT <kmp5@vt.edu>
2023-04-14 11:26:09 -0400

    Format

--- NDTensors/test/runtests.jl
+++ NDTensors/test/runtests.jl
@@ -7,7 +7,7 @@ ops[1] = NDTensors.cpu
 @show NDTensors.cuda_enabled
 if NDTensors.cuda_enabled
   println("Testing with CUDA")
-  using Pkg;
+  using Pkg
   Pkg.add("CUDA")
   using CUDA
   CUDA.allowscalar()
