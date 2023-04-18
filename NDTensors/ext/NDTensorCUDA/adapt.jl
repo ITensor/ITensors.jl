1043247bc398af7a2f9244faf4a40e96fd6ac988
kmp5VT <kmp5@vt.edu>
2023-04-18 14:05:13 -0400

    add an CUDA ndims function for julia v"1.6" and below

--- NDTensors/ext/NDTensorCUDA/adapt.jl
+++ NDTensors/ext/NDTensorCUDA/adapt.jl
@@ -22,3 +22,9 @@ function NDTensors.adapt_storagetype(
 ) where {ElT,StoreT,B}
   return NDTensors.emptytype(NDTensors.adapt_storagetype(CuVector{ElT,B}, StoreT))
 end
+
+## In house patch to deal issue of calling ndims with an Array of unspecified eltype
+## https://github.com/JuliaLang/julia/pull/40682
+if VERSION < v"1.7"
+  ndims(::Type{<:CuArray{<:Any,N}}) where {N} = N
+end
