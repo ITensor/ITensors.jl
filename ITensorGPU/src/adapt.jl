f6f0b5c48614404b1dc69f145acc7f3807632735
kmp5VT <kmp5@vt.edu>
2023-04-26 19:19:25 -0400

    CuVector breaks cu

--- ITensorGPU/src/adapt.jl
+++ ITensorGPU/src/adapt.jl
@@ -3,7 +3,7 @@
 #
 
 NDTensors.cu(eltype::Type{<:Number}, x) = fmap(x -> adapt(CuVector{eltype, default_buffertype()}, x), x)
-NDTensors.cu(x) = fmap(x -> adapt(CuVector, x), x)
+NDTensors.cu(x) = fmap(x -> adapt(CuArray, x), x)
 
 function NDTensors.set_eltype_if_unspecified(
   arraytype::Type{CuVector{T}}, eltype::Type
