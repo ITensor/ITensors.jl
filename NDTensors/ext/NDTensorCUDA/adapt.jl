e67e7c7825b4c5436e78b8634b5ec0058573edbd
Karl Pierce <kmp5@vt.edu>
2023-04-26 14:27:34 -0400

    Add comment for NDTensorCuArayAdaptor

--- NDTensors/ext/NDTensorCUDA/adapt.jl
+++ NDTensors/ext/NDTensorCUDA/adapt.jl
@@ -2,6 +2,10 @@
 # Used to adapt `EmptyStorage` types
 #
 
+## Here we need an NDTensorCuArrayAdaptor because the CuArrayAdaptor provided by CUDA
+## converts 64 bit numbers to 32 bit.  We cannot write `adapt(CuVector, x)` because this
+## Will not allow us to properly utilize the buffer preference without changing the value of
+## default_buffertype. Also `adapt(CuVector{<:Any, <:Any, Buffertype})` fails to work properly
 struct NDTensorCuArrayAdaptor{B} end
 ## TODO make this work for unified. This works but overwrites CUDA's adapt_storage. This fails for emptystorage...
 @inline function NDTensors.cu(xs; unified::Bool=false)
