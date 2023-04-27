e0b4fa15bbd01225ded85d27b13a7ff8771bfeaf
kmp5VT <kmp5@vt.edu>
2023-04-27 14:39:55 -0400

    Add missing zeros function from DiagTensor

--- NDTensors/src/diag/diagtensor.jl
+++ NDTensors/src/diag/diagtensor.jl
@@ -35,7 +35,11 @@ function zeros(tensortype::Type{<:DiagTensor}, inds)
   return tensor(generic_zeros(storagetype(tensortype), mindim(inds)), inds)
 end
 
-function zeros(tensortype::Type{<:DiagTensor}, inds::Tuple{})
+function zeros(tensortype::Type{<:DiagTensor}, inds::Dims)
+  return tensor(generic_zeros(storagetype(tensortype), mindim(inds)), inds)
+end
+
+function zeros(tensortype::Type{<:DiagTensor}, inds::Tuple)
   return tensor(generic_zeros(storagetype(tensortype), mindim(inds)), inds)
 end
 
