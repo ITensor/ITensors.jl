b5be13f33aea9380fa0a3eebed68fbf3fda1c974
kmp5VT <kmp5@vt.edu>
2023-06-15 14:44:43 -0400

    Add warning if CUDA isn't functional

--- NDTensors/test/device_list.jl
+++ NDTensors/test/device_list.jl
@@ -8,6 +8,8 @@ function devices_list(test_args)
     CUDA.allowscalar()
     if CUDA.functional()
       push!(devs, NDTensors.cu)
+    else
+      println("Warning: CUDA.jl is not functional on this architecture and tests will be skipped.")
     end
   end
 
