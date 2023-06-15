ccbd32e1d78ad5253aa5ab7b81d1947871da83d7
kmp5VT <kmp5@vt.edu>
2023-06-15 14:40:15 -0400

    Remove unecessary set_ndims function

--- NDTensors/ext/NDTensorMetal/set_types.jl
+++ NDTensors/ext/NDTensorMetal/set_types.jl
@@ -2,10 +2,6 @@ function set_eltype(arraytype::Type{<:MtlArray}, eltype::Type)
   return MtlArray{eltype,ndims(arraytype)}
 end
 
-function set_ndims(arraytype::Type{<:MtlArray{T,<:Any}}, ndims) where {T}
-  return MtlArray{eltype(arraytype),ndims}
-end
-
 function set_ndims(arraytype::Type{<:MtlArray{T}}, ndims) where {T}
   return MtlArray{eltype(arraytype),ndims}
 end
