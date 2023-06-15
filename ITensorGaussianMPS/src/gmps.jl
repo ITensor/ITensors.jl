9c74b87ed57d70cf4711d416ece18f2327ca4331
Ryan Levy <rlevy3@illinois.edu>
2023-06-15 14:40:16 -0400

    Expand basic operators for hopping

--- ITensorGaussianMPS/src/gmps.jl
+++ ITensorGaussianMPS/src/gmps.jl
@@ -135,6 +135,13 @@ is_annihilation_operator(::OpName"c") = true
 is_annihilation_operator(::OpName"c↑") = true
 is_annihilation_operator(::OpName"c↓") = true
 
+expand_to_ladder_operators(o::Op) = expand_to_ladder_operators(ITensors.name(o))
+expand_to_ladder_operators(o::String) = expand_to_ladder_operators(OpName(o))
+expand_to_ladder_operators(opname::OpName) = opname # By default does nothing
+expand_to_ladder_operators(::OpName"N") = ["Cdag", "C"]
+expand_to_ladder_operators(::OpName"Nup") = ["Cdagup", "Cup"]
+expand_to_ladder_operators(::OpName"Ndn") = ["Cdagdn", "Cdn"]
+
 #interlaced_hamiltonian(h::AbstractMatrix) = h
 #blocked_hamiltonian(h::AbstractMatrix) = Hermitian(reverse_interleave(Matrix(h)))
 
@@ -151,6 +158,12 @@ function quadrant(term)
     error("Unknown quadratic hopping term: $term")
   end
   return q
+end
+
+function single_to_quadratic(term)
+  site = ITensors.site(term[1])
+  new_ops = expand_to_ladder_operators(term[1])
+  return coefficient(term) * Op(new_ops[1], site) * Op(new_ops[2], site)
 end
 
 function quadratic_operator(os::OpSum)
@@ -170,6 +183,7 @@ function quadratic_operator(os::OpSum)
     #@show term.coef
     coef = isreal(coefficient(term)) ? real(coefficient(term)) : coefficient(term)
     coefs[n] = coef
+    term = (length(term) == 1) ? single_to_quadratic(term) : term
     length(term) ≠ 2 && error("Must create hopping Hamiltonian from quadratic Hamiltonian")
     quads[n] = quadrant(term)
     sites[n] = ntuple(n -> ITensors.site(term[n]), Val(2))
