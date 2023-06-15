c6e3fb570de99dc20f52c9cd212a8e9540c3d60f
kmp5VT <kmp5@vt.edu>
2023-06-15 13:45:54 -0400

    Remove unused cu code from mps/adapt

--- src/mps/adapt.jl
+++ src/mps/adapt.jl
@@ -1,5 +1,1 @@
 adapt_structure(to, x::Union{MPS,MPO}) = map(xᵢ -> adapt(to, xᵢ), x)
-
-# function NDTensors.cu(x::Union{MPS,MPO}; unified=false)
-#   return map(xᵢ -> NDTensors.cu(xᵢ; unified=unified), x)
-# end
