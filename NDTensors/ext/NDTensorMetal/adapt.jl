1f46d484dcc9bd2e62f04bff2ff089276baf5447
Karl Pierce <kmp5@vt.edu>
2023-07-05 19:44:10 -0400

    format

--- NDTensors/ext/NDTensorMetal/adapt.jl
+++ NDTensors/ext/NDTensorMetal/adapt.jl
@@ -1,5 +1,5 @@
 import Metal: MtlArrayAdaptor, Private, Shared;
-@inline NDTensors.mtl(xs; storage=Private)  = NDTensors.adapt(MtlArray, xs)
+@inline NDTensors.mtl(xs; storage=Private) = NDTensors.adapt(MtlArray, xs)
 
 ## In the future, to preserve the value of `storage` we need to use `MtlArrayAdaptor`
 # @inline NDTensors.mtl(xs; storage=Private) = NDTensors.adapt(MtlArrayAdaptor{storage}(), xs)
