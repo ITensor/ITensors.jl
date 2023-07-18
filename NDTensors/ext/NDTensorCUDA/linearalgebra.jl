97b16526440cf5b9d407f96afbdaad87373fc2d5
kmp5VT <kmp5@vt.edu>
2023-07-18 14:44:12 -0400

    format

--- NDTensors/ext/NDTensorCUDA/linearalgebra.jl
+++ NDTensors/ext/NDTensorCUDA/linearalgebra.jl
@@ -3,7 +3,7 @@ using CUDA
 function NDTensors.svd_catch_error(A::CuMatrix; alg="JacobiAlgorithm")
   if alg == "JacobiAlgorithm"
     alg = CUDA.CUSOLVER.JacobiAlgorithm()
-  else alg == "QRAlgorithm"
+  else
     alg = CUDA.CUSOLVER.QRAlgorithm()
   end
   USV = try
