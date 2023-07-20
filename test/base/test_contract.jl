bf057e1f0a83c907dddbfde2b935c45fd3704407
Karl Pierce <kmp5@vt.edu>
2023-07-20 10:42:06 -0400

    Add an inplace contraction test simulating https://itensor.discourse.group/t/inplace-multiplication-of-tensors-with-the-same-shape-but-different-indicies/900/16

--- test/base/test_contract.jl
+++ test/base/test_contract.jl
@@ -241,6 +241,16 @@ digits(::Type{T}, i, j, k) where {T} = T(i * 10^2 + j * 10 + k)
         @test CArray ≈ array(permute(C, α, i, j))
       end
     end
+    @testset "Test contract in-place ITensors (4-Tensor*Matrix -> 4-Tensor)" begin
+      A = randomITensor(T, (j,i))
+      B = randomITensor(T, (j,k,l,α))
+      C = ITensor(zero(T), (i,k,α,l))
+      contract!(C, A, B, 1.0, 0.0)
+      contract!(C, A, B, 1.0, 1.0)
+      D = A * B
+      D .+= A * B
+      C == D
+    end
   end # End contraction testset
 end
 
