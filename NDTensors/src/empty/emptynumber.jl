6d7eaa830422ca983df45655471f57215818fab7
Karl Pierce <kmp5@vt.edu>
2023-07-21 12:53:28 -0400

    Move empty number to its own file

---
+++ NDTensors/src/empty/emptynumber.jl
@@ -0,0 +1,34 @@
+#
+# Represents a number that can be set to any type.
+#
+
+struct EmptyNumber <: Real end
+
+zero(::Type{EmptyNumber}) = EmptyNumber()
+zero(n::EmptyNumber) = zero(typeof(n))
+
+# This helps handle a lot of basic algebra, like:
+# EmptyNumber() + 2.3 == 2.3
+convert(::Type{T}, x::EmptyNumber) where {T<:Number} = T(zero(T))
+
+# TODO: Should this be implemented?
+#Complex(x::Real, ::EmptyNumber) = x
+
+# This is to help define `float(::EmptyNumber) = 0.0`.
+# This helps with defining `norm` of `EmptyStorage{EmptyNumber}`.
+AbstractFloat(::NDTensors.EmptyNumber) = zero(AbstractFloat)
+
+# Basic arithmetic
+(::EmptyNumber + ::EmptyNumber) = EmptyNumber()
+(::EmptyNumber - ::EmptyNumber) = EmptyNumber()
+(::EmptyNumber + n::Number) = n
+(n::Number + ::EmptyNumber) = n
+(::EmptyNumber - n::Number) = -n
+(n::Number - ::EmptyNumber) = n
+(::Number * ::EmptyNumber) = EmptyNumber()
+(::EmptyNumber * ::Number) = EmptyNumber()
+(::EmptyNumber * ::EmptyNumber) = EmptyNumber()
+(::EmptyNumber / ::Number) = EmptyNumber()
+(::Number / ::EmptyNumber) = throw(DivideError())
+(::EmptyNumber / ::EmptyNumber) = throw(DivideError())
+-(::EmptyNumber) = EmptyNumber()
