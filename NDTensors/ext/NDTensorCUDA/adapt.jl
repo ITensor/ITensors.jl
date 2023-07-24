f8ce757842d8ed51aa5670d637bb55fbe2be21d2
kmp5VT <kmp5@vt.edu>
2023-07-24 10:11:42 -0400

    Remove empty storage and make adapt for zeros fill array which creates CuArray when you do `cu(Zeros(eltype, number))`

--- NDTensors/ext/NDTensorCUDA/adapt.jl
+++ NDTensors/ext/NDTensorCUDA/adapt.jl
@@ -3,7 +3,7 @@
 ## Will not allow us to properly utilize the buffer preference without changing the value of
 ## default_buffertype. Also `adapt(CuVector{<:Any, <:Any, Buffertype})` fails to work properly
 struct NDTensorCuArrayAdaptor{B} end
-## TODO make this work for unified. This works but overwrites CUDA's adapt_storage. This fails for emptystorage...
+## TODO make this work for unified. This works but overwrites CUDA's adapt_storage. 
 function cu(xs; unified::Bool=false)
   return fmap(
     x -> adapt(NDTensorCuArrayAdaptor{unified ? Mem.UnifiedBuffer : Mem.DeviceBuffer}(), x),
@@ -19,9 +19,15 @@ function Adapt.adapt_storage(adaptor::NDTensorCuArrayAdaptor, xs::AbstractArray)
   return isbits(xs) ? xs : CuArray{ElT,1,BufT}(xs)
 end
 
-function NDTensors.adapt_storagetype(
-  adaptor::NDTensorCuArrayAdaptor, xs::Type{EmptyStorage{ElT,StoreT}}
-) where {ElT,StoreT}
+function Adapt.adapt_storage(adaptor::NDTensorCuArrayAdaptor, xs::Zeros)
+  ElT = eltype(xs)
   BufT = buffertype(adaptor)
-  return NDTensors.emptytype(NDTensors.adapt_storagetype(CuVector{ElT,BufT}, StoreT))
+  return CuArray{ElT, 1, BufT}(xs)
 end
+
+# function NDTensors.adapt_storagetype(
+#   adaptor::NDTensorCuArrayAdaptor, xs::Type{EmptyStorage{ElT,StoreT}}
+# ) where {ElT,StoreT}
+#   BufT = buffertype(adaptor)
+#   return NDTensors.emptytype(NDTensors.adapt_storagetype(CuVector{ElT,BufT}, StoreT))
+# end
