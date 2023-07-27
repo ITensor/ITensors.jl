769f4521db4b40566001f4d8765f505b4c108f7b
Karl Pierce <kmp5@vt.edu>
2023-07-27 17:19:55 -0400

    Move set_parameters to set_types.jl file

---
+++ NDTensors/src/zeros/set_types.jl
@@ -0,0 +1,42 @@
+# `SetParameters.jl` overloads.
+get_parameter(::Type{<:Zeros{P1}}, ::Position{1}) where {P1} = P1
+get_parameter(::Type{<:Zeros{<:Any,P2}}, ::Position{2}) where {P2} = P2
+get_parameter(::Type{<:Zeros{<:Any,<:Any,P3}}, ::Position{3}) where {P3} = P3
+
+# Set parameter 1
+set_parameter(::Type{<:Zeros}, ::Position{1}, P1) = Zeros{P1}
+set_parameter(::Type{<:Zeros{<:Any,P2}}, ::Position{1}, P1) where {P2} = Zeros{P1,P2}
+function set_parameter(::Type{<:Zeros{<:Any,<:Any,P3}}, ::Position{1}, P1) where {P3}
+  @assert eltype(P3) == P1
+  return CuArray{P1,<:Any,P3}
+end
+function set_parameter(::Type{<:Zeros{<:Any,P2,P3}}, ::Position{1}, P1) where {P2,P3}
+  @assert eltype(P3) == P1
+  return CuArray{P1,P2,P3}
+end
+
+# Set parameter 2
+set_parameter(::Type{<:Zeros}, ::Position{2}, P2) = Zeros{<:Any,P2}
+set_parameter(::Type{<:Zeros{P1}}, ::Position{2}, P2) where {P1} = Zeros{P1,P2}
+function set_parameter(::Type{<:Zeros{<:Any,<:Any,P3}}, ::Position{2}, P2) where {P3}
+  P1 = eltype(P3)
+  return Zeros{P1,P2,P3}
+end
+function set_parameter(::Type{<:Zeros{P1,<:Any,P3}}, ::Position{2}, P2) where {P1,P3}
+  return Zeros{P1,P2,P3}
+end
+
+# Set parameter 3
+set_parameter(::Type{<:Zeros}, ::Position{3}, P3) = Zeros{<:Any,<:Any,P3}
+set_parameter(::Type{<:Zeros{P1}}, ::Position{3}, P3) where {P1} = Zeros{P1,<:Any,P3}
+function set_parameter(::Type{<:Zeros{<:Any,P2}}, ::Position{3}, P3) where {P2}
+  P1 = eltype(P3)
+  return CuArray{P1,P2,P3}
+end
+set_parameter(::Type{<:Zeros{P1,P2}}, ::Position{3}, P3) where {P1,P2} = CuArray{P1,P2,P3}
+
+default_parameter(::Type{<:Zeros}, ::Position{1}) = Float64
+default_parameter(::Type{<:Zeros}, ::Position{2}) = 1
+default_parameter(::Type{<:Zeros}, ::Position{3}) = Vector{Float64}
+
+nparameters(::Type{<:Zeros}) = Val(3)
--- NDTensors/src/zeros/zeros.jl
+++ NDTensors/src/zeros/zeros.jl
@@ -1,10 +1,11 @@
 struct Zeros{ElT,N,DataT} <: AbstractArray{ElT,N}
   z::FillArrays.Zeros
+  is::Tuple
   function NDTensors.Zeros{ElT,N,DataT}(inds::Tuple) where {ElT,N,DataT}
     @assert eltype(DataT) == ElT
     @assert ndims(DataT) == N
     z = FillArrays.Zeros(ElT, dim(inds))
-    return new{ElT,N,DataT}(z)
+    return new{ElT,N,DataT}(z, inds)
   end
 end
 
@@ -23,30 +24,16 @@ getindex(zero::Zeros) = getindex(zero.z)
 
 array(zero::Zeros) = datatype(zero)(zero.z)
 Array(zero::Zeros) = array(zero)
+dims(z::Zeros) = z.is
 
 Base.convert(x::Type{T}, z::NDTensors.Zeros) where {T<:Array} = Base.convert(x, z.z)
+function Base.convert(z::Type{<:NDTensors.Zeros}, a::AbstractArray)
+  ElT = promote(eltype(z), eltype(a))
+  @assert ndims(z) == ndims(a) == 1
+  return Zeros{ElT, 1, similartype(a, ElT)}(dims(z))
+end
 Base.getindex(a::Zeros, i) = Base.getindex(a.z, i)
 Base.sum(z::Zeros) = sum(z.z)
 LinearAlgebra.norm(z::Zeros) = norm(z.z)
 setindex!(A::NDTensors.Zeros, v, I) = setindex!(A.z, v, I)
 
-NDTensors.set_parameter(::Type{<:Zeros}, ::Position{1}, P1) = Zeros{P1}
-function NDTensors.set_parameter(::Type{<:Zeros{<:Any,P2}}, ::Position{1}, P1) where {P2}
-  return Zeros{P1,P2}
-end
-function NDTensors.set_parameter(
-  ::Type{<:Zeros{<:Any,P2,P3}}, ::Position{1}, P1
-) where {P2,P3<:AbstractArray}
-  P = NDTensors.set_parameter(P3, Position(1), P1)
-  return Zeros{P1,P2,P}
-end
-NDTensors.set_parameter(::Type{<:Zeros}, ::Position{2}, P2) = Zeros{<:Any,P2}
-NDTensors.set_parameter(::Type{<:Zeros{P1}}, ::Position{2}, P2) where {P1} = Zeros{P1,P2}
-function NDTensors.set_parameter(
-  ::Type{<:Zeros{P1,<:Any,P3}}, ::Position{2}, P2
-) where {P1,P3<:AbstractArray}
-  return Zeros{P1,P2,NDTensors.set_parameter(P3, Position(1), P1)}
-end
-function NDTensors.set_parameter(::Type{<:Zeros{P1,P2}}, ::Position{3}, P3) where {P1,P2}
-  return Zeros{P1,P2,NDTensors.set_parameter(P3, Position(1), P1)}
-end
