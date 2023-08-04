fe08b9e6e39b548f6654ce709f9a4bd4b8514239
Karl Pierce <kmp5@vt.edu>
2023-08-03 20:42:11 -0400

    Remove where

--- src/itensor/emptyitensor.jl
+++ src/itensor/emptyitensor.jl
@@ -9,13 +9,13 @@
 
 Construct an ITensor with storage type `NDTensors.EmptyStorage`, indices `inds`, and element type `ElT`. If the element type is not specified, it defaults to `NDTensors.default_eltype()`, which represents a number type that can take on any value (for example, the type of the first value it is set to).
 """
-function emptyITensor(::Type{ElT}, is::Indices) where {ElT<:Number}
-  z = NDTensors.Zeros{ElT,1,NDTensors.default_datatype(ElT)}(is)
+function emptyITensor(elt::Type{<:Number}, is::Indices)
+  z = NDTensors.Zeros{elt,1,NDTensors.default_datatype(elt)}(is)
   return itensor(z, is)
 end
 
-function emptyITensor(::Type{ElT}, is...) where {ElT<:Number}
-  return emptyITensor(ElT, indices(is...))
+function emptyITensor(elt::Type{<:Number}, is...)
+  return emptyITensor(elt, indices(is...))
 end
 
 emptyITensor(is::Indices) = emptyITensor(NDTensors.default_eltype(), is)
