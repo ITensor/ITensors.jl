990c50a2bb9a16cb50aaf0ec065905b53d9b5b6b
kmp5VT <kmp5@vt.edu>
2023-08-07 13:50:25 -0400

    Add axes to zeros and iszero -> is_unallocated_zeros

--- NDTensors/src/zeros/zeros.jl
+++ NDTensors/src/zeros/zeros.jl
@@ -1,11 +1,10 @@
-struct Zeros{ElT,N,DataT} <: AbstractArray{ElT,N}
+struct Zeros{ElT,N,Axes,Alloc<:AbstractArray} <: AbstractArray{ElT,N}
   z::FillArrays.Zeros
-  is::Tuple
-  function NDTensors.Zeros{ElT,N,DataT}(inds::Tuple) where {ElT,N,DataT}
-    @assert eltype(DataT) == ElT
-    @assert ndims(DataT) == N
-    z = FillArrays.Zeros(ElT, dim(inds))
-    return new{ElT,N,DataT}(z, inds)
+  function NDTensors.Zeros{ElT,N,Alloc}(inds::Tuple) where {ElT,N,Alloc}
+    @assert eltype(Alloc) == ElT
+    z = FillArrays.Zeros{ElT, N}(inds)
+    Axes = axes(z)
+    return new{ElT,N,Axes,Alloc}(z)
   end
 end
 
@@ -24,7 +23,7 @@ getindex(zero::Zeros) = getindex(zero.z)
 
 array(zero::Zeros) = datatype(zero)(zero.z)
 Array(zero::Zeros) = array(zero)
-dims(z::Zeros) = z.is
+dims(z::Zeros) = axes(z.z)
 copy(z::Zeros) = Zeros{eltype(z),1,datatype(z)}(dims(z))
 
 Base.convert(x::Type{T}, z::NDTensors.Zeros) where {T<:Array} = Base.convert(x, z.z)
@@ -34,8 +33,8 @@ Base.sum(z::Zeros) = sum(z.z)
 LinearAlgebra.norm(z::Zeros) = norm(z.z)
 setindex!(A::NDTensors.Zeros, v, I) = setindex!(A.z, v, I)
 
-Base.iszero(t::Tensor) = iszero(storage(t))
-Base.iszero(st::TensorStorage) = data(st) isa Zeros
+is_unallocated_zeros(t::Tensor) = is_unallocated_zeros(storage(t))
+is_unallocated_zeros(st::TensorStorage) = data(st) isa Zeros
 
 function (arraytype::Type{<:Zeros})(::AllowAlias, A::Zeros)
   return A
