311a5fe754ac53a8a20bcb451e92115df687b8b4
kmp5VT <kmp5@vt.edu>
2023-08-07 10:51:14 -0400

    Fix some naming

--- NDTensors/test/runtests.jl
+++ NDTensors/test/runtests.jl
@@ -9,11 +9,11 @@ if isempty(ARGS) || "base" in ARGS
   )
 end
 if "cuda" in ARGS || "all" in ARGS
-  println("""\nArguments ARGS = $(ARGS) contain `"cuda"`. Running NDTensorCUDA tests.""")
+  println("""\nArguments ARGS = $(ARGS) contain `"cuda"`. Running NDTensorsCUDAExt tests.""")
   using CUDA
 end
 if "metal" in ARGS || "all" in ARGS
-  println("""\nArguments ARGS = $(ARGS) contain`"metal"`. Running NDTensorMetal tests.""")
+  println("""\nArguments ARGS = $(ARGS) contain`"metal"`. Running NDTensorsMetalExt tests.""")
   using Metal
 end
 
