3f41e800e489d1416a38ffd1c92a58f9f1e1c814
Miles <miles.stoudenmire@gmail.com>
2023-08-10 08:51:17 -0400

    Update code style in test_fermions

--- test/base/test_fermions.jl
+++ test/base/test_fermions.jl
@@ -475,13 +475,13 @@ import ITensors: Out, In
   @testset "Product MPS consistency checks" begin
     s = siteinds("Fermion", 3; conserve_qns=true)
 
-    pA = productMPS(s, [2, 1, 2])
+    pA = MPS(s, [2, 1, 2])
     TA = ITensor(s[1], s[2], s[3])
     TA[s[1] => 2, s[2] => 1, s[3] => 2] = 1.0
     A = pA[1] * pA[2] * pA[3]
     @test norm(A - TA) < 1E-8
 
-    pB = productMPS(s, [1, 2, 2])
+    pB = MPS(s, [1, 2, 2])
     TB = ITensor(s[1], s[2], s[3])
     TB[s[1] => 1, s[2] => 2, s[3] => 2] = 1.0
     B = pB[1] * pB[2] * pB[3]
@@ -496,7 +496,7 @@ import ITensors: Out, In
     state = [1 for n in 1:N]
     state[1] = 2
     state[2] = 2
-    psi = productMPS(sites, state)
+    psi = MPS(sites, state)
     psi_fluxes = [flux(psi[n]) for n in 1:N]
 
     psi_orig = copy(psi)
@@ -507,21 +507,21 @@ import ITensors: Out, In
 
   @testset "MPS inner regression test" begin
     sites = siteinds("Fermion", 3; conserve_qns=true)
-    psi = productMPS(sites, [2, 2, 1])
+    psi = MPS(sites, [2, 2, 1])
     @test inner(psi, psi) ≈ 1.0
   end
 
-  @testset "Fermionic AutoMPO Tests" begin
+  @testset "Fermionic OpSum Tests" begin
     @testset "Spinless Fermion Hamiltonian" begin
       N = 2
       sites = siteinds("Fermion", N; conserve_qns=true)
       t1 = 1.0
-      ampo = AutoMPO()
+      os = OpSum()
       for b in 1:(N - 1)
-        ampo += -t1, "Cdag", b, "C", b + 1
-        ampo += -t1, "Cdag", b + 1, "C", b
+        os += -t1, "Cdag", b, "C", b + 1
+        os += -t1, "Cdag", b + 1, "C", b
       end
-      H = MPO(ampo, sites)
+      H = MPO(os, sites)
 
       HH = H[1]
       for n in 2:N
@@ -538,13 +538,13 @@ import ITensors: Out, In
 
       sites = siteinds("Fermion", N; conserve_qns=true)
 
-      ampo = AutoMPO()
+      os = OpSum()
       for b in 1:(N - 1)
-        ampo += -t1, "Cdag", b, "C", b + 1
-        ampo += -t1, "Cdag", b + 1, "C", b
-        ampo += V1, "N", b, "N", b + 1
+        os += -t1, "Cdag", b, "C", b + 1
+        os += -t1, "Cdag", b + 1, "C", b
+        os += V1, "N", b, "N", b + 1
       end
-      H = MPO(ampo, sites)
+      H = MPO(os, sites)
 
       for j in 1:(N - 2)
         stateA = [1 for n in 1:N]
@@ -555,8 +555,8 @@ import ITensors: Out, In
         stateB[j + 1] = 2
         stateB[N] = 2 # to make MPS bosonic
 
-        psiA = productMPS(sites, stateA)
-        psiB = productMPS(sites, stateB)
+        psiA = MPS(sites, stateA)
+        psiB = MPS(sites, stateB)
 
         @test inner(psiA', H, psiB) ≈ -t1
         @test inner(psiB', H, psiA) ≈ -t1
@@ -566,7 +566,7 @@ import ITensors: Out, In
         state = [1 for n in 1:N]
         state[j] = 2
         state[j + 1] = 2
-        psi = productMPS(sites, state)
+        psi = MPS(sites, state)
         @test inner(psi', H, psi) ≈ V1
       end
     end
@@ -576,31 +576,31 @@ import ITensors: Out, In
       t1 = 1.79
       t2 = 0.427
       s = siteinds("Fermion", N; conserve_qns=true)
-      a = AutoMPO()
+      os = OpSum()
       for n in 1:(N - 1)
-        a += -t1, "Cdag", n, "C", n + 1
-        a += -t1, "Cdag", n + 1, "C", n
+        os += -t1, "Cdag", n, "C", n + 1
+        os += -t1, "Cdag", n + 1, "C", n
       end
       for n in 1:(N - 2)
-        a += -t2, "Cdag", n, "C", n + 2
-        a += -t2, "Cdag", n + 2, "C", n
+        os += -t2, "Cdag", n, "C", n + 2
+        os += -t2, "Cdag", n + 2, "C", n
       end
-      H = MPO(a, s)
+      H = MPO(os, s)
 
       state1 = [1 for n in 1:N]
       state1[1] = 2
       state1[4] = 2
-      psi1 = productMPS(s, state1)
+      psi1 = MPS(s, state1)
 
       state2 = [1 for n in 1:N]
       state2[2] = 2
       state2[4] = 2
-      psi2 = productMPS(s, state2)
+      psi2 = MPS(s, state2)
 
       state3 = [1 for n in 1:N]
       state3[3] = 2
       state3[4] = 2
-      psi3 = productMPS(s, state3)
+      psi3 = MPS(s, state3)
 
       @test inner(psi1', H, psi2) ≈ -t1
       @test inner(psi2', H, psi1) ≈ -t1
@@ -613,9 +613,9 @@ import ITensors: Out, In
       # Add stationary particle to site 2,
       # hopping over should change sign:
       state1[2] = 2
-      psi1 = productMPS(s, state1)
+      psi1 = MPS(s, state1)
       state3[2] = 2
-      psi3 = productMPS(s, state3)
+      psi3 = MPS(s, state3)
       @test inner(psi1', H, psi3) ≈ +t2
       @test inner(psi3', H, psi1) ≈ +t2
     end
@@ -737,13 +737,7 @@ import ITensors: Out, In
       T = ITensor(dag(s[1]), s[2], dag(s[3]))
       T[1, 1, 1] = 1.0
       T[2, 2, 1] = 1.0
-      F = ITensor(s[1]', s[1])
-      for (j, k) in zip(1:2, reverse(1:2))
-        F[j, k] = 1.0
-      end
-      T *= F
-      U, S, V, spec, u, v = svd(T, [s[1]', s[2]])
-      U *= dag(F)
+      U, S, V, spec, u, v = svd(T, [dag(s[1]), s[2]])
       @test norm(T - U * S * V) < 1E-14
       UU = dag(U) * prime(U, u)
       @test_broken norm(UU - id(u)) ≈ 0
@@ -838,21 +832,21 @@ import ITensors: Out, In
 
       s = siteinds("Fermion", N; conserve_qns=true)
 
-      ampot = AutoMPO()
-      ampoV = AutoMPO()
+      ost = OpSum()
+      osV = OpSum()
       for b in 1:(N - 1)
-        ampot += -t1, "Cdag", b, "C", b + 1
-        ampot += -t1, "Cdag", b + 1, "C", b
-        ampoV += V1, "N", b, "N", b + 1
+        ost += -t1, "Cdag", b, "C", b + 1
+        ost += -t1, "Cdag", b + 1, "C", b
+        osV += V1, "N", b, "N", b + 1
       end
-      Ht = MPO(ampot, s)
-      HV = MPO(ampoV, s)
+      Ht = MPO(ost, s)
+      HV = MPO(osV, s)
 
       state = ["Emp" for n in 1:N]
       for i in 1:2:N
         state[i] = "Occ"
       end
-      psi0 = productMPS(s, state)
+      psi0 = MPS(s, state)
 
       sweeps = Sweeps(3)
       maxdim!(sweeps, 20, 20, 40, 80, 200)
@@ -879,22 +873,22 @@ import ITensors: Out, In
 
       s = siteinds("Fermion", N; conserve_qns=true)
 
-      ampot = AutoMPO()
+      ost = OpSum()
       for b in 1:(N - 1)
-        ampot += -t1, "Cdag", b, "C", b + 1
-        ampot += -t1, "Cdag", b + 1, "C", b
+        ost += -t1, "Cdag", b, "C", b + 1
+        ost += -t1, "Cdag", b + 1, "C", b
       end
       for b in 1:(N - 2)
-        ampot += -t2, "Cdag", b, "C", b + 2
-        ampot += -t2, "Cdag", b + 2, "C", b
+        ost += -t2, "Cdag", b, "C", b + 2
+        ost += -t2, "Cdag", b + 2, "C", b
       end
-      Ht = MPO(ampot, s)
+      Ht = MPO(ost, s)
 
       state = ["Emp" for n in 1:N]
       for i in 1:2:N
         state[i] = "Occ"
       end
-      psi0 = productMPS(s, state)
+      psi0 = MPS(s, state)
 
       sweeps = Sweeps(3)
       maxdim!(sweeps, 20, 20, 40, 80, 200)
@@ -920,13 +914,13 @@ import ITensors: Out, In
       s = siteinds("Fermion", N; conserve_qns=true)
 
       # Ground state |000⟩
-      ψ000 = productMPS(s, "0")
+      ψ000 = MPS(s, "0")
 
       # Start state |011⟩
-      ψ011 = productMPS(s, n -> n == 2 || n == 3 ? "1" : "0")
+      ψ011 = MPS(s, n -> n == 2 || n == 3 ? "1" : "0")
 
       # Reference state |110⟩
-      ψ110 = productMPS(s, n -> n == 1 || n == 2 ? "1" : "0")
+      ψ110 = MPS(s, n -> n == 1 || n == 2 ? "1" : "0")
 
       function ITensors.op(::OpName"CdagC", ::SiteType, s1::Index, s2::Index)
         return op("Cdag", s1) * op("C", s2)
@@ -940,9 +934,9 @@ import ITensors: Out, In
 
       @test inner(ψ1, ψ110) == -1
 
-      a = AutoMPO()
-      a += "Cdag", 1, "C", 3
-      H = MPO(a, s)
+      os = OpSum()
+      os += "Cdag", 1, "C", 3
+      H = MPO(os, s)
 
       # Results in -|110⟩
       ψ2 = noprime(contract(H, ψ011; cutoff=1e-15))
@@ -1028,13 +1022,13 @@ import ITensors: Out, In
       @test norm(T - U * S * V) < 1E-10
     end
 
-    @testset "AutoMPO Regression Test" begin
+    @testset "OpSum Regression Test" begin
       N = 3
       s = siteinds("Fermion", N; conserve_qns=true)
 
-      a = AutoMPO()
-      a += "Cdag", 1, "C", 3
-      @test_nowarn H = MPO(a, s)
+      os = OpSum()
+      os += "Cdag", 1, "C", 3
+      @test_nowarn H = MPO(os, s)
     end
   end # Regression Tests
 
