577da178364d60c12a5098f749042a15478d776c
Karl Pierce <kmp5@vt.edu>
2023-09-06 15:46:32 -0400

    Add comment about Number based `data_isa` function

--- NDTensors/src/abstractarray/data_isa.jl
+++ NDTensors/src/abstractarray/data_isa.jl
@@ -1,4 +1,6 @@
 data_isa(t::Tensor, datatype::Type) = data_isa(storage(t), datatype)
 data_isa(s::TensorStorage, datatype::Type) = data_isa(data(s), datatype)
 data_isa(d::AbstractArray, datatype::Type) = d isa datatype # Might have to unwrap if it is reshaped, sliced, etc.
+## This is for the UniformDiag type can be removed
+## after switching to FillArrays.Diag
 data_isa(n::Number, datatype::Type) = false
