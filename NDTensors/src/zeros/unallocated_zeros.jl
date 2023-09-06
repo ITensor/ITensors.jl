d8e27be71f4d979884f714ed33f1a776e3207c7b
Karl Pierce <kmp5@vt.edu>
2023-09-06 10:43:41 -0400

    remove allocate from unallocated_zeros.jl

--- NDTensors/src/zeros/unallocated_zeros.jl
+++ NDTensors/src/zeros/unallocated_zeros.jl
@@ -109,32 +109,3 @@ end
 ## Check datatypes to see if underlying storage is a 
 ## NDTensors.UnallocatedZeros
 is_unallocated_zeros(a) = data_isa(a, NDTensors.UnallocatedZeros)
-
-function allocate(T::Tensor)
-  if !is_unallocated_zeros(T)
-    return T
-  end
-  return tensor(
-    set_datatype(typeof(NDTensors.storage(T)), alloctype(data(T)))(allocate(data(T))),
-    inds(T),
-  )
-  #@show convert(type, out_data)
-  #return type(allocate(data(T)), inds(T))
-end
-
-function allocate(T::Tensor, elt::Type)
-  if !is_unallocated_zeros(T)
-    return T
-  end
-  ## allocate the tensor if is_unallocated_zeros
-  ElT = promote_type(eltype(data(T)), elt)
-  d = similartype(alloctype(data(T)), ElT)(undef, dim(to_shape(typeof(data(T)), inds(T))))
-  fill!(d, 0)
-  return Tensor(d, inds(T))
-end
-
-allocate(d::AbstractArray) = d
-
-function allocate(z::UnallocatedZeros)
-  return alloctype(z)(undef, dims(z))
-end
