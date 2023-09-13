43ce955e3238644ad970e76ff6208fa804d178ab
Karl Pierce <kmp5@vt.edu>
2023-09-13 12:40:19 -0400

    Add more unit tests

--- NDTensors/test/zero.jl
+++ NDTensors/test/zero.jl
@@ -23,5 +23,28 @@ using FillArrays
   @test norm(z) == zero(T)
   @test z[3, 5, 24] == zero(T)
   fz = FillArrays.Zeros{T,N}(is)
-  @test_broken z = UnallocatedZeros{T,1,Base.axes(()),Array{T,1}}(())
+  #@test_broken z = UnallocatedZeros{T,1,Base.axes(()),Array{T,1}}(())
+  
+  z = UnallocatedZeros{T, 1, Base.axes(()), Vector{T}}()
+  @test length(z) == 0
+
+  z = UnallocatedZeros(Vector, 10)
+  @test NDTensors.alloctype(z) == Vector{NDTensors.default_eltype()}
+  @test length(z) == 10
+
+  z = UnallocatedZeros{T}(Array, 43,20,3)
+  @test dim(z) == 43 * 20 * 3
+  @test eltype(z) == T
+  @test NDTensors.alloctype(z) == Array{T, 3}
+  @test ndims(z) ==3
+  @test axes(z) == (Base.OneTo(43), Base.OneTo(20), Base.OneTo(3))
+
+  @test typeof(data(z)) == Zeros{Float64, 3, Tuple{Base.OneTo{Int64}, Base.OneTo{Int64}, Base.OneTo{Int64}}}
+  array
+  @test array(z) == NDTensors.allocate(z)
+  zp = copy(z)
+  @test zp == z
+  @test eltype(complex(z)) == eltype(NDTensors.alloctype(complex(z))) == complex(T)
+
+  @test sum(z) == zero(T)
 end
