682bcf3c83f40701a1e9b6204e4c635cbbd5e6c7
kmp5VT <kmp5@vt.edu>
2023-09-13 17:17:37 -0400

    Make generic_zero for CUDA

--- NDTensors/ext/NDTensorsCUDAExt/fill.jl
+++ NDTensors/ext/NDTensorsCUDAExt/fill.jl
@@ -1,6 +1,16 @@
+import NDTensors.SetParameters: set_unspecified_parameters, DefaultParameters
+import NDTensors: leaf_parenttype
+
 function NDTensors.generic_randn(arraytype::Type{<:CuArray}, dim::Integer=0)
-  arraytype_specified = NDTensors.SetParameters.set_unspecified_parameters(
-    NDTensors.leaf_parenttype(arraytype), NDTensors.SetParameters.DefaultParameters()
+  arraytype_specified = set_unspecified_parameters(
+    leaf_parenttype(arraytype), DefaultParameters()
   )
   return CUDA.randn(eltype(arraytype_specified), dim)
+end
+
+function NDTensors.generic_zeros(arraytype::Type{<:CuArray}, dim::Integer=0)
+  arraytype_specified = NDTensors.set_unspecified_parameters(
+    leaf_parenttype(arraytype), DefaultParameters()
+  )
+  return CUDA.zeros(eltype(arraytype_specified), dim)
 end
