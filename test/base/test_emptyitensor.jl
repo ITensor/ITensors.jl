91f3a83556f2c5e95a9366c521e40ffc5262ca00
kmp5VT <kmp5@vt.edu>
2023-09-13 15:06:16 -0400

    format

--- test/base/test_emptyitensor.jl
+++ test/base/test_emptyitensor.jl
@@ -69,29 +69,20 @@ end
 
   @test hassameinds(C, (i'', i))
   storage(C)
-  @test storage(C) isa BlockSparse{
-    UnspecifiedZero,
-    NDTensors.UnallocatedZeros{
-      NDTensors.UnspecifiedZero,1,Tuple{Base.OneTo{Int64}},Vector{UnspecifiedZero}
-    },
-    2,
-  }
+  storage(C)
+  @test storage(C) isa BlockSparse{NDTensors.UnspecifiedZero, NDTensors.UnallocatedZeros{NDTensors.UnspecifiedZero, 1, Tuple{Base.OneTo{Int64}}, Vector{NDTensors.UnspecifiedZero}}, 2}
 
   B = randomITensor(dag(i), i')
 
   C = A' * B
 
   @test hassameinds(C, (i'', i))
-  @test storage(C) isa BlockSparse{
-    Float64,NDTensors.UnallocatedZeros{Float64,1,Tuple{Base.OneTo{Int64}},Vector{Float64}},2
-  }
+  @test storage(C) isa BlockSparse{Float64, NDTensors.UnallocatedZeros{Float64, 1, Tuple{Base.OneTo{Int64}}, Vector{Float64}}, 2}
 
   C = B' * A
 
   @test hassameinds(C, (i'', i))
-  @test storage(C) isa BlockSparse{
-    Float64,NDTensors.UnallocatedZeros{Float64,1,Tuple{Base.OneTo{Int64}},Vector{Float64}},2
-  }
+  @test storage(C) isa  BlockSparse{Float64, NDTensors.UnallocatedZeros{Float64, 1, Tuple{Base.OneTo{Int64}}, Vector{Float64}}, 2}
 
   C = B + A
   @test inds(C) == inds(B)
