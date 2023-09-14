03a9b2764d18f5b505c75e35d610267664dfd7d6
kmp5VT <kmp5@vt.edu>
2023-09-14 11:12:30 -0400

    import -> using. Can use NDTenosrs randn! instead of another function

--- NDTensors/ext/NDTensorsCUDAExt/fill.jl
+++ NDTensors/ext/NDTensorsCUDAExt/fill.jl
@@ -1,12 +1,12 @@
-import NDTensors.SetParameters: set_unspecified_parameters, DefaultParameters
-import NDTensors: leaf_parenttype
+using NDTensors.SetParameters: set_unspecified_parameters, DefaultParameters
+using NDTensors: leaf_parenttype
 
-function NDTensors.generic_randn(arraytype::Type{<:CuArray}, dim::Integer=0)
-  arraytype_specified = set_unspecified_parameters(
-    leaf_parenttype(arraytype), DefaultParameters()
-  )
-  return CUDA.randn(eltype(arraytype_specified), dim)
-end
+# function NDTensors.generic_randn(arraytype::Type{<:CuArray}, dim::Integer=0)
+#   arraytype_specified = set_unspecified_parameters(
+#     leaf_parenttype(arraytype), DefaultParameters()
+#   )
+#   return CUDA.randn(eltype(arraytype_specified), dim)
+# end
 
 function NDTensors.generic_zeros(arraytype::Type{<:CuArray}, dim::Integer=0)
   arraytype_specified = NDTensors.set_unspecified_parameters(
