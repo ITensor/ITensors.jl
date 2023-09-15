97f3b9e21fd56c4eb451da9fcb6102d41451daea
kmp5VT <kmp5@vt.edu>
2023-09-15 10:58:33 -0400

    Update unspecified promote_type

--- NDTensors/src/unspec_eltype/unspecified_zero.jl
+++ NDTensors/src/unspec_eltype/unspecified_zero.jl
@@ -36,8 +36,10 @@ AbstractFloat(::UnspecifiedZero) = zero(AbstractFloat)
 (::UnspecifiedZero / ::UnspecifiedZero) = throw(DivideError())
 -(::UnspecifiedZero) = UnspecifiedZero()
 
-Base.promote_type(::Type{<:UnspecifiedZero}, ElT::Type) = ElT
+Base.promote_type(z::Type{<:UnspecifiedZero}, ElT::Type) = promote_type(ElT, z)
 Base.promote_type(ElT::Type, ::Type{<:UnspecifiedZero}) = ElT
 Base.promote_type(::Type{<:UnspecifiedZero}, ::Type{<:UnspecifiedZero}) = UnspecifiedZero
-Base.promote_type(::Type{<:Complex{<:UnspecifiedZero}}, ElT::Type) = Complex{ElT}
-Base.promote_type(ElT::Type, ::Type{<:Complex{<:UnspecifiedZero}}) = Complex{ElT}
+Base.promote_type(z::Type{<:Complex{<:UnspecifiedZero}}, ElT::Type) = promote_type(ElT, z)
+Base.promote_type(ElT::Type{<:Real}, ::Type{<:Complex{<:UnspecifiedZero}}) = Complex{ElT}
+Base.promote_type(z::Type{<:Complex{<:UnspecifiedZero}}, ElT::Type{<:Complex}) = promote_type(ElT, z)
+Base.promote_type(ElT::Type{<:Complex}, ::Type{<:Complex{<:UnspecifiedZero}}) = ElT
