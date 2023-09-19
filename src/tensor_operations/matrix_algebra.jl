02c3beaf60923866ae86822d5e7cb63b19a497d5
Joseph Tindall <joeytindall94@googlemail.com>
2023-09-19 15:46:03 -0400

    Added customisable functionality for operating solely on the diagonal elements of an ITensor

--- src/tensor_operations/matrix_algebra.jl
+++ src/tensor_operations/matrix_algebra.jl
@@ -87,3 +87,25 @@ function exp(A::ITensor; kwargs...)
   Lis = Ris'
   return exp(A, Lis, Ris; kwargs...)
 end
+
+function map_diag!(f::Function, it_destination::ITensor, it_source::ITensor)
+  return itensor(map_diag!(f, tensor(it_destination), tensor(it_source)))
+end
+map_diag(f::Function, it::ITensor) = map_diag!(f, copy(it), it)
+
+function map_diag!(f::Function, t_destination::Tensor, t_source::Tensor)
+  for i in 1:diaglength(t_destination)
+    setdiagindex!(t_destination, f(getdiagindex(t_source, i)), i)
+  end
+  return t_destination
+end
+map_diag(f::Function, t::Tensor) = map_diag!(f, copy(t), t)
+
+# Convenience functions
+sqrt_diag(it::ITensor) = map_diag(sqrt, it)
+inv_diag(it::ITensor) = map_diag(inv, it)
+invsqrt_diag(it::ITensor) = map_diag(inv ∘ sqrt, it)
+pinv_diag(it::ITensor) = map_diag(pinv, it)
+pinvsqrt_diag(it::ITensor) = map_diag(pinv ∘ sqrt, it)
+sqrtabs_diag(it::ITensor) = map_diag(sqrt ∘ abs, it)
+inv_sqrtabs_diag(it::ITensor) = map_diag(inv ∘ sqrt ∘ abs, it)
