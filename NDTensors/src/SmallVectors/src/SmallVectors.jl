78abf268fe01df61a5fd2f5687a13ef8048ab268
mtfishman <mfishman@flatironinstitute.org>
2023-09-21 18:10:02 -0400

    Remove some old code

--- NDTensors/src/SmallVectors/src/SmallVectors.jl
+++ NDTensors/src/SmallVectors/src/SmallVectors.jl
@@ -1,19 +1,12 @@
 module SmallVectors
   using StaticArrays
 
-  include("StaticArraysExt.jl")
-  using .StaticArraysExt
-
   export SmallVector, MSmallVector, SubSmallVector
 
   struct NotImplemented <: Exception
     msg::String
   end
   NotImplemented() = NotImplemented("Not implemented.")
-
-  struct BufferDimensionMismatch <: Exception
-    msg::String
-  end
 
   include("abstractsmallvector/abstractsmallvector.jl")
   include("abstractsmallvector/deque.jl")
