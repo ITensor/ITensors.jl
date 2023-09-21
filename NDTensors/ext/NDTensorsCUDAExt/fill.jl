89d28375cf403dcd369fca2ed4000d08deb17f1a
kmp5VT <kmp5@vt.edu>
2023-09-21 14:29:15 -0400

    Remove commented code

--- NDTensors/ext/NDTensorsCUDAExt/fill.jl
+++ NDTensors/ext/NDTensorsCUDAExt/fill.jl
@@ -1,13 +1,6 @@
 using NDTensors.SetParameters: set_unspecified_parameters, DefaultParameters
 using NDTensors: leaf_parenttype
 
-# function NDTensors.generic_randn(arraytype::Type{<:CuArray}, dim::Integer=0)
-#   arraytype_specified = set_unspecified_parameters(
-#     leaf_parenttype(arraytype), DefaultParameters()
-#   )
-#   return CUDA.randn(eltype(arraytype_specified), dim)
-# end
-
 function NDTensors.generic_zeros(arraytype::Type{<:CuArray}, dim::Integer=0)
   arraytype_specified = NDTensors.set_unspecified_parameters(
     leaf_parenttype(arraytype), DefaultParameters()
