fefae9d4aa604c5ee1ec3339047250b1906e0df1
Matt Fishman <mtfishman@users.noreply.github.com>
2023-09-28 12:01:48 -0400

    Format
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- NDTensors/src/arraytensor/arraytensor.jl
+++ NDTensors/src/arraytensor/arraytensor.jl
@@ -22,7 +22,9 @@ const MatrixStorageTensor{T,S,I} = Tensor{T,2,S,I} where {S<:MatrixStorage{T}}
 const MatrixOrArrayStorageTensor{T,S,I} =
   Tensor{T,N,S,I} where {N,S<:MatrixOrArrayStorage{T}}
 
-Tensor(storage::MatrixOrArrayStorageTensor, inds::Tuple) = Tensor(NeverAlias(), storage, inds)
+function Tensor(storage::MatrixOrArrayStorageTensor, inds::Tuple)
+  return Tensor(NeverAlias(), storage, inds)
+end
 
 function Tensor(as::AliasStyle, storage::MatrixOrArrayStorage, inds::Tuple)
   return Tensor{eltype(storage),length(inds),typeof(storage),typeof(inds)}(
