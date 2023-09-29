ea0f602cd24307913b797c23b398914f8cd40e51
Matt Fishman <mtfishman@users.noreply.github.com>
2023-09-28 20:34:05 -0400

    [NDTensors] Add `SmallVectors` submodule (#1202)

--- NDTensors/test/runtests.jl
+++ NDTensors/test/runtests.jl
@@ -20,6 +20,7 @@ end
 @safetestset "NDTensors" begin
   @testset "$filename" for filename in [
     "SetParameters.jl",
+    "SmallVectors.jl",
     "linearalgebra.jl",
     "dense.jl",
     "blocksparse.jl",
