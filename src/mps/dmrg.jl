74791ac2639dca68d1bf7c1204f8df73b8417ca1
kmp5VT <kmp5@vt.edu>
2023-10-02 16:39:29 -0400

    unified memory hack [experimental]

--- src/mps/dmrg.jl
+++ src/mps/dmrg.jl
@@ -278,8 +278,8 @@ function dmrg(PH, psi0::MPS, sweeps::Sweeps; kwargs...)
         end
 
         energy = vals[1]
-        phi::ITensor = vecs[1]
-
+        phi::ITensor = itensor(convert(typeof(tensor(phi)), tensor(vecs[1])))
+        
         ortho = ha == 1 ? "left" : "right"
 
         drho = nothing
