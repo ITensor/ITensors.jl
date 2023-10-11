5634b1bfc79b1d329eb22e8ec1759468a73bc1ff
kmp5VT <kmp5@vt.edu>
2023-10-11 15:15:11 -0400

    More robust permutedims functions

--- NDTensors/src/abstractarray/permutedims.jl
+++ NDTensors/src/abstractarray/permutedims.jl
@@ -1,3 +1,23 @@
-function permutedims!!(::Type{<:AbstractArray}, M, perm)
-  return @strided Mdest = permutedims(M, perm)
+function permutedims(::Type{<:Array}, M, perm)
+  return @strided Mdest = Base.permutedims(M, perm)
+end
+
+function permutedims!(::Type{<:Array}, Mdest, ::Type{<:Array}, M, perm)
+  return @strided Mdest .= Base.permutedims(M, perm)
+end
+
+function permutedims!!(::Type{<:Array}, B, ::Type{<:Array}, A, perm, f)
+  @strided B .= f.(B, Base.permutedims(A, perm))
+end
+
+function NDTensors.permutedims(::Type{<:AbstractArray}, M, perm)
+  return Base.permutedims(M, perm)
+end
+
+function permutedims!(::Type{<:AbstractArray}, Mdest, ::Type{<:AbstractArray}, M, perm)
+  return Mdest .= Base.permutedims(M, perm)
+end
+
+function permutedims!!(::Type{<:AbstractArray}, B, ::Type{<:AbstractArray}, A, perm, f)
+  B .= f.(B, Base.permutedims(A, perm))
 end
