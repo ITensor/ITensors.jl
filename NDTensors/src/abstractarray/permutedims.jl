be69b1308530ab0980f613a556e8407b4b375ae7
kmp5VT <kmp5@vt.edu>
2023-10-11 15:19:19 -0400

    Add a comment/idea

--- NDTensors/src/abstractarray/permutedims.jl
+++ NDTensors/src/abstractarray/permutedims.jl
@@ -2,6 +2,7 @@ function permutedims(::Type{<:Array}, M, perm)
   return @strided Mdest = Base.permutedims(M, perm)
 end
 
+## TODO is it possible to do something like f = identity and remove the permutedims!! funciton?
 function permutedims!(::Type{<:Array}, Mdest, ::Type{<:Array}, M, perm)
   return @strided Mdest .= Base.permutedims(M, perm)
 end
