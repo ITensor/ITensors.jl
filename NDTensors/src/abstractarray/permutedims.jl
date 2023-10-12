e4afbadd32afaa1b9b2567027377ed147171dc64
kmp5VT <kmp5@vt.edu>
2023-10-12 09:41:10 -0400

    Make a choke point function for permutedims

--- NDTensors/src/abstractarray/permutedims.jl
+++ NDTensors/src/abstractarray/permutedims.jl
@@ -6,6 +6,10 @@ function permutedims!(::Type{<:AbstractArray}, Mdest, ::Type{<:AbstractArray}, M
   return Mdest .= Base.permutedims(M, perm)
 end
 
+function permutedims!!(B::AbstractArray, A::AbstractArray, perm, f)
+  return permutedims!!(leaf_parenttype(B), B, leaf_parenttype(A), A, perm, f)
+end
+
 function permutedims!!(::Type{<:AbstractArray}, B, ::Type{<:AbstractArray}, A, perm, f)
   return B .= f.(B, Base.permutedims(A, perm))
 end
