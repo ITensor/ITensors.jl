281cef0f8988473ca780d0cf054e7a06203663bd
kmp5VT <kmp5@vt.edu>
2023-10-23 09:13:07 -0400

    Add comment

--- NDTensors/ext/NDTensorsCUDAExt/linearalgebra.jl
+++ NDTensors/ext/NDTensorsCUDAExt/linearalgebra.jl
@@ -12,6 +12,9 @@ function NDTensors.svd_catch_error(A::CuMatrix; alg="JacobiAlgorithm")
   return USV
 end
 
+## TODO Here truncate does logical operations of the values in P
+## So its more efficient to just make it a CPU vector and 
+## convert back to GPU
 function NDTensors.truncate!(P::CuArray; kwargs...)
   cpuP = NDTensors.cpu(P)
   value = NDTensors.truncate!(cpuP; kwargs...)
