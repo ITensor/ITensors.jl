cea22f97269d5bf9bac4160ace7a143513b6fafd
Matt Fishman <mtfishman@users.noreply.github.com>
2023-10-25 10:55:03 -0400

    [NDTensors] Avoid more scalar indexing operations in block sparse GPU code (#1217)

---
+++ NDTensors/ext/NDTensorsMetalExt/append.jl
@@ -0,0 +1,5 @@
+# This circumvents an issues that `MtlArray` can't call `resize!`.
+# TODO: Raise an issue with Metal.jl.
+function NDTensors.append!!(::Type{<:MtlArray}, collection, collections...)
+  return vcat(collection, collections...)
+end
