7915a8e1968be1c008888f265720ebde09c9be59
kmp5VT <kmp5@vt.edu>
2023-10-26 15:15:31 -0400

    Arrays calls permutedims without leaf_parenttype

--- NDTensors/src/arraytensor/array.jl
+++ NDTensors/src/arraytensor/array.jl
@@ -61,8 +61,6 @@ end
 function permutedims!(
   output_array::MatrixOrArrayStorage, array::MatrixOrArrayStorage, perm, f::Function
 )
-  output_array = permutedims!!(
-    leaf_parenttype(output_array), output_array, leaf_parenttype(array), array, perm, f
-  )
+  output_array = permutedims!!(output_array, array, perm, f)
   return output_array
 end
