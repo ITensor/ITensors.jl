f69ece3f31e28efad335ee77c76d4aa6e383326e
Karl Pierce <kmp5@vt.edu>
2023-10-27 15:13:31 -0400

    alphabetical order

--- NDTensors/src/Unwrap/src/Unwrap.jl
+++ NDTensors/src/Unwrap/src/Unwrap.jl
@@ -11,9 +11,10 @@ include("import.jl")
 ## TODO Create functions which take the `Expose` type and launch functions
 ## using that type
 ## Exposed based functions
-include("functions/permutedims.jl")
-include("functions/mul.jl")
 include("functions/copyto.jl")
+include("functions/linearalgebra.jl")
+include("functions/mul.jl")
+include("functions/permutedims.jl")
 
 export IsWrappedArray, is_wrapped_array, parenttype, unwrap_type, expose, Exposed, unexpose
 
