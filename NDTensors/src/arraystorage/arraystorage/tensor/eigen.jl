7c0509b8cd1fc3b0b62f65e62358563bd1bb77ba
mtfishman <mfishman@flatironinstitute.org>
2023-10-30 11:49:56 -0400

    Improve eigen code a bit

--- NDTensors/src/arraystorage/arraystorage/tensor/eigen.jl
+++ NDTensors/src/arraystorage/arraystorage/tensor/eigen.jl
@@ -1,31 +1,28 @@
 # TODO: Rewrite this function to be more modern:
 # 1. List keyword arguments in function signature.
 # 2. Output `Spectrum` as a keyword argument that gets overwritten.
-# 3. Dispatch on `alg`.
-# 4. Remove keyword argument deprecations.
-# 5. Make this into two layers, one that handles indices and one that works with `Matrix`.
-# 6. Use `eltype` instead of `where`.
+# 3. Make this into two layers, one that handles indices and one that works with `AbstractMatrix`.
 function eigen(
-  T::Hermitian{ElT,<:ArrayStorageTensor{ElT,2,IndsT}}; kwargs...
-) where {ElT<:Union{Real,Complex},IndsT}
-  # Keyword argument deprecations
-  use_absolute_cutoff = false
-  if haskey(kwargs, :absoluteCutoff)
-    @warn "In svd, keyword argument absoluteCutoff is deprecated in favor of use_absolute_cutoff"
-    use_absolute_cutoff = get(kwargs, :absoluteCutoff, use_absolute_cutoff)
-  end
-  use_relative_cutoff = true
-  if haskey(kwargs, :doRelCutoff)
-    @warn "In svd, keyword argument doRelCutoff is deprecated in favor of use_relative_cutoff"
-    use_relative_cutoff = get(kwargs, :doRelCutoff, use_relative_cutoff)
-  end
-
-  truncate = haskey(kwargs, :maxdim) || haskey(kwargs, :cutoff)
-  maxdim::Int = get(kwargs, :maxdim, minimum(dims(T)))
-  mindim::Int = get(kwargs, :mindim, 1)
-  cutoff::Union{Nothing,Float64} = get(kwargs, :cutoff, 0.0)
-  use_absolute_cutoff::Bool = get(kwargs, :use_absolute_cutoff, use_absolute_cutoff)
-  use_relative_cutoff::Bool = get(kwargs, :use_relative_cutoff, use_relative_cutoff)
+  T::Hermitian{<:Any,<:ArrayStorageTensor};
+  use_absolute_cutoff=false,
+  use_relative_cutoff=true,
+  maxdim=nothing,
+  mindim=1,
+  cutoff=nothing,
+  # These are getting passed erroneously.
+  # TODO: Make sure they don't get passed down
+  # to here.
+  ishermitian=nothing,
+  which_decomp=nothing,
+  tags=nothing,
+  eigen_perturbation=nothing,
+  ortho=nothing,
+  normalize=nothing,
+  svd_alg=nothing,
+)
+  truncate = !isnothing(maxdim) || !isnothing(cutoff)
+  maxdim = isnothing(maxdim) ? minimum(dims(T)) : maxdim
+  cutoff = isnothing(cutoff) ? zero(eltype(T)) : cutoff
 
   matrixT = matrix(T)
   ## TODO Here I am calling parent to ensure that the correct `any` function
@@ -48,7 +45,7 @@ function eigen(
 
   if truncate
     DM, truncerr, _ = truncate!!(
-      DM; mindim, maxdim, cutoff, use_absolute_cutoff, use_relative_cutoff, kwargs...
+      DM; mindim, maxdim, cutoff, use_absolute_cutoff, use_relative_cutoff
     )
     dD = length(DM)
     if dD < size(VM, 2)
@@ -61,11 +58,15 @@ function eigen(
   spec = Spectrum(DM, truncerr)
 
   # Make the new indices to go onto V
-  l = eltype(IndsT)(dD)
-  r = eltype(IndsT)(dD)
-  Vinds = IndsT((dag(ind(T, 2)), dag(r)))
-  Dinds = IndsT((l, dag(r)))
+  # TODO: Put in a separate function, such as
+  # `rewrap_inds` or something like that.
+  indstype = typeof(inds(T))
+  l = eltype(indstype)(dD)
+  r = eltype(indstype)(dD)
+  Vinds = indstype((dag(ind(T, 2)), dag(r)))
+  Dinds = indstype((l, dag(r)))
   V = tensor(VM, Vinds)
+  # TODO: Replace with `DiagonalArray`.
   D = tensor(Diag(DM), Dinds)
   return D, V, spec
 end
