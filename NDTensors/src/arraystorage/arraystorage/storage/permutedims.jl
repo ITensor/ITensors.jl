abe78eb9d7591078b463935e009b49b026b62de6
kmp5VT <kmp5@vt.edu>
2023-10-31 16:07:46 -0400

    format

--- NDTensors/src/arraystorage/arraystorage/storage/permutedims.jl
+++ NDTensors/src/arraystorage/arraystorage/storage/permutedims.jl
@@ -1,7 +1,6 @@
 function permutedims!(
   output_array::MatrixOrArrayStorage, array::MatrixOrArrayStorage, perm, f::Function
 )
-  output_array = permutedims!!(output_array, array, perm, f
-  )
+  output_array = permutedims!!(output_array, array, perm, f)
   return output_array
 end
