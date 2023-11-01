96a8334f85248ee404e33b4ad1793b6fd44212f9
kmp5VT <kmp5@vt.edu>
2023-11-01 13:00:26 -0400

    I do need this function

--- NDTensors/ext/NDTensorsCUDAExt/linearalgebra.jl
+++ NDTensors/ext/NDTensorsCUDAExt/linearalgebra.jl
@@ -1,14 +1,14 @@
-## I don't think we need this function anymore
-# function NDTensors.svd_catch_error(A::CuMatrix; alg="JacobiAlgorithm")
-#   if alg == "JacobiAlgorithm"
-#     alg = CUDA.CUSOLVER.JacobiAlgorithm()
-#   else
-#     alg = CUDA.CUSOLVER.QRAlgorithm()
-#   end
-#   USV = try
-#     svd(expose(A); alg=alg)
-#   catch
-#     return nothing
-#   end
-#   return USV
-# end
+# # I don't think we need this function anymore
+function NDTensors.svd_catch_error(A::CuMatrix; alg="JacobiAlgorithm")
+  if alg == "JacobiAlgorithm"
+    alg = CUDA.CUSOLVER.JacobiAlgorithm()
+  else
+    alg = CUDA.CUSOLVER.QRAlgorithm()
+  end
+  USV = try
+    svd(expose(A); alg=alg)
+  catch
+    return nothing
+  end
+  return USV
+end
