48a8c159b912c1ff1fb760bff3ba56567be5e3d3
kmp5VT <kmp5@vt.edu>
2023-11-01 17:00:59 -0400

    Add logic to fix CUDA qr with rectangular matrices

--- NDTensors/ext/NDTensorsCUDAExt/linearalgebra.jl
+++ NDTensors/ext/NDTensorsCUDAExt/linearalgebra.jl
@@ -1,13 +1,39 @@
-function NDTensors.svd_catch_error(A::CuMatrix; alg="JacobiAlgorithm")
+function NDTensors.svd_catch_error(A::CuMatrix; alg::String="JacobiAlgorithm")
   if alg == "JacobiAlgorithm"
     alg = CUDA.CUSOLVER.JacobiAlgorithm()
   else
     alg = CUDA.CUSOLVER.QRAlgorithm()
   end
+  return NDTensors.svd_catch_error(A, alg)
+end
+
+function NDTensors.svd_catch_error(A::CuMatrix, ::CUDA.CUSOLVER.JacobiAlgorithm)
   USV = try
-    svd(expose(A); alg=alg)
+    svd(A; alg=CUDA.CUSOLVER.JacobiAlgorithm())
   catch
     return nothing
+  end
+  return USV
+end
+
+function NDTensors.svd_catch_error(A::CuMatrix, ::CUDA.CUSOLVER.QRAlgorithm)
+  s = size(A)
+  if s[1] < s[2]
+    At = copy(Adjoint(A))
+    
+    USV = try 
+      svd(At; alg=CUDA.CUSOLVER.QRAlgorithm())
+    catch
+      return nothing
+    end
+    MV ,MS, MU = USV;
+    USV = SVD(copy(MU), MS, Adjoint(MV))
+  else
+    USV = try 
+      svd(A; alg=CUDA.CUSOLVER.QRAlgorithm())
+    catch
+      return nothing
+    end
   end
   return USV
 end
