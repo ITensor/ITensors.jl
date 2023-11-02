f5c7701e06df386739fd7f543f8fe7af2d08b1a3
kmp5VT <kmp5@vt.edu>
2023-11-02 14:49:04 -0400

    Add type based default_svd_alg

---
+++ NDTensors/ext/NDTensorsCUDAExt/default_kwargs.jl
@@ -0,0 +1,1 @@
+NDTensors.default_svd_alg(a, ::Type{<:CuArray}) = "qr_algorithm"
--- NDTensors/src/default_kwargs.jl
+++ NDTensors/src/default_kwargs.jl
@@ -4,6 +4,6 @@ replace_nothing(value, replacement) = value
 default_maxdim(a) = minimum(size(a))
 default_mindim(a) = true
 default_cutoff(a) = zero(eltype(a))
-default_svd_alg(a) = "divide_and_conquer"
+default_svd_alg(a, ::Type{<:AbstractArray}) = "divide_and_conquer"
 default_use_absolute_cutoff(a) = false
 default_use_relative_cutoff(a) = true
