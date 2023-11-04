b816865a09e4b4d90f7a0913e4d965c742bd43c9
mtfishman <mfishman@flatironinstitute.org>
2023-11-04 07:16:44 -0400

    Block sparse combine cleanup

--- NDTensors/src/arraystorage/blocksparsearray/tensor/combiner/contract.jl
+++ NDTensors/src/arraystorage/blocksparsearray/tensor/combiner/contract.jl
@@ -1,76 +1,58 @@
-# TODO: Make a `CombinerArray`.
 function contract(
-  tensor_src::Tensor{T,N,<:BlockSparseArray{T,N}},
-  tensor_src_labels,
-  tensor_combiner::Tensor{Any,M,<:CombinerArray{M}},
-  tensor_combiner_labels,
+  t_src::Tensor{T,N,<:BlockSparseArray{T,N}},
+  labels_src,
+  t_comb::Tensor{Any,M,<:CombinerArray{M}},
+  labels_comb,
 ) where {T,N,M}
-  array_dest = contract(
-    storage(tensor_src), tensor_src_labels, storage(tensor_combiner), tensor_combiner_labels
-  )
-  tensor_dest_inds =
-    if is_combining(
-      storage(tensor_src),
-      tensor_src_labels,
-      storage(tensor_combiner),
-      tensor_combiner_labels,
-    )
-      contract_combine_inds(
-        tensor_src, tensor_src_labels, tensor_combiner, tensor_combiner_labels
-      )
-    else
-      contract_uncombine_inds(
-        tensor_src, tensor_src_labels, tensor_combiner, tensor_combiner_labels
-      )
-    end
-  return tensor(array_dest, tensor_dest_inds)
+  array_dest = contract(storage(t_src), labels_src, storage(t_comb), labels_comb)
+  inds_dest = if is_combining(storage(t_src), labels_src, storage(t_comb), labels_comb)
+    contract_combine_inds(t_src, labels_src, t_comb, labels_comb)
+  else
+    contract_uncombine_inds(t_src, labels_src, t_comb, labels_comb)
+  end
+  return tensor(array_dest, inds_dest)
 end
 
 function contract(
-  tensor_combiner::Tensor{Any,M,<:CombinerArray{M}},
-  tensor_combiner_labels,
-  tensor_src::Tensor{T,N,<:BlockSparseArray{T,N}},
-  tensor_src_labels,
+  t_comb::Tensor{Any,M,<:CombinerArray{M}},
+  labels_comb,
+  t_src::Tensor{T,N,<:BlockSparseArray{T,N}},
+  labels_src,
 ) where {T,N,M}
-  return contract(tensor_src, tensor_src_labels, tensor_combiner, tensor_combiner_labels)
+  return contract(t_src, labels_src, t_comb, labels_comb)
 end
 
-# TODO: Move to `contract_combine.jl`.
 function contract_combine_inds(
-  tensor_src::Tensor{<:Any,<:Any,<:BlockSparseArray},
-  tensor_src_labels,
-  tensor_combiner::Tensor{<:Any,<:Any,<:CombinerArray},
-  tensor_combiner_labels,
+  t_src::Tensor{<:Any,<:Any,<:BlockSparseArray},
+  labels_src,
+  t_comb::Tensor{<:Any,<:Any,<:CombinerArray},
+  labels_comb,
 )
+
+  # TODO: Not needed? Maybe for uncombining?
   # Get the label marking the combined index
   # By convention the combined index is the first one
   # TODO: Consider storing the location of the combined
   # index in preperation for multiple combined indices
   # TODO: Use `combinedind_label(...)`, `uncombinedind_labels(...)`, etc.
-  cpos_in_tensor_combiner_labels = 1
-  clabel = tensor_combiner_labels[cpos_in_tensor_combiner_labels]
-  c = combinedind(storage(tensor_combiner))
-  labels_uc = deleteat(tensor_combiner_labels, cpos_in_tensor_combiner_labels)
-  tensor_dest_labels = contract_labels(tensor_combiner_labels, tensor_src_labels)
-  cpos_in_tensor_dest_labels = findfirst(==(clabel), tensor_dest_labels)
-  tensor_dest_labels_uc = insertat(
-    tensor_dest_labels, labels_uc, cpos_in_tensor_dest_labels
-  )
-  return contract_inds(
-    inds(tensor_combiner),
-    tensor_combiner_labels,
-    inds(tensor_src),
-    tensor_src_labels,
-    tensor_dest_labels,
-  )
+  ## cpos_in_labels_comb = 1
+  ## clabel = labels_comb[cpos_in_labels_comb]
+  ## c = combinedind(storage(t_comb))
+  ## labels_uc = deleteat(labels_comb, cpos_in_labels_comb)
+  ## cpos_in_labels_dest = findfirst(==(clabel), labels_dest)
+  ## labels_dest_uc = insertat(
+  ##   labels_dest, labels_uc, cpos_in_labels_dest
+  ## )
+
+  labels_dest = contract_labels(labels_comb, labels_src)
+  return contract_inds(inds(t_comb), labels_comb, inds(t_src), labels_src, labels_dest)
 end
 
-# TODO: Move to `contract_uncombine.jl`.
 function contract_uncombine_inds(
-  tensor_src::Tensor{<:Any,<:Any,<:BlockSparseArray},
-  tensor_src_labels,
-  tensor_combiner::Tensor{<:Any,<:Any,<:CombinerArray},
-  tensor_combiner_labels,
+  t_src::Tensor{<:Any,<:Any,<:BlockSparseArray},
+  labels_src,
+  t_comb::Tensor{<:Any,<:Any,<:CombinerArray},
+  labels_comb,
 )
   return error("Not implemented")
 end
