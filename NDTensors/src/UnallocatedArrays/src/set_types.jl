b5ac4cebffed6d55c19490a1390b69bed0f4ad96
kmp5VT <kmp5@vt.edu>
2023-11-08 15:39:54 -0500

    Fix set_types

--- NDTensors/src/UnallocatedArrays/src/set_types.jl
+++ NDTensors/src/UnallocatedArrays/src/set_types.jl
@@ -1,15 +1,17 @@
 ## TODO I have just started working on this, it needs more work still.
 for Typ in (:UnallocatedFill, :UnallocatedZeros)
-  # `SetParameters.jl` overloads.
-  get_parameter(::Type{<:$Typ{P1}}, ::Position{1}) where {P1} = P1
-  get_parameter(::Type{<:$Typ{<:Any,P2}}, ::Position{2}) where {P2} = P2
-  get_parameter(::Type{<:$Typ{<:Any,<:Any,P3}}, ::Position{3}) where {P3} = P3
-  get_parameter(::Type{<:$Typ{<:Any,<:Any,<:Any,P4}}, ::Position{4}) where {P4} = P4
+  @eval begin
+    # `SetParameters.jl` overloads.
+    SetParameters.get_parameter(::Type{<:$Typ{P1}}, ::Position{1}) where {P1} = P1
+    SetParameters.get_parameter(::Type{<:$Typ{<:Any,P2}}, ::Position{2}) where {P2} = P2
+    SetParameters.get_parameter(::Type{<:$Typ{<:Any,<:Any,P3}}, ::Position{3}) where {P3} = P3
+    SetParameters.get_parameter(::Type{<:$Typ{<:Any,<:Any,<:Any,P4}}, ::Position{4}) where {P4} = P4
 
-  default_parameter(::Type{<:$Typ}, ::Position{1}) = UnspecifiedTypes.UnallocatedZeros
-  default_parameter(::Type{<:$Typ}, ::Position{2}) = 0
-  default_parameter(::Type{<:$Typ}, ::Position{3}) = Tuple{}
-  default_parameter(::Type{<:$Typ}, ::Position{4}) = UnspecifiedTypes.UnspecifiedArray
+    SetParameters.default_parameter(::Type{<:$Typ}, ::Position{1}) = UnspecifiedTypes.UnallocatedZeros
+    SetParameters.default_parameter(::Type{<:$Typ}, ::Position{2}) = 0
+    SetParameters.default_parameter(::Type{<:$Typ}, ::Position{3}) = Tuple{}
+    SetParameters.default_parameter(::Type{<:$Typ}, ::Position{4}) = UnspecifiedTypes.UnspecifiedArray
 
-  nparameters(::Type{<:$Typ}) = Val(4)
+    nparameters(::Type{<:$Typ}) = Val(4)
+  end
 end
