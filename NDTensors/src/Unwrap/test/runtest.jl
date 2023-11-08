d0e787494fea3b8b318a0a4b9ae586d7312b302b
kmp5VT <kmp5@vt.edu>
2023-11-08 11:17:26 -0500

    Make tests work for different backend storage types.

--- NDTensors/src/Unwrap/test/runtest.jl
+++ NDTensors/src/Unwrap/test/runtest.jl
@@ -1,22 +1,26 @@
 using Test
 using NDTensors.Unwrap
+using NDTensors
 using LinearAlgebra
 
+include("../../../test/device_list.jl")
 ## Still working on this
 ## TODO add cu
-@testset "Testing Unwrap" begin
-  v = Vector{Float64}(undef, 10)
+@testset "Testing Unwrap" for dev in devices_list(ARGS)
+  v = dev(Vector{Float64}(undef, 10))
   vt = transpose(v)
   va = v'
 
   E = expose(v)
   Et = expose(vt)
   Ea = expose(va)
-  @test typeof(E) == Exposed{Vector{Float64},Vector{Float64}}
+  v_type = typeof(v)
+  e_type = eltype(v)
+  @test typeof(E) == Exposed{v_type,v_type}
   @test typeof(Et) ==
-    Exposed{Vector{Float64},LinearAlgebra.Transpose{Float64,Vector{Float64}}}
+    Exposed{v_type,LinearAlgebra.Transpose{e_type,v_type}}
   @test typeof(Ea) ==
-    Exposed{Vector{Float64},LinearAlgebra.Adjoint{Float64,Vector{Float64}}}
+    Exposed{v_type,LinearAlgebra.Adjoint{e_type,v_type}}
 
   @test parent(E) == v
   @test parent(Et) == v
@@ -32,13 +36,14 @@ using LinearAlgebra
   Et = expose(mt)
   Ea = expose(ma)
 
-  @test typeof(E) == Exposed{Matrix{Float64},Matrix{Float64}}
+  m_type = typeof(m)
+  @test typeof(E) == Exposed{m_type, m_type}
   @test typeof(Et) ==
-    Exposed{Matrix{Float64},LinearAlgebra.Transpose{Float64,Matrix{Float64}}}
+    Exposed{m_type, LinearAlgebra.Transpose{e_type,m_type}}
   @test typeof(Ea) ==
-    Exposed{Matrix{Float64},LinearAlgebra.Adjoint{Float64,Matrix{Float64}}}
+    Exposed{m_type, LinearAlgebra.Adjoint{e_type,m_type}}
 
-  o = Vector{Float32}(undef, 1)
+  o = dev(Vector{Float32})(undef, 1)
   expose(o)[] = 2
   expose(o)[] == 2
 
@@ -57,14 +62,16 @@ using LinearAlgebra
   q, r = Unwrap.qr_positive(expose(mp))
   @test q * r ≈ mp
 
-  square = rand(Float64, (10, 10))
-  ## TODO finish this test
-  l, U = eigen(expose(square))
+  square = NDTensors.cu(rand(Float64, (10, 10)))
+  square = (square + square) ./ 2.
+  ## CUDA only supports Hermitian or Symmetric eigen decompositions
+  ## So I symmetrize square and call symetric here
+  l, U = eigen(expose(Symmetric(square)))
 
   U, S, V, = svd(expose(mp))
   @test U * Diagonal(S) * V' ≈ mp
 
-  cm = fill!(Matrix{Float64}(undef, (2, 2)), 0.0)
+  cm = dev(fill!(Matrix{Float64}(undef, (2, 2)), 0.0))
   mul!(expose(cm), expose(mp), expose(mp'), 1.0, 0.0)
   cm ≈ mp * mp'
 
@@ -76,4 +83,13 @@ using LinearAlgebra
   permutedims!(expose(m), expose(mt), (2, 1), +)
   @test size(m) == (5, 2)
   @test norm(m) == sqrt(6^2 * 10)
+  
+  m = reshape(m, (5,2,1))
+  mt = fill!(similar(m), 3.)
+  m = permutedims(expose(m), (2,1,3))
+  @test size(m) == (2,5,1)
+  permutedims!(expose(m), expose(mt), (2,1,3))
+  @test norm(m) == sqrt(3^2 * 10)
+  permutedims!(expose(m), expose(mt), (2,1,3), -)
+  @test norm(m) == 0
 end
