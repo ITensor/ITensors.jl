fe051c53cba0cd4417b84c9ed7f89519f4367daf
kmp5VT <kmp5@vt.edu>
2023-11-09 09:51:38 -0500

    Import setparameters

--- NDTensors/src/UnallocatedArrays/src/import.jl
+++ NDTensors/src/UnallocatedArrays/src/import.jl
@@ -1,2 +1,3 @@
 import FillArrays: axes, convert, getindex_value, size, sum
 import Base: getindex, copy
+import NDTensors.SetParameters: nparameters, get_parameter, set_parameter, default_parameter
