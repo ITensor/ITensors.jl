fcd199767ff3843e2064ce17bb971adf499048e3
kmp5VT <kmp5@vt.edu>
2023-11-09 12:48:40 -0500

    Some updates

--- NDTensors/src/UnallocatedArrays/src/unallocated_impl.jl
+++ NDTensors/src/UnallocatedArrays/src/unallocated_impl.jl
@@ -8,8 +8,13 @@ for Typ in (:UnallocatedFill, :UnallocatedZeros)
     alloctype(A::$Typ) = alloctype(typeof(A))
     alloctype(Atype::Type{<:$Typ}) = get_parameter(Atype, Position{4}())
 
-    Array(A::$Typ) = alloctype(typeof(A))
+    allocate(A::$Typ) = alloctype(A)(parent(A))
 
+    ## With these functions defined I can print UnallocatedArrays
+    ## compute things like sum and norm, compute the size and length
+    @inline Base.axes(A::$Typ) = axes(parent(A))
+    Base.size(A::$Typ) = size(parent(A))
+    FillArrays.getindex_value(A::$Typ) = FillArrays.getindex_value(parent(A))
     copy(A::$Typ) = A
     ## TODO Implement vec
     # Base.vec(Z::UnallocatedZeros) = typeof(Z)(length(Z))
@@ -17,9 +22,9 @@ for Typ in (:UnallocatedFill, :UnallocatedZeros)
     ## Set parameter functions are working properly
     set_alloctype(F::Type{<:$Typ}, alloc::Type{<:AbstractArray}) =
       set_parameter(F, Position{4}(), alloc)
+
     Base.complex(A::$Typ) = set_alloctype(
       complex(parent(A)), set_eltype(alloctype(A), complex(eltype(alloctype(A))))
     )
   end
-
 end
