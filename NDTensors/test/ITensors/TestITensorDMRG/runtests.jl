d9ed7aa6f6f95f042909be628e85eedf0caea58b
mtfishman <mfishman@flatironinstitute.org>
2023-11-09 15:30:43 -0500

    Cleanup

--- NDTensors/test/ITensors/TestITensorDMRG/runtests.jl
+++ NDTensors/test/ITensors/TestITensorDMRG/runtests.jl
@@ -6,12 +6,9 @@ include("../../device_list.jl")
 
 @testset "Testing DMRG different backends" begin
   for dev in devices_list(ARGS),
-    N in [4, 10],
+    conserve_qns in [false, true],
     elt in (Float32, ComplexF32, Float64, ComplexF64),
-    conserve_qns in [false, true]
-
-    @show dev, elt
-    @show TestITensorDMRG.is_supported_eltype(dev, elt)
+    N in [4, 10]
 
     if !TestITensorDMRG.is_supported_eltype(dev, elt)
       continue
