8d0135d6c1e9d42b68719f856a8c8c423115fe9c
kmp5VT <kmp5@vt.edu>
2023-11-09 10:58:50 -0500

    Yes we should force Alloc to be the same dimensions as the `Fill`

--- NDTensors/src/UnallocatedArrays/src/unallocatedfill.jl
+++ NDTensors/src/UnallocatedArrays/src/unallocatedfill.jl
@@ -1,6 +1,6 @@
 ## TODO All constructors not fully implemented but so far it matches the 
 ## constructors found in `FillArrays`. Need to fix io
-struct UnallocatedFill{ElT,N,Axes,Alloc<:AbstractArray} <:
+struct UnallocatedFill{ElT,N,Axes,Alloc<:AbstractArray{ElT, N}} <:
        FillArrays.AbstractFill{ElT,N,Axes}
   f::FillArrays.Fill{ElT,N,Axes}
   ## TODO use `set_parameters` as constructor to these types
