df752fe66a9f0d2eb8136fafbfe3455141252f9f
Karl Pierce <kmp5@vt.edu>
2023-11-09 15:01:24 -0500

    Remove @eval and fix the complex function

--- NDTensors/src/UnallocatedArrays/src/unallocated_impl.jl
+++ NDTensors/src/UnallocatedArrays/src/unallocated_impl.jl
@@ -1,30 +1,26 @@
-for Typ in (:UnallocatedFill, :UnallocatedZeros)
-  ## Here are functions specifically defined for UnallocatedArrays
+## Here are functions specifically defined for UnallocatedArrays
   ## not implemented by FillArrays
-  ## TODO use set_parameters/get_parameters instead of alloctype and other 
-  ## type info functions.
   ## TODO determine min number of functions needed to be forwarded
-  @eval begin
-    alloctype(A::$Typ) = alloctype(typeof(A))
-    alloctype(Atype::Type{<:$Typ}) = get_parameter(Atype, Position{4}())
 
-    allocate(A::$Typ) = alloctype(A)(parent(A))
+alloctype(A::Union{<:UnallocatedFill,<:UnallocatedZeros}) = alloctype(typeof(A))
+alloctype(Atype::Type{<:Union{<:UnallocatedFill, <:UnallocatedZeros}}) = get_parameter(Atype, Position{4}())
 
-    ## With these functions defined I can print UnallocatedArrays
-    ## compute things like sum and norm, compute the size and length
-    @inline Base.axes(A::$Typ) = axes(parent(A))
-    Base.size(A::$Typ) = size(parent(A))
-    FillArrays.getindex_value(A::$Typ) = FillArrays.getindex_value(parent(A))
-    copy(A::$Typ) = A
-    ## TODO Implement vec
-    # Base.vec(Z::UnallocatedZeros) = typeof(Z)(length(Z))
-    ## TODO Still working here I am not sure these functions and the
-    ## Set parameter functions are working properly
-    set_alloctype(F::Type{<:$Typ}, alloc::Type{<:AbstractArray}) =
-      set_parameter(F, Position{4}(), alloc)
+allocate(A::Union{<:UnallocatedFill,<:UnallocatedZeros}) = alloctype(A)(parent(A))
 
-    Base.complex(A::$Typ) = set_alloctype(
-      complex(parent(A)), set_eltype(alloctype(A), complex(eltype(alloctype(A))))
-    )
-  end
-end
+## TODO Still working here I am not sure these functions and the
+## Set parameter functions are working properly
+set_alloctype(F::Type{<:Union{<:UnallocatedFill,<:UnallocatedZeros}}, alloc::Type{<:AbstractArray}) =
+  set_parameter(F, Position{4}(), alloc)
+set_eltype(F::Type{<:Union{<:UnallocatedFill,<:UnallocatedZeros}}, elt)
+
+## With these functions defined I can print UnallocatedArrays
+## compute things like sum and norm, compute the size and length
+@inline Base.axes(A::Union{<:UnallocatedFill,<:UnallocatedZeros}) = axes(parent(A))
+Base.size(A::Union{<:UnallocatedFill,<:UnallocatedZeros}) = size(parent(A))
+FillArrays.getindex_value(A::Union{<:UnallocatedFill,<:UnallocatedZeros}) = FillArrays.getindex_value(parent(A))
+Base.copy(A::Union{<:UnallocatedFill,<:UnallocatedZeros}) = A
+## Can't actually use NDTensors.set_eltype because it doesn't 
+## exist yet in thie area
+Base.complex(A::Union{<:UnallocatedFill,<:UnallocatedZeros}) = set_alloctype(complex(parent(A)), set_parameter(alloctype(A), Position{1}(), complex(eltype(A))))
+#     ## TODO Implement vec
+#     # Base.vec(Z::UnallocatedZeros) = typeof(Z)(length(Z))
