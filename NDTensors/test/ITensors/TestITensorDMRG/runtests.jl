9cce9e2dafbdbf31edac69cc858ecc5f940c4bfa
mtfishman <mfishman@flatironinstitute.org>
2023-11-09 16:38:47 -0500

    More information in tests

--- NDTensors/test/ITensors/TestITensorDMRG/runtests.jl
+++ NDTensors/test/ITensors/TestITensorDMRG/runtests.jl
@@ -4,15 +4,13 @@ include("TestITensorDMRG.jl")
 
 include("../../device_list.jl")
 
-@testset "Testing DMRG different backends" begin
-  for dev in devices_list(ARGS),
-    conserve_qns in [false, true],
-    elt in (Float32, ComplexF32, Float64, ComplexF64),
-    N in [4, 10]
+@testset "Test DMRG $dev, $conserve_qns, $elt, $N" for dev in devices_list(ARGS),
+  conserve_qns in [false, true],
+  elt in (Float32, ComplexF32, Float64, ComplexF64),
+  N in [4, 10]
 
-    if !TestITensorDMRG.is_supported_eltype(dev, elt)
-      continue
-    end
-    TestITensorDMRG.test_dmrg(elt, N; dev, conserve_qns)
+  if !TestITensorDMRG.is_supported_eltype(dev, elt)
+    continue
   end
+  TestITensorDMRG.test_dmrg(elt, N; dev, conserve_qns)
 end
