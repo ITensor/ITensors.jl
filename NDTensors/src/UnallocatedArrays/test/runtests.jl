ab7e910964a8c6a6f8b2f960501e0ca209bf1217
kmp5VT <kmp5@vt.edu>
2023-11-09 12:48:53 -0500

    Add a unittest

---
+++ NDTensors/src/UnallocatedArrays/test/runtests.jl
@@ -0,0 +1,36 @@
+using FillArrays
+using NDTensors.UnallocatedArrays
+using LinearAlgebra
+using Test
+
+begin
+    z = Zeros{Float64}((2,3))
+    Z = UnallocatedZeros{eltype(z), ndims(z), typeof(axes(z)), Matrix{eltype(z)}}(z)
+
+    @test size(Z) == (2,3)
+    @test length(Z) == 6
+    @test sum(Z) == 0
+    @test norm(Z) == 0
+    @test Z[2,3] == 0
+    @test allocate(Z) isa Matrix{eltype(z)}
+    Zp = set_alloctype(z, Matrix{eltype(z)})
+    @test Zp == Z
+    Zc = copy(Z)
+    @test Zc == Z
+
+    f = Fill{Float64}(3., (2,3,4))
+    F = UnallocatedFill{eltype(f), ndims(f), typeof(axes(f)), Array{eltype(f), ndims(f)}}(f)
+
+    @test size(F) == (2,3,4)
+    @test length(F) == 24
+    @test sum(F) ≈ 3 * 24
+    @test norm(F) ≈ sqrt(3^2 * 24)
+    @test F[2,3,1] == 3.
+    @test allocate(F) isa Array{eltype(z), 3}
+    Fp = allocate(F)
+    @test norm(Fp) ≈ norm(F)
+    Fp = set_alloctype(f, Array{eltype(f), ndims(f)})
+    @test Fp == F
+    Fc = copy(F)
+    @test Fc == F
+end
