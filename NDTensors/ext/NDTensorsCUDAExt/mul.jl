efcde0c71d323afb0558b21e8e8bea2560b8ccb5
kmp5VT <kmp5@vt.edu>
2023-11-09 16:03:08 -0500

    format

--- NDTensors/ext/NDTensorsCUDAExt/mul.jl
+++ NDTensors/ext/NDTensorsCUDAExt/mul.jl
@@ -1,3 +1,11 @@
-function LinearAlgebra.mul!(C::Exposed{<:CuArray, <:LinearAlgebra.Transpose}, A::Exposed{<:CuArray}, B::Exposed{<:CuArray}, α, β)
-    mul!(expose(parent(C)), expose(transpose(unexpose(B))), expose(transpose(unexpose(A))), α, β)
+function LinearAlgebra.mul!(
+  C::Exposed{<:CuArray,<:LinearAlgebra.Transpose},
+  A::Exposed{<:CuArray},
+  B::Exposed{<:CuArray},
+  α,
+  β,
+)
+  return mul!(
+    expose(parent(C)), expose(transpose(unexpose(B))), expose(transpose(unexpose(A))), α, β
+  )
 end
