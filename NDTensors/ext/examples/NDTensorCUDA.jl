c40a76f5a876d667b5e445fb6eab8bc0dd0b9511
kmp5VT <kmp5@vt.edu>
2023-11-13 16:32:46 -0500

    Add allowscalar theres an issue in zygote

--- NDTensors/ext/examples/NDTensorCUDA.jl
+++ NDTensors/ext/examples/NDTensorCUDA.jl
@@ -61,7 +61,7 @@ function main()
   #Currently this code fails with CUDA.allowscalar(false)
   # Because of outer calling the _gemm! function which calls a 
   # generic implementation
-  grad = gradient(f, cA, cB, cC, cD)
+  CUDA.@allowscalar grad = gradient(f, cA, cB, cC, cD)
   @test NDTensors.cpu(cB * cC * cD) ≈ NDTensors.cpu(grad[1])
   @test (cB * cC * cD) ≈ grad[1]
   # Create a tuple of indices
