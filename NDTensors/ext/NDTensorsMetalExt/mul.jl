ab39bed7af0e15ae9499fe3b92f64e129f2fe124
Karl Pierce <kmp5@vt.edu>
2023-11-16 17:40:35 -0500

    Same issue on Metal and CUDA

--- NDTensors/ext/NDTensorsMetalExt/mul.jl
+++ NDTensors/ext/NDTensorsMetalExt/mul.jl
@@ -19,3 +19,21 @@ function LinearAlgebra.mul!(
   mul!(CM', BM', AM', α, β)
   return unexpose(CM)
 end
+
+## Fix issue in Metal.jl where it cannot distinguish Transpose{Reshape{Adjoint{CuArray}}}
+## as a CuArray and calls generic matmul
+function LinearAlgebra.mul!(
+  CM::Exposed{<:MtlArray},
+  AM::Exposed{<:MtlArray},
+  BM::Exposed{
+    <:MtlArray,
+    <:LinearAlgebra.Transpose{
+      <:Any,<:Base.ReshapedArray{<:Any,<:Any,<:LinearAlgebra.Adjoint}
+    },
+  },
+  α,
+  β,
+)
+  mul!(CM, AM, expose(transpose(copy(expose(parent(BM))))), α, β)
+  return unexpose(CM)
+end
