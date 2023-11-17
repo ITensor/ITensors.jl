911542d53c559a9089a7022d197ac8ff5cc69095
Karl Pierce <kmp5@vt.edu>
2023-11-17 13:11:21 -0500

    Format

--- NDTensors/ext/NDTensorsMetalExt/permutedims.jl
+++ NDTensors/ext/NDTensorsMetalExt/permutedims.jl
@@ -1,4 +1,4 @@
-function Base.permutedims(E::Exposed{<:MtlArray, <:Base.ReshapedArray}, perm)
+function Base.permutedims(E::Exposed{<:MtlArray,<:Base.ReshapedArray}, perm)
   A = copy(E)
   return permutedims(expose(A), perm)
 end
@@ -14,7 +14,12 @@ end
 ## Theres an issue in metal that `ReshapedArray' wrapped arrays cannot be permuted
 ## To get around this copy and permute Esrc, reshape to the size of Edest's parent
 ## and broadcast into the parent.
-function Base.permutedims!(Edest::Exposed{<:MtlArray, <:Base.ReshapedArray}, Esrc::Exposed{<:MtlArray, <:Base.ReshapedArray}, perm, f)
+function Base.permutedims!(
+  Edest::Exposed{<:MtlArray,<:Base.ReshapedArray},
+  Esrc::Exposed{<:MtlArray,<:Base.ReshapedArray},
+  perm,
+  f,
+)
   Aperm = unwrap_type(Esrc)(reshape(permutedims(Esrc, perm), size(parent(Edest))))
   parent(Edest) .= f.(parent(Edest), Aperm)
   return unexpose(Edest)
