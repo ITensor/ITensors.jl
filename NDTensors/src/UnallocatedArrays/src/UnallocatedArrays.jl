8925888002e8b315f4f2abe2ec5890857a64f44f
kmp5VT <kmp5@vt.edu>
2023-11-20 09:56:01 -0500

    Add UnallocatedArray.jl

--- NDTensors/src/UnallocatedArrays/src/UnallocatedArrays.jl
+++ NDTensors/src/UnallocatedArrays/src/UnallocatedArrays.jl
@@ -5,10 +5,10 @@ using NDTensors.SetParameters
 
 include("import.jl")
 
+include("abstractunallocatedarray.jl")
 include("unallocatedfill.jl")
 include("unallocatedzeros.jl")
 include("set_types.jl")
-include("unallocated_impl.jl")
 
 export UnallocatedFill, UnallocatedZeros, alloctype, set_alloctype, allocate
 end
