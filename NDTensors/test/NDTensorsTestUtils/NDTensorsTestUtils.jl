6780956383641a832c6c210ea1063fc5ee20eb5b
Karl Pierce <kmp5@vt.edu>
2023-11-20 12:46:14 -0500

    Create NDTensorsTestUtils

---
+++ NDTensors/test/NDTensorsTestUtils/NDTensorsTestUtils.jl
@@ -0,0 +1,11 @@
+module NDTensorsTestUtils
+
+  using NDTensors
+
+  include("device_list.jl")
+  include("is_suppoted_eltype.jl")
+
+  default_rtol(elt::Type) = 10^(0.75 * log10(eps(real(elt))))
+
+  export default_rtol, is_supported_eltype, devices_list;
+end
