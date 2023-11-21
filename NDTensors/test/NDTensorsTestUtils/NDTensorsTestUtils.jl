c61bc5b17b2dc94fa733f205e70de4ff3c9fcd3b
Karl Pierce <kmp5@vt.edu>
2023-11-21 12:03:05 -0500

    Fix spelling mistake

--- NDTensors/test/NDTensorsTestUtils/NDTensorsTestUtils.jl
+++ NDTensors/test/NDTensorsTestUtils/NDTensorsTestUtils.jl
@@ -3,9 +3,8 @@ module NDTensorsTestUtils
 using NDTensors
 
 include("device_list.jl")
-include("is_suppoted_eltype.jl")
+include("is_supported_eltype.jl")
 
 default_rtol(elt::Type) = 10^(0.75 * log10(eps(real(elt))))
 
-export default_rtol, is_supported_eltype, devices_list
 end
