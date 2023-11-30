8a5a3d7fdea665bf6393db92fdcc0f3cfa406547
kmp5VT <kmp5@vt.edu>
2023-11-30 13:46:11 -0500

    format

--- NDTensors/test/arraytensor/runtests.jl
+++ NDTensors/test/arraytensor/runtests.jl
@@ -1,8 +1,8 @@
 @eval module $(gensym())
-  using Test: @testset
-  @testset "Tensor wrapping AbstractArrays $(f)" for f in [
-    "array.jl", "blocksparsearray.jl", "diagonalarray.jl"
-  ]
-    include(f)
-  end
+using Test: @testset
+@testset "Tensor wrapping AbstractArrays $(f)" for f in [
+  "array.jl", "blocksparsearray.jl", "diagonalarray.jl"
+]
+  include(f)
+end
 end
