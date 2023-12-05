a905a697b25309117528cbbe1fc4f9dd896ab781
mtfishman <mfishman@flatironinstitute.org>
2023-12-05 09:57:59 -0500

    Small sparse matmul refactor

--- NDTensors/src/lib/SparseArrayInterface/src/sparsearrayinterface/SparseArrayInterfaceLinearAlgebraExt.jl
+++ NDTensors/src/lib/SparseArrayInterface/src/sparsearrayinterface/SparseArrayInterfaceLinearAlgebraExt.jl
@@ -2,6 +2,13 @@ using LinearAlgebra: mul!, norm
 
 sparse_norm(a::AbstractArray, p::Real=2) = norm(sparse_storage(a))
 
+function mul_indices(I1::CartesianIndex{2}, I2::CartesianIndex{2})
+  if I1[2] ≠ I2[1]
+    return nothing
+  end
+  return CartesianIndex(I1[1], I2[2])
+end
+
 function default_mul!!(
   a_dest::AbstractMatrix,
   a1::AbstractMatrix,
@@ -30,8 +37,8 @@ function sparse_mul!(
 )
   for I1 in stored_indices(a1)
     for I2 in stored_indices(a2)
-      if Tuple(I1)[2] == Tuple(I2)[1]
-        I_dest = CartesianIndex(Tuple(I1)[1], Tuple(I2)[2])
+      I_dest = mul_indices(I1, I2)
+      if !isnothing(I_dest)
         a_dest[I_dest] = mul!!(a_dest[I_dest], a1[I1], a2[I2], α, β)
       end
     end
