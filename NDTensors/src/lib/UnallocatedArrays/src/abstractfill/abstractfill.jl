4bd30283c96b30ba57ac4be847bb036aa4b87b42
Karl Pierce <kmp5@vt.edu>
2023-12-13 12:29:37 -0500

    Assue alloc defines constructor for type

--- NDTensors/src/lib/UnallocatedArrays/src/abstractfill/abstractfill.jl
+++ NDTensors/src/lib/UnallocatedArrays/src/abstractfill/abstractfill.jl
@@ -2,10 +2,9 @@ using FillArrays: AbstractFill
 using NDTensors.SetParameters: Position, get_parameter, set_parameters
 ## Here are functions specifically defined for UnallocatedArrays
 ## not implemented by FillArrays
-## TODO determine min number of functions needed to be forwarded
+## TODO this might need a more generic name maybe like compute unit
 function alloctype(A::AbstractFill)
-  al = A.alloc
-  return (al isa Type ? al : typeof(al))
+  return A.alloc
 end
 
 ## TODO this fails if the parameter is a type
