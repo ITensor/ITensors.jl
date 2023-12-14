fcfd54cc753c6602191654f0d8508c53fb126d3f
Miles <miles.stoudenmire@gmail.com>
2023-12-13 23:18:28 -0500

    Move tests of categories to use Label

--- NDTensors/src/lib/Sectors/test/runtests.jl
+++ NDTensors/src/lib/Sectors/test/runtests.jl
@@ -1,20 +1,20 @@
 import NDTensors.Sectors:
   ⊗,
   ⊕,
-  U,
+  category,
+  Fib,
+  Ising,
+  istrivial,
+  Label,
+  nactive,
+  name,
+  Sector,
   SU,
   SUd,
   SUz,
-  Z,
-  Fib,
-  Ising,
-  Sector,
-  nactive,
-  Label,
-  category,
-  name,
-  istrivial,
-  values
+  U,
+  values,
+  Z
 using Test
 
 @testset "Test Label system" begin
@@ -37,48 +37,9 @@ using Test
     @test l1 ⊗ l1 == [l2]
   end
 
-  @testset "SU(2) Labels" begin
-    l0 = Label(SU(2), 0)
-    l½ = Label(SU(2), 1//2)
-    l1 = Label(SU(2), 1)
-    l3_2 = Label(SU(2), 3//2)
-
-    @test istrivial(l0)
-    @test !istrivial(l½)
-    @test !istrivial(l1)
-
-    @test l0 ⊗ l½ == [l½]
-    @test l0 ⊗ l1 == [l1]
-    @test l½ ⊗ l½ == l0 ⊕ l1
-    @test l½ ⊗ l1 == l½ ⊕ l3_2
-  end
-
-  @testset "Named Labels" begin
-    n0 = Label("N", U(1), 0)
-    n1 = Label("N", U(1), 1)
-    @test n0 ⊗ n0 == [n0]
-    @test n0 ⊗ n1 == [n1]
-    @test name(first(n0 ⊗ n0)) == "N"
-  end
-end
-
-@testset "Test Sector System" begin
-  @testset "U(1)" begin
-    q0 = Sector(U(1), 0)
-    q1 = Sector(U(1), 1)
-    q2 = Sector(U(1), 2)
-    q3 = Sector(U(1), 3)
-
-    @test q0 ⊗ q0 == [q0]
-    @test q0 ⊗ q1 == [q1]
-    @test q1 ⊗ q0 == [q1]
-    @test q0 ⊗ q2 == [q2]
-    @test q1 ⊗ q2 == [q3]
-  end
-
   @testset "Ƶ_2" begin
-    z0 = Sector(Z(2), 0)
-    z1 = Sector(Z(2), 1)
+    z0 = Label(Z(2), 0)
+    z1 = Label(Z(2), 1)
 
     @test z0 ⊗ z0 == [z0]
     @test z0 ⊗ z1 == [z1]
@@ -87,9 +48,9 @@ end
   end
 
   @testset "Ƶ_3" begin
-    z0 = Sector(Z(3), 0)
-    z1 = Sector(Z(3), 1)
-    z2 = Sector(Z(3), 2)
+    z0 = Label(Z(3), 0)
+    z1 = Label(Z(3), 1)
+    z2 = Label(Z(3), 2)
 
     @test z0 ⊗ z0 == [z0]
     @test z0 ⊗ z1 == [z1]
@@ -100,20 +61,22 @@ end
   end
 
   @testset "SU(2)" begin
-    j0 = Sector(SU(2), 0)
-    j½ = Sector(SU(2), 1//2)
-    j1 = Sector(SU(2), 1)
-    j3_2 = Sector(SU(2), 3//2)
-    j2 = Sector(SU(2), 2)
+    j0 = Label(SU(2), 0)
+    j½ = Label(SU(2), 1//2)
+    j1 = Label(SU(2), 1)
+    j3_2 = Label(SU(2), 3//2)
+    j2 = Label(SU(2), 2)
+
+    @test istrivial(j0)
+    @test !istrivial(j½)
+    @test !istrivial(j1)
 
     @test j0 ⊗ j0 == [j0]
     @test j0 ⊗ j½ == [j½]
     @test j0 ⊗ j1 == [j1]
-
     @test j½ ⊗ j½ == [j0, j1]
     @test j½ ⊗ j½ == j0 ⊕ j1
     @test j½ ⊗ j1 == j½ ⊕ j3_2
-    @test j1 ⊗ j1 == j0 ⊕ j1 ⊕ j2
   end
 
   #
@@ -123,11 +86,11 @@ end
   # each representation
   #
   @testset "SUd(2)" begin
-    d1 = Sector(SUd(2), 1) # spin 0
-    d2 = Sector(SUd(2), 2) # spin 1/2
-    d3 = Sector(SUd(2), 3) # spin 1
-    d4 = Sector(SUd(2), 4) # spin 3/2
-    d5 = Sector(SUd(2), 5) # spin 2
+    d1 = Label(SUd(2), 1) # spin 0
+    d2 = Label(SUd(2), 2) # spin 1/2
+    d3 = Label(SUd(2), 3) # spin 1
+    d4 = Label(SUd(2), 4) # spin 3/2
+    d5 = Label(SUd(2), 5) # spin 2
 
     @test d1 ⊗ d1 == [d1]
     @test d1 ⊗ d2 == [d2]
@@ -139,9 +102,9 @@ end
   end
 
   @testset "Ising" begin
-    ı = Sector(Ising, "1")
-    σ = Sector(Ising, "σ")
-    ψ = Sector(Ising, "ψ")
+    ı = Label(Ising, "1")
+    σ = Label(Ising, "σ")
+    ψ = Label(Ising, "ψ")
 
     @test ı ⊗ ı == [ı]
     @test ı ⊗ σ == [σ]
@@ -149,10 +112,6 @@ end
     @test σ ⊗ σ == ı ⊕ ψ
     @test ψ ⊗ σ == [σ]
     @test ψ ⊗ ψ == [ı]
-
-    @test Sector(Ising, 0) == ı
-    @test Sector(Ising, 1//2) == σ
-    @test Sector(Ising, 1) == ψ
   end
 
   @testset "Fibonacci" begin
@@ -168,15 +127,15 @@ end
   end
 
   @testset "SU(2) with z component" begin
-    q½p = Sector("J", SUz(2), (1//2, 1//2))
-    q½m = Sector("J", SUz(2), (1//2, -1//2))
-    q1p = Sector("J", SUz(2), (1, +1))
-    q10 = Sector("J", SUz(2), (1, 0))
-    q1m = Sector("J", SUz(2), (1, -1))
-    q00 = Sector("J", SUz(2), (0, 0))
-    q22 = Sector("J", SUz(2), (2, +2))
-    q21 = Sector("J", SUz(2), (2, +1))
-    q20 = Sector("J", SUz(2), (2, 0))
+    q½p = Label(SUz(2), (1//2, 1//2))
+    q½m = Label(SUz(2), (1//2, -1//2))
+    q1p = Label(SUz(2), (1, +1))
+    q10 = Label(SUz(2), (1, 0))
+    q1m = Label(SUz(2), (1, -1))
+    q00 = Label(SUz(2), (0, 0))
+    q22 = Label(SUz(2), (2, +2))
+    q21 = Label(SUz(2), (2, +1))
+    q20 = Label(SUz(2), (2, 0))
 
     @test q½p ⊗ q½p == [q1p]
     @test q½p ⊗ q½m == q00 ⊕ q10
@@ -187,6 +146,16 @@ end
     @test q1p ⊗ q1m == q00 ⊕ q10 ⊕ q20
   end
 
+  @testset "Named Labels" begin
+    n0 = Label("N", U(1), 0)
+    n1 = Label("N", U(1), 1)
+    @test n0 ⊗ n0 == [n0]
+    @test n0 ⊗ n1 == [n1]
+    @test name(first(n0 ⊗ n0)) == "N"
+  end
+end
+
+@testset "Test Sector System" begin
   @testset "Multiple U(1)'s" begin
     q00 = Sector()
     q10 = Sector("A", U(1), 1)
@@ -203,7 +172,8 @@ end
     q0 = Sector()
     q0h = Sector(("J", SU(2), 1//2))
     q10 = Sector(("N", U(1), 1), ("J", SU(2), 0))
-    q1h = Sector(("N", U(1), 1), ("J", SU(2), 1//2))
+    # Put names in reverse order sometimes:
+    q1h = Sector(("J", SU(2), 1//2), ("N", U(1), 1))
     q11 = Sector(("N", U(1), 1), ("J", SU(2), 1))
     q20 = Sector(("N", U(1), 2))
     q2h = Sector(("N", U(1), 2), ("J", SU(2), 1//2))
