aabd4e93dece930c8d85bbe479a472214a6a2976
Karl Pierce <kmp5@vt.edu>
2023-12-22 12:37:45 -0500

    Clean up set types with Unspecify_parameters and UnallocatedArray

--- NDTensors/src/lib/UnallocatedArrays/src/set_types.jl
+++ NDTensors/src/lib/UnallocatedArrays/src/set_types.jl
@@ -4,35 +4,42 @@ using NDTensors.UnspecifiedTypes: UnspecifiedArray, UnspecifiedNumber, Unspecifi
 ## TODO All I need to do is overload AbstractFill functions with 4 parameters
 # `SetParameters.jl` overloads.
 function SetParameters.get_parameter(
-  ::Type{<:UnallocatedFill{<:Any,<:Any,<:Any,P4}}, ::Position{4}
-) where {P4}
-  return P4
-end
-function SetParameters.get_parameter(
-  ::Type{<:UnallocatedZeros{<:Any,<:Any,<:Any,P4}}, ::Position{4}
+  ::Type{<:UnallocatedArray{<:Any,<:Any,<:Any,P4}}, ::Position{4}
 ) where {P4}
   return P4
 end
 
 # ## Setting paramaters
 function SetParameters.set_parameter(
-  T::Type{<:UnallocatedFill{P1,P2,P3,<:Any}}, ::Position{4}, P4::Type{<:AbstractArray}
-) where {P1,P2,P3}
-  return T{P4}
+  T::Type{<:UnallocatedArray{P,P2,P3,P4}}, ::Position{1}, P1
+) where {P,P2,P3,P4}
+  return unspecify_parameters(T){P1,P2,P3,P4}
 end
+
 function SetParameters.set_parameter(
-  T::Type{<:UnallocatedZeros{P1,P2,P3,<:Any}}, ::Position{4}, P4::Type{<:AbstractArray}
+  T::Type{<:UnallocatedArray{P1,P,P3,P4}}, ::Position{2}, P2
+) where {P1,P,P3,P4}
+  return unspecify_parameters(T){P1,P2,P3,P4}
+end
+
+function SetParameters.set_parameter(
+  T::Type{<:UnallocatedArray{P1,P2,P,P4}}, ::Position{3}, P3
+) where {P1,P2,P,P4}
+  return unspecify_parameters(T){P1,P2,P3,P4}
+end
+
+function SetParameters.set_parameter(
+  T::Type{<:UnallocatedArray{P1,P2,P3}}, ::Position{4}, P4
 ) where {P1,P2,P3}
-  return T{P4}
+  return unspecify_parameters(T){P1,P2,P3,P4}
 end
 
 # ## default parameters
-function SetParameters.default_parameter(::Type{<:UnallocatedFill}, ::Position{4})
-  return UnspecifiedArray{UnspecifiedNumber{UnspecifiedZero},0}
-end
-function SetParameters.default_parameter(::Type{<:UnallocatedZeros}, ::Position{4})
+function SetParameters.default_parameter(::Type{<:UnallocatedArray}, ::Position{4})
   return UnspecifiedArray{UnspecifiedNumber{UnspecifiedZero},0}
 end
 
-SetParameters.nparameters(::Type{<:UnallocatedFill}) = Val(4)
-SetParameters.nparameters(::Type{<:UnallocatedZeros}) = Val(4)
+SetParameters.nparameters(::Type{<:UnallocatedArray}) = Val(4)
+
+unspecify_parameters(::Type{<:UnallocatedFill}) = UnallocatedFill
+unspecify_parameters(::Type{<:UnallocatedZeros}) = UnallocatedZeros
