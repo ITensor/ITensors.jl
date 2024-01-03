027902a2092e2f95528b1109cff6284ac757bf56
Miles <miles.stoudenmire@gmail.com>
2024-01-02 22:09:08 -0500

    Split group and category definitions

--- NDTensors/src/lib/Sectors/src/Sectors.jl
+++ NDTensors/src/lib/Sectors/src/Sectors.jl
@@ -4,6 +4,7 @@ using ..SortedSets
 import ..SortedSets: AbstractSmallSet
 
 include("abstractcategory.jl")
+include("group_definitions.jl")
 include("category_definitions.jl")
 include("abstractnamedset.jl")
 include("sector.jl")
