601ff2a1620a1e3e8852e930bb2acfeaf304ce41
mtfishman <mfishman@flatironinstitute.org>
2024-01-08 17:43:29 -0500

    Start working on SparseMatrixCSC compatibility

--- NDTensors/src/lib/SparseArrayInterface/src/abstractsparsearray/SparseArrayInterfaceSparseArraysExt.jl
+++ NDTensors/src/lib/SparseArrayInterface/src/abstractsparsearray/SparseArrayInterfaceSparseArraysExt.jl
@@ -1,5 +1,9 @@
-using SparseArrays: SparseArrays
+using SparseArrays: SparseArrays, SparseMatrixCSC
 using ..SparseArrayInterface: nstored
 
 # Julia Base `AbstractSparseArray` interface
 SparseArrays.nnz(a::AbstractSparseArray) = nstored(a)
+
+sparse_storage(a::SparseMatrixCSC) = error("Not implemented")
+storage_index_to_index(a::SparseMatrixCSC, I) = error("Not implemented")
+index_to_storage_index(a::SparseMatrixCSC{<:Any,N}, I::CartesianIndex{N}) where {N} = error("Not implemented")
