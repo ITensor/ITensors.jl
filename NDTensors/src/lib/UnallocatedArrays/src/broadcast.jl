a34337d291a80e16796b2196e5b0246103bf40bf
kmp5VT <kmp5@vt.edu>
2024-01-10 16:04:56 -0500

    Update broadcast functions

--- NDTensors/src/lib/UnallocatedArrays/src/broadcast.jl
+++ NDTensors/src/lib/UnallocatedArrays/src/broadcast.jl
@@ -4,20 +4,23 @@ struct NotZeroPreserving <: ZeroPreserving end
 
 # Assume operations don't preserve zeros for safety
 ZeroPreserving(x) = NotZeroPreserving()
-@eval for type in (complex, Complex, Float16, Float32, Float64, Int)
-  ZeroPreserving(::typeof($type)) = IsZeroPreserving()
-end
+ZeroPreserving(::typeof(Complex)) = IsZeroPreserving()
+ZeroPreserving(::typeof(Real)) = IsZeroPreserving()
 
 function Broadcast.broadcasted(style::Broadcast.DefaultArrayStyle, f, a::UnallocatedZeros)
   return _broadcasted(style, f, ZeroPreserving(f), a)
 end
 
-function _broadcasted(style::Broadcast.DefaultArrayStyle, f, ::IsZeroPreserving, a::UnallocatedZeros)
-    z = f.(parent(a))
+function _broadcasted(
+  style::Broadcast.DefaultArrayStyle, f, ::IsZeroPreserving, a::UnallocatedZeros
+)
+  z = f.(parent(a))
   return FillArrays.broadcasted_zeros(f, a, eltype(z), axes(z))
 end
 
-function _broadcasted(style::Broadcast.DefaultArrayStyle, f, ::NotZeroPreserving, a::UnallocatedZeros)
-    f = f.(parent(a))
+function _broadcasted(
+  style::Broadcast.DefaultArrayStyle, f, ::NotZeroPreserving, a::UnallocatedZeros
+)
+  f = f.(parent(a))
   return FillArrays.broadcasted_fill(f, a, FillArrays.getindex_value(f), axes(f))
 end
