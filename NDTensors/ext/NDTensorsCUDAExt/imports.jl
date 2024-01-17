15618d809c07589e600c2da76cdb30f5d04e04ed
Karl Pierce <kmp5@vt.edu>
2024-01-17 16:42:13 -0500

    Don't import specify_eltype

--- NDTensors/ext/NDTensorsCUDAExt/imports.jl
+++ NDTensors/ext/NDTensorsCUDAExt/imports.jl
@@ -1,4 +1,4 @@
-import NDTensors: cu, set_ndims, set_eltype, specify_eltype, similartype
+import NDTensors: cu, set_ndims, set_eltype, similartype
 import NDTensors:
   ContractionProperties, _contract!, GemmBackend, auto_select_backend, _gemm!, iscu
 import NDTensors.SetParameters: nparameters, get_parameter, set_parameter, default_parameter
--- NDTensors/ext/NDTensorsMetalExt/imports.jl
+++ NDTensors/ext/NDTensorsMetalExt/imports.jl
@@ -1,4 +1,4 @@
-import NDTensors: mtl, set_ndims, set_eltype, specify_eltype
+import NDTensors: mtl, set_ndims, set_eltype
 import NDTensors.SetParameters: nparameters, get_parameter, set_parameter, default_parameter
 
 using NDTensors.Unwrap: Exposed, unwrap_type, unexpose, expose
