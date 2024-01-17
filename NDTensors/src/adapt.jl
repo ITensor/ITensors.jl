6a63ff5aef23f85f78340047f85d6d72f1af9a97
Karl Pierce <kmp5@vt.edu>
2024-01-17 16:45:07 -0500

    Use specify_parameters over specify_eltype

--- NDTensors/src/adapt.jl
+++ NDTensors/src/adapt.jl
@@ -31,9 +31,9 @@ double_precision(x) = fmap(x -> adapt(double_precision(eltype(x)), x), x)
 #
 
 function adapt_storagetype(to::Type{<:AbstractVector}, x::Type{<:TensorStorage})
-  return set_datatype(x, specify_eltype(to, eltype(x)))
+  return set_datatype(x, specify_parameters(to, eltype(x)))
 end
 
 function adapt_storagetype(to::Type{<:AbstractArray}, x::Type{<:TensorStorage})
-  return set_datatype(x, specify_eltype(set_ndims(to, 1), eltype(x)))
+  return set_datatype(x, specify_parameters(set_ndims(to, 1), eltype(x)))
 end
