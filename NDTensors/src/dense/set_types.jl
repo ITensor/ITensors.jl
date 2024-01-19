f3601b1451c7eb86d1c1e00178b2a79a0e492d33
Karl Pierce <kmp5@vt.edu>
2024-01-19 14:58:58 -0500

    remove set_parameters definitions from NDTensors

--- NDTensors/src/dense/set_types.jl
+++ NDTensors/src/dense/set_types.jl
@@ -1,27 +1,3 @@
-## TODO verify unecessary and delete
-# function SetParameters.specify_parameters(
-#   storetype::Type{<:Dense{ElT,DataT}}, datatype::Type{<:AbstractArray}=default_datatype(ElT)
-# ) where {ElT,DataT<:AbstractArray{ElT}}
-#   return storetype
-# end
-# function SetParameters.specify_parameters(
-#   storetype::Type{<:Dense{ElT,DataT}}, datatype::Type{<:AbstractArray}=default_datatype(ElT)
-# ) where {ElT,DataT<:AbstractArray}
-#   return set_datatype(storetype, specify_parameters(DataT, ElT))
-# end
-# function SetParameters.specify_parameters(
-#   storetype::Type{<:Dense{ElT}}, datatype::Type{<:AbstractArray}=default_datatype(ElT)
-# ) where {ElT}
-#   return default_storagetype(specify_parameters(datatype, ElT), ())
-# end
-
-# function SetParameters.specify_parameters(
-#   storetype::Type{<:Dense},
-#   datatype::Type{<:AbstractArray}=default_datatype(default_eltype()),
-# )
-#   return default_storagetype(datatype, ())
-# end
-
 function set_datatype(storagetype::Type{<:Dense}, datatype::Type{<:AbstractVector})
   return Dense{eltype(datatype),datatype}
 end
