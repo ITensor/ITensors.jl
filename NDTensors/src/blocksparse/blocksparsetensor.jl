a05749699fe9853253c77a5c52bc098bbb8d6e60
Karl Pierce <kmp5@vt.edu>
2024-01-22 12:03:45 -0500

    cleanup function

--- NDTensors/src/blocksparse/blocksparsetensor.jl
+++ NDTensors/src/blocksparse/blocksparsetensor.jl
@@ -243,7 +243,6 @@ end
 @propagate_inbounds function getindex(T::BlockSparseTensor{ElT,0}) where {ElT}
   nnzblocks(T) == 0 && return zero(ElT)
   return expose(storage(T))[]
-  #storage(T)[]
 end
 
 # These may not be valid if the Tensor has no blocks
