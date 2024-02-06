611e350daf6eb45cfea6b23367f848b397413d7a
Karl Pierce <kmp5@vt.edu>
2024-02-06 17:41:40 -0500

    Update parameters file remove get_parameters

--- NDTensors/src/lib/TypeParameterAccessors/src/parameters.jl
+++ NDTensors/src/lib/TypeParameterAccessors/src/parameters.jl
@@ -7,38 +7,23 @@ parameters(type::DataType) = Tuple(type.parameters)
 
 parameters(type::UnionAll) = parameters(unionall_to_datatype(type))
 
+parameters(object) = parameters(typeof(object))
+
 """
-    get_parameter(object, position::Int)
+    parameter(object, position::Int)
 
 Get a type parameter of the object `object` at the position `position`.
 """
-get_parameter(type, position::Int) = parameters(type)[position]
+parameter(type, position::Int) = parameters(type)[position]
 
 """
-    get_parameter(object, position::Position)
+    parameter(object, position::Position)
 
 Get a type parameter of the object `object` at the position `position`.
 """
-get_parameter(object::Type, position::Position) = get_parameter(object, parameter(position))
-
-"""
-    get_parameter(type::Type)
-
-Get the first type parameter of the type `type`.
-"""
-get_parameter(type::Type) = parameter(type, 1)
-
-"""
-    get_parameter(object)
-
-Get the first type parameter of the object `object`.
-"""
-get_parameter(object) = get_parameter(typeof(object))
-
-parameter(type::Type, pos::Int) = parameters(type)[pos]
-
-parameter(type::Type, pos::Position) = parameters(type)[parameter(pos)]
+parameter(object::Type, position::Position) = parameter(object, parameter(position))
 
 parameter(x) = parameter(typeof(x))
 
 parameter(type::Type) = only(parameters(type))
+ 
