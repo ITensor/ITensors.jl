023770ffba33353dd655a450578b401b587b0e78
Karl Pierce <kmp5@vt.edu>
2024-02-07 10:44:30 -0500

    Update documentation

--- NDTensors/src/lib/TypeParameterAccessors/src/parameters.jl
+++ NDTensors/src/lib/TypeParameterAccessors/src/parameters.jl
@@ -1,29 +1,49 @@
 """
-    parameters(object, position::Int)
+    parameters(type::DataType)
 
-Get a type parameter of the object `object` at the position `position`.
+Gets all the type parameters of the DataType `type`.
 """
 parameters(type::DataType) = Tuple(type.parameters)
 
+"""
+    parameters(type::UnionAll, position::Int)
+
+Gets all the type parameters of the UnionAll `type`.
+"""
 parameters(type::UnionAll) = parameters(unionall_to_datatype(type))
 
+"""
+    parameters(object, position::Int)
+
+Gets all the type parameters of the object `object`.
+"""
 parameters(object) = parameters(typeof(object))
 
 """
-    parameter(object, position::Int)
+    parameter(type::Type, position::Int)
 
-Get a type parameter of the object `object` at the position `position`.
+Get a type parameter of the type `type` at the position `position`.
 """
-parameter(type, position::Int) = parameters(type)[position]
+parameter(type::Type, position::Int) = parameters(type)[position]
 
 """
-    parameter(object, position::Position)
+    parameter(type::Type, position::Position)
 
-Get a type parameter of the object `object` at the position `position`.
+Get a type parameter of the type `type` at the position `position`.
 """
-parameter(object::Type, position::Position) = parameter(object, parameter(position))
+parameter(type::Type, position::Position) = parameter(object, parameter(position))
 
-parameter(x) = parameter(typeof(x))
+"""
+    parameter(type::Type)
 
+Gets the single parameter of the Type `type`. Will throw an error if `type` has more than one parameter.
+"""
 parameter(type::Type) = only(parameters(type))
+
+"""
+    parameter(type::Type)
+
+Gets the single parameter of the object `object`. Will throw an error if `object` has more than one parameter.
+"""
+parameter(x) = parameter(typeof(x))
  
