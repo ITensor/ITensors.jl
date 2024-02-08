2c3e62ffd9cabb813e6e7e7074bf7c037c14ef46
Karl Pierce <kmp5@vt.edu>
2024-02-08 17:27:10 -0500

    Updates to base functions to make more type stable

--- NDTensors/src/lib/TypeParameterAccessors/src/typeparamaccessor.jl
+++ NDTensors/src/lib/TypeParameterAccessors/src/typeparamaccessor.jl
@@ -1,22 +1,20 @@
-unionall_to_datatype(type::Type) = Base.unwrap_unionall(type)
-function datatype_to_unionall(type::Type, unionall_reference::Type)
-  return Base.rewrap_unionall(type, unionall_reference)
-end
+# Relies on Julia internals
+Base.@assume_effects :terminates_globally to_datatype(type::UnionAll) = Base.unwrap_unionall(type)
+Base.@assume_effects :terminates_globally to_unionall(type::DataType, unionall_reference::Type) = Base.rewrap_unionall(type, unionall_reference)
+
+# `TypeParameterAccessor` functionality.
+to_datatype(type::DataType) = type
+to_unionall(type::UnionAll, unionall_reference::Type) = type
+to_unionall(type::UnionAll) = type
+to_unionall(type::DataType) = to_unionall(type, unspecify_parameters(type))
+
+# `TypeParameterAccessor` functionality.
+to_unionall(type::Type) = to_unionall(type, unspecify_parameters(type))
 
 unspecify_parameters(type::DataType) = Base.typename(type).wrapper
 
-# `SetParameters` functionality.
-datatype_to_unionall(type::Type) = datatype_to_unionall(type, unspecify_parameters(type))
-
 function unspecify_parameters(type::UnionAll)
-  return unspecify_parameters(unionall_to_datatype(type))
+  return unspecify_parameters(to_datatype(type))
 end
-
-nparameters(type::Type) = length(parameters(type))
 
 is_parameter_specified(type::Type, pos) = !(parameter(type, pos) isa TypeVar)
-
-function specify_parameter(type::Type, pos, val)
-  is_parameter_specified(type, pos) && return type
-  return set_parameter(type, pos, val)
-end
