354a07b2f1cbbcc2ed0bdafb450ff1a212780b59
Karl Pierce <kmp5@vt.edu>
2024-02-08 15:06:03 -0500

    Move functions that depend on `Position` to `position.jl`

--- NDTensors/src/lib/TypeParameterAccessors/src/position.jl
+++ NDTensors/src/lib/TypeParameterAccessors/src/position.jl
@@ -9,6 +9,8 @@ Position(x) = Position{x}()
 
 Get a type parameter of the type `type` at the position `position`.
 """
-parameter(type::Type, position::Position) = parameter(type, parameter(position))
+parameter(type::Type, position::Position) = parameter(type, Int(position))
+
+set_parameter(type::Type, pos::Position, val) = set_parameter(type, parameter(pos), val)
 
 Int(p::Position) = parameter(p)
