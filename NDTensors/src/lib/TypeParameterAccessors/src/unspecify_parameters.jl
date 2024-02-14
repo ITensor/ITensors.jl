25d09d5304bf2a2b127eff00b9fbe4ae9ed2a224
kmp5VT <kmp5@vt.edu>
2024-02-14 17:59:26 -0500

    Fix typo

--- NDTensors/src/lib/TypeParameterAccessors/src/unspecify_parameters.jl
+++ NDTensors/src/lib/TypeParameterAccessors/src/unspecify_parameters.jl
@@ -1,5 +1,5 @@
 unspecify_parameters(type::DataType) = Base.typename(type).wrapper
 
 function unspecify_parameters(type::UnionAll)
-  return unspecify_parameters(Base.unwrap_union(type))
+  return unspecify_parameters(Base.unwrap_unionall(type))
 end
