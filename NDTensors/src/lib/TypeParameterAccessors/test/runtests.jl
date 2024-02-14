09a9dff0bb78c673f2004d6c005d42e9d248813d
kmp5VT <kmp5@vt.edu>
2024-02-14 18:00:01 -0500

    Remove default_parameter test and add back unspecifying parameters test

--- NDTensors/src/lib/TypeParameterAccessors/test/runtests.jl
+++ NDTensors/src/lib/TypeParameterAccessors/test/runtests.jl
@@ -127,76 +127,15 @@ using LinearAlgebra: Transpose
   #   @test @inferred(default_parameter(Array{Float32,3}, Position(2))) == 1
   # end
 
-  # @testset "Set to the default parameter" begin
-  #   @test @inferred(set_parameters(Array{Float32,3}, Position(1), DefaultParameter())) ==
-  #     Array{Float64,3}
-  #   @test @inferred(set_parameters(Array{Float32,3}, Position(2), DefaultParameter())) ==
-  #     Array{Float32,1}
-  #   @test @inferred(set_parameters(Array{Float32}, Position(1), DefaultParameter())) ==
-  #     Array{Float64}
-  #   @test @inferred(set_parameters(Array{Float32}, Position(2), DefaultParameter())) ==
-  #     Array{Float32,1}
-  #   @test @inferred(set_parameters(Array{<:Any,3}, Position(1), DefaultParameter())) ==
-  #     Array{Float64,3}
-  #   # TODO: Inferrence is broken for this case
-  #   @test @inferred(Any, set_parameters(Array{<:Any,3}, Position(2), DefaultParameter())) ==
-  #     Array{<:Any,1}
-  #   @test @inferred(set_parameters(Array, Position(1), DefaultParameter())) ==
-  #     Array{Float64}
-  #   # TODO: Inferrence is broken for this case
-  #   @test @inferred(Any, set_parameters(Array, Position(2), DefaultParameter())) ==
-  #     Array{<:Any,1}
-  # end
-
-  # @testset "Set to the default parameters" begin
-  #   @test @inferred(set_parameters(Array{Float32,3}, DefaultParameters())) ==
-  #     Array{Float64,1}
-  #   @test @inferred(set_parameters(Array{Float32}, DefaultParameters())) == Array{Float64,1}
-  #   @test @inferred(set_parameters(Array{<:Any,3}, DefaultParameters())) == Array{Float64,1}
-  #   @test @inferred(set_parameters(Array, DefaultParameters())) == Array{Float64,1}
-  # end
-
-  # @testset "Set to the default parameter if unspecified" begin
-  #   @test @inferred(
-  #     specify_parameters(Array{Float32,3}, Position(1), DefaultParameter())
-  #   ) == Array{Float32,3}
-  #   @test @inferred(
-  #     specify_parameters(Array{Float32,3}, Position(2), DefaultParameter())
-  #   ) == Array{Float32,3}
-  #   @test @inferred(specify_parameters(Array{Float32}, Position(1), DefaultParameter())) ==
-  #     Array{Float32}
-  #   @test @inferred(specify_parameters(Array{Float32}, Position(2), DefaultParameter())) ==
-  #     Array{Float32,1}
-  #   @test @inferred(specify_parameters(Array{<:Any,3}, Position(1), DefaultParameter())) ==
-  #     Array{Float64,3}
-  #   @test @inferred(specify_parameters(Array{<:Any,3}, Position(2), DefaultParameter())) ==
-  #     Array{<:Any,3}
-  #   @test @inferred(specify_parameters(Array, Position(1), DefaultParameter())) ==
-  #     Array{Float64}
-  #   # TODO: Inferrence is broken for this case
-  #   @test @inferred(Any, specify_parameters(Array, Position(2), DefaultParameter())) ==
-  #     Array{<:Any,1}
-  # end
-
-  # @testset "Set to the default parameters if unspecified" begin
-  #   @test @inferred(specify_parameters(Array{Float32,3}, DefaultParameters())) ==
-  #     Array{Float32,3}
-  #   @test @inferred(specify_parameters(Array{Float32}, DefaultParameters())) ==
-  #     Array{Float32,1}
-  #   @test @inferred(specify_parameters(Array{<:Any,3}, DefaultParameters())) ==
-  #     Array{Float64,3}
-  #   @test @inferred(specify_parameters(Array, DefaultParameters())) == Array{Float64,1}
-  # end
-
-  # @testset "Test unspecifing parameters" begin
-  #   v = Vector{Float32}
-  #   m = Matrix{Float64}
-  #   a = Array{ComplexF32}
-  #   val = Val{3}
-  #   @test TypeParameterAccessors.unspecify_parameters(v) == Array
-  #   @test TypeParameterAccessors.unspecify_parameters(m) == Array
-  #   @test TypeParameterAccessors.unspecify_parameters(a) == Array
-  #   @test TypeParameterAccessors.unspecify_parameters(val) == Val
-  # end
+  @testset "Test unspecifying parameters" begin
+    v = Vector{Float32}
+    m = Matrix{Float64}
+    a = Array{ComplexF32}
+    val = Val{3}
+    @test TypeParameterAccessors.unspecify_parameters(v) == Array
+    @test TypeParameterAccessors.unspecify_parameters(m) == Array
+    @test TypeParameterAccessors.unspecify_parameters(a) == Array
+    @test TypeParameterAccessors.unspecify_parameters(val) == Val
+  end
 end
 end
