719a8810f9ab40e9b2e7d8c4281ad38a7c1057a6
Karl Pierce <kmp5@vt.edu>
2024-02-16 17:01:02 -0500

    Add mytype for testing

---
+++ NDTensors/src/lib/TypeParameterAccessors/test/mytype.jl
@@ -0,0 +1,20 @@
+struct MyType{T, N}
+end
+
+struct MyTypeNamedParams{N,T,V}
+end
+
+using NDTensors.TypeParameterAccessors: TypeParameterAccessors, Position
+TypeParameterAccessors.position(::Type{<:MyTypeNamedParams}, ::typeof(eltype)) = Position(2)
+TypeParameterAccessors.position(::Type{<:MyTypeNamedParams}, ::typeof(ndims)) = Position(1)
+third_type(type::Type{<:MyTypeNamedParams}) = parameter(type, position(type, third_type))
+TypeParameterAccessors.position(::Type{<:MyTypeNamedParams}, ::typeof(third_type)) = Position(3)
+
+struct MyTypeDefaults{V, T, N}
+end
+
+## TODO change this to use names
+TypeParameterAccessors.parameter_function() = (Position(1), Position(2), Position(3))
+TypeParameterAccessors.default_parameter(::Type{<:MyTypeDefaults}, ::Position{1}) = "3"
+TypeParameterAccessors.default_parameter(::Type{<:MyTypeDefaults}, ::Position{2}) = Float32
+TypeParameterAccessors.default_parameter(::Type{<:MyTypeDefaults}, ::Position{3}) = 2
