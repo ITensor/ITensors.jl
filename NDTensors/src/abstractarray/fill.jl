6f4929efdd6d3fa292ab86b4020def6c92e1f66e
Karl Pierce <kmp5@vt.edu>
2024-02-28 11:08:07 -0500

    Update the fill file

--- NDTensors/src/abstractarray/fill.jl
+++ NDTensors/src/abstractarray/fill.jl
@@ -1,15 +1,17 @@
-using .TypeParameterAccessors: specify_default_parameters, unwrap_array_type
+using .TypeParameterAccessors: set_ndims, specify_default_parameters, unwrap_array_type
 
+## TODO I modified this to accept any type and just match the output to the number of dims.
+# for example generic_zeros(Vector, 2,3) = Matrix{Float64}[0,0,0;0,0,0;]
 function generic_randn(
-  arraytype::Type{<:AbstractArray}, dim::Integer=0; rng=Random.default_rng()
+  arraytype::Type{<:AbstractArray}, dims...; rng=Random.default_rng()
 )
-  arraytype_specified = specify_default_parameters(unwrap_array_type(arraytype))
-  data = similar(arraytype_specified, dim)
+  arraytype_specified = set_ndims(specify_default_parameters(unwrap_array_type(arraytype)), length(dims));
+  data = similar(arraytype_specified, dims)
   return randn!(rng, data)
 end
 
 function generic_zeros(arraytype::Type{<:AbstractArray}, dims...)
-  arraytype_specified = specify_default_parameters(unwrap_array_type(arraytype))
+  arraytype_specified = set_ndims(specify_default_parameters(unwrap_array_type(arraytype)), length(dims));
   ElT = eltype(arraytype_specified)
-  return fill!(similar(arraytype_specified, dims...), zero(ElT))
+  return fill!(similar(arraytype_specified, dims), zero(ElT))
 end
