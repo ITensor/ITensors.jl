4e968617bf2b10b5cb4ffa349a65d41a204ce8bb
Karl Pierce <kmp5@vt.edu>
2024-03-06 11:58:26 -0500

    Move DiagBlockSparse tensor to its own folder

---
+++ NDTensors/src/blocksparse/diagblocksparse/typeparameteraccessors.jl
@@ -0,0 +1,3 @@
+TypeParameterAccessors.position(::Type{<:DiagBlockSparse}, ::typeof(eltype)) = Position(1)
+TypeParameterAccessors.position(::Type{<:DiagBlockSparse}, ::typeof(parenttype)) = Position(2)
+TypeParameterAccessors.position(::Type{<:DiagBlockSparse}, ::typeof(Base.ndims)) = Position(3)
