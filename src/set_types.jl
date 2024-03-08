cc754fbe79a65aa08bcab1d4c6660244000033f6
kmp5VT <kmp5@vt.edu>
2024-03-08 15:02:36 -0500

    format

--- src/set_types.jl
+++ src/set_types.jl
@@ -1,3 +1,5 @@
 using NDTensors.TypeParameterAccessors: unwrap_array_type
 NDTensors.TypeParameterAccessors.parenttype(::ITensor) = typeof(tensor(T))
-NDTensors.TypeParameterAccessors.unwrap_array_type(T::ITensor) = unwrap_array_type(tensor(T))
+function NDTensors.TypeParameterAccessors.unwrap_array_type(T::ITensor)
+  return unwrap_array_type(tensor(T))
+end
