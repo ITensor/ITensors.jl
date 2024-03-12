ae6b65a72fe9eccebad51831bba3df78c4102cf4
Karl Pierce <kmp5@vt.edu>
2024-03-12 14:35:44 -0400

    Try to make a generic adapt_storagetype

--- NDTensors/ext/NDTensorsMetalExt/adapt.jl
+++ NDTensors/ext/NDTensorsMetalExt/adapt.jl
@@ -1,3 +1,4 @@
+using NDTensors.TypeParameterAccessors: TypeParameterAccessors
 NDTensors.cpu(e::Exposed{<:MtlArray}) = adapt(Array, e)
 
 function mtl(xs; storage=DefaultStorageMode)
@@ -6,7 +7,8 @@ end
 
 # More general than the version in Metal.jl
 function Adapt.adapt_storage(arraytype::Type{<:MtlArray}, xs::AbstractArray)
-  params = get_parameters(xs)
-  arraytype_specified = specify_parameters(arraytype, params...)
+  arraytype_specified = TypeParameterAccessors.specify_type_parameters(
+    arraytype, (eltype, Base.ndims), TypeParameterAccessors.type_parameters(xs, (eltype, Base.ndims))
+  )
   return isbitstype(typeof(xs)) ? xs : convert(arraytype_specified, xs)
 end
