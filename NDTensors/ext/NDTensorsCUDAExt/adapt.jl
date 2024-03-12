21bd41c8c86025bdc6f1832961cf62cb2894b41c
kmp5VT <kmp5@vt.edu>
2024-03-12 14:52:23 -0400

    import storagemode

--- NDTensors/ext/NDTensorsCUDAExt/adapt.jl
+++ NDTensors/ext/NDTensorsCUDAExt/adapt.jl
@@ -1,4 +1,5 @@
 using NDTensors.TypeParameterAccessors: TypeParameterAccessors
+using NDTensors: storagemode
 ## Here we need an NDTensorCuArrayAdaptor because the CuArrayAdaptor provided by CUDA
 ## converts 64 bit numbers to 32 bit.  We cannot write `adapt(CuVector, x)` because this
 ## Will not allow us to properly utilize the buffer preference without changing the value of
