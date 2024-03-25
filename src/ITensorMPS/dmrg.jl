749000ad0910b5926523218bb79ebc8434f89151
kmp5VT <kmp5@vt.edu>
2024-03-25 10:57:47 -0400

    Add a quick fix to get tests working

--- src/ITensorMPS/dmrg.jl
+++ src/ITensorMPS/dmrg.jl
@@ -250,7 +250,7 @@ function dmrg(
         ## Adapt is only called when using CUDA backend. CPU will work as implemented previously.
         ## TODO this might be the only place we really need iscu if its not fixed.
         phi::ITensor = if NDTensors.iscu(phi) && NDTensors.iscu(vecs[1])
-          adapt(set_eltype(unwrap_array_type(phi), eltype(vecs[1])), vecs[1])
+          adapt(NDTensors.TypeParameterAccessors.set_eltype(unwrap_array_type(phi), eltype(vecs[1])), vecs[1])
         else
           vecs[1]
         end
