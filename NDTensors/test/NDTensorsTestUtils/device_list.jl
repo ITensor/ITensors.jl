193099e4958915cbc69f9993cece005d226e9554
Karl Pierce <kmp5@vt.edu>
2024-04-15 09:52:46 -0400

    Switch to update not versioning explicitly.

--- NDTensors/test/NDTensorsTestUtils/device_list.jl
+++ NDTensors/test/NDTensorsTestUtils/device_list.jl
@@ -13,10 +13,9 @@ if "rocm" in ARGS || "all" in ARGS
 end
 if "metal" in ARGS || "all" in ARGS
   ## Warning Metal does not work in Julia versions below 1.8
-  ## Essentially compat versioning here because we can't put Metal
-  ## in the Project.toml because it causes issues during testing in 
-  ## Julia versions below 1.8
-  Pkg.add(; name="Metal", version="1.1.0")
+  Pkg.add("Metal")
+  ## This line forces Pkg to update metal to latest version
+  Pkg.update("Metal"; preserve=PRESERVE_DIRECT)
   using Metal
 end
 
