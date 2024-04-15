0e1869f7c577ff013093b59844531f66d24cadd4
Olivier Gauthé <ogauthe@flatironinstitute.org>
2024-04-15 11:09:10 -0400

    add comment on label type

--- NDTensors/src/lib/Sectors/src/category_definitions/u1.jl
+++ NDTensors/src/lib/Sectors/src/category_definitions/u1.jl
@@ -4,6 +4,8 @@ using HalfIntegers: Half
 # U₁ group (circle group, or particle number, total Sz etc.)
 #
 
+# use HalfInteger as internal label to allow easy conversion to/from SU(2)
+# still allows to call U1(::Int) and to use it as an Int
 struct U1 <: AbstractCategory
   n::Half{Int}
 end
