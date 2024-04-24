a56be7113852e4754521b33cf0b61a57fcca81dd
kmp5VT <kmp5@vt.edu>
2024-04-24 15:32:49 -0400

    update cutensor contract to try to debug

--- NDTensors/ext/NDTensorscuTENSORExt/contract.jl
+++ NDTensors/ext/NDTensorscuTENSORExt/contract.jl
@@ -1,4 +1,4 @@
-using NDTensors: NDTensors, Tensor
+using NDTensors: NDTensors, Tensor, array, inds
 using NDTensors.Expose: Exposed, unexpose
 using cuTENSOR: cuTENSOR, CuArray, CuTensor
 function NDTensors.contract(
@@ -11,21 +11,21 @@ function NDTensors.contract(
   tensor1 = unexpose(Etensor1)
   tensor2 = unexpose(Etensor2)
   ## convert the ITensors into CuTensors
-  cutensorA = CuTensor(array(tensor1), collect(labelstensor1))
-  cutensorB = CuTensor(array(tensor2), collect(labelstensor2))
+  ## for 
+  cutensorA = CuTensor(convert(CuArray, array(tensor1)), collect(labelstensor1))
+  cutensorB = CuTensor(convert(CuArray, array(tensor1)), collect(labelstensor2))
 
   ## contract the CuTensors
   cutensorC = cutensorA * cutensorB
 
   ## TODO this is a first draft to this idea to see if the 
   ## conversion works 
-  output_tensor = NDTensors.contraction_output(
-    tensor1, labelstensor1, tensor2, labelstensor2, labelsoutput_tensor
-  )
+  indsR = contract_inds(inds(tensor1), labelstensor1, inds(tensor2), labelstensor2, labelsoutput_tensor)
+  TensorR = contraction_output_type(typeof(tensor1), typeof(tensor2), labelsoutput_tensor)
 
   ## Replace the data in the output_tensor with the correct data from the cutensor contraction
   ## it is necessary to flatten the data
-  typeof(output_tensor)(reshape(cutensorC.data, dim(output_tensor)), inds(output_tensor))
+  output_tensor = TensorR(reshape(cutensorC.data, dim(output_tensor)), indsR)
 
   ## return output_tensor
   return output_tensor
