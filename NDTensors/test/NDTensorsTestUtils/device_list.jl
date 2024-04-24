71710835b8f9af4100ab6a2370ad9df3adf33400
kmp5VT <kmp5@vt.edu>
2024-04-24 10:42:52 -0400

    Start adding cuTENSOR to NDTensor test suite

--- NDTensors/test/NDTensorsTestUtils/device_list.jl
+++ NDTensors/test/NDTensorsTestUtils/device_list.jl
@@ -16,6 +16,9 @@ if "metal" in ARGS || "all" in ARGS
   Pkg.add("Metal")
   using Metal
 end
+if "cutensor" in ARGS || "all" in ARGS
+  using CUDA, cuTENSOR
+end
 
 function devices_list(test_args)
   devs = Vector{Function}(undef, 0)
@@ -23,7 +26,7 @@ function devices_list(test_args)
     push!(devs, NDTensors.cpu)
   end
 
-  if "cuda" in test_args || "all" in test_args
+  if "cuda" in test_args || "cutensor" in test_args || "all" in test_args
     if CUDA.functional()
       push!(devs, NDTensors.CUDAExtensions.cu)
     else
