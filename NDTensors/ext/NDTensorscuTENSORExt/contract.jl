b99c427deb4ccd0bc7bddfa71eb6c8bfef3709af
kmp5VT <kmp5@vt.edu>
2024-04-25 15:12:52 -0400

    Use adapt to fix CuTensor issue

--- NDTensors/ext/NDTensorscuTENSORExt/contract.jl
+++ NDTensors/ext/NDTensorscuTENSORExt/contract.jl
@@ -1,6 +1,8 @@
 using NDTensors: NDTensors, Tensor, array, contraction_output_type, contract_inds, inds
 using NDTensors.Expose: Exposed, unexpose
 using cuTENSOR: cuTENSOR, CuArray, CuTensor
+using Adapt: adapt
+
 function NDTensors.contract(
   Etensor1::Exposed{<:CuArray},
   labelstensor1,
@@ -10,6 +12,9 @@ function NDTensors.contract(
 )
   tensor1 = unexpose(Etensor1)
   tensor2 = unexpose(Etensor2)
+  elt = promote_type(eltype(tensor1), eltype(tensor2))
+  tensor1 = adapt(elt, tensor1)
+  tensor2 = adapt(elt, tensor2)
   ## convert the ITensors into CuTensors
   ## This can fail when array(tensor) returns a ReshapedArray(CuArray)
   cutensorA = CuTensor(array(tensor1), collect(labelstensor1))
@@ -23,14 +28,14 @@ function NDTensors.contract(
   indsR = contract_inds(
     inds(tensor1), labelstensor1, inds(tensor2), labelstensor2, labelsoutput_tensor
   )
-  TensorR = contraction_output_type(typeof(tensor1), typeof(tensor2), labelsoutput_tensor)
+  TensorR = contraction_output_type(typeof(tensor1), typeof(tensor2), indsR)
 
   ## Replace the data in the output_tensor with the correct data from the cutensor contraction
   ## it is necessary to flatten the data
   ## TODO this could possibly fail for BlockSparse so need to determine that
   output_tensor = TensorR(
     NDTensors.AllowAlias(),
-    NDTensors.storagetype(TensorR)(reshape(cutensorC.data, dim(indsR))),
+    NDTensors.storagetype(TensorR)(reshape(cutensorC.data, length(cutensorC.data))),
     indsR,
   )
 
