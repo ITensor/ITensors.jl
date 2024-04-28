d9b2818852c07599e562444f663a6d00d12702d6
kmp5VT <kmp5@vt.edu>
2024-04-28 14:41:40 -0400

    Remove blocksparse contract, reworking

--- NDTensors/ext/NDTensorscuTENSORExt/contract.jl
+++ NDTensors/ext/NDTensorscuTENSORExt/contract.jl
@@ -27,37 +27,6 @@ function NDTensors.contract(
   )
 end
 
-## working to fix blocksparse implementation. 
-function NDTensors.contract(
-  tensor1::Exposed{<:CuArray,<:BlockSparseTensor},
-  labelstensor1,
-  tensor2::Exposed{<:CuArray,<:BlockSparseTensor},
-  labelstensor2,
-  labelsR,
-)
-  ## temporarily don't use cutensor here, just keep implemented to prevent error
-  tensor1 = unexpose(tensor1)
-  tensor2 = unexpose(tensor2)
-  R, contraction_plan = contraction_output(
-    tensor1, labelstensor1, tensor2, labelstensor2, labelsR
-  )
-  R = contract!(
-    R, labelsR, tensor1, labelstensor1, tensor2, labelstensor2, contraction_plan
-  )
-  return R
-  #   tensor1 = unexpose(Etensor1)
-  #   tensor2 = unexpose(Etensor2)
-
-  #   denseoutput_tensor = cutensor_contract(dense(tensor1), labelstensor1, dense(tensor2), labelstensor2, labelsoutput_tensor)
-
-  #   ## transform the dense tensor back to blocksparse
-  #   indsR = contract_inds(
-  #     inds(tensor1), labelstensor1, inds(tensor2), labelstensor2, labelsoutput_tensor
-  #   )
-  #   TensorR = contraction_output_type(typeof(tensor1), typeof(tensor2), indsR)
-  #   return to_sparse(TensorR, denseoutput_tensor, indsR)
-end
-
 ## TODO this only works for dense tensors
 function cutensor_contract(
   tensor1::Tensor, labelstensor1, tensor2::Tensor, labelstensor2, labelsoutput_tensor
