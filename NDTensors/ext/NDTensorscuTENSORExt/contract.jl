a953c287b5d3d38420bb92596e67dd9d9abd4abc
kmp5VT <kmp5@vt.edu>
2024-05-03 10:27:40 -0400

    Suggested updates to cutensor contract

--- NDTensors/ext/NDTensorscuTENSORExt/contract.jl
+++ NDTensors/ext/NDTensorscuTENSORExt/contract.jl
@@ -1,6 +1,6 @@
 using NDTensors: NDTensors, DenseTensor, array
 using NDTensors.Expose: Exposed, unexpose
-using cuTENSOR: CuArray, CuTensor, mul!
+using cuTENSOR: CuArray, CuTensor
 
 function NDTensors.contract!(
   R::Exposed{<:CuArray,<:DenseTensor},
@@ -12,11 +12,9 @@ function NDTensors.contract!(
   α::Number=one(Bool),
   β::Number=zero(Bool),
 )
-  cuR, cuT1, cuT2 =
-    CuTensor.(
-      array.((unexpose(R), unexpose(T1), unexpose(T2))),
-      collect.((labelsR, labelsT1, labelsT2)),
-    )
-  mul!(cuR, cuT1, cuT2, α, β)
+  cuR = CuTensor(array(unexpose(R)), collect(labelsR))
+  cuT1 = CuTensor(array(unexpose(T1)), collect(labelsT1))
+  cuT2 = CuTensor(array(unexpose(T2)), collect(labelsT2))
+  cuTENSOR.mul!(cuR, cuT1, cuT2, α, β)
   return R
 end
