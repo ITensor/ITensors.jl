c94960baaf74ad65ad89aa6c2159bff71bdc3811
mtfishman <mfishman@flatironinstitute.org>
2024-05-07 14:52:40 -0400

    [ITensors] Delete benchmark subdirectory

--- benchmark/benchmarks.jl
+++
@@ -1,9 +0,0 @@
-using BenchmarkTools
-
-const SUITE = BenchmarkGroup()
-
-for file in readdir(@__DIR__)
-  if startswith(file, "bench_") && endswith(file, ".jl")
-    SUITE[file[(length("bench_") + 1):(end - length(".jl"))]] = include(file)
-  end
-end
