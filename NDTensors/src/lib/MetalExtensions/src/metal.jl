08672a2d5cfb98954a27154bf2a77f10e9df01f7
Matt Fishman <mtfishman@users.noreply.github.com>
2024-05-08 09:03:31 -0400

    [ITensorGPU] Move to separate repository (#1412)

--- NDTensors/src/lib/MetalExtensions/src/metal.jl
+++ NDTensors/src/lib/MetalExtensions/src/metal.jl
@@ -1,12 +1,12 @@
 using NDTensors.TypeParameterAccessors: TypeParameterAccessors, Position
 using NDTensors.GPUArraysCoreExtensions: storagemode
-# Implemented in `ITensorGPU` and NDTensorsMetalExt
+# Implemented in NDTensorsMetalExt
 function mtl end
 
 ## Here we need an MtlArrayAdaptor because the MtlArrayAdaptor provided by Metal
 ## converts 64 bit numbers to 32 bit.  We cannot write `adapt(MtlArray, x)` because this
 ## Will not allow us to properly utilize the buffer preference without changing the value of
-## default_buffertype. Also `adapt(MtlArray{<:Any, <:Any, Buffertype})` fails to work properly 
+## default_buffertype. Also `adapt(MtlArray{<:Any, <:Any, Buffertype})` fails to work properly
 
 struct MtlArrayAdaptor{B} end
 
