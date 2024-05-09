b5a91795f457a40dc0a304f2809746848f50908b
Matt Fishman <mtfishman@users.noreply.github.com>
2024-05-08 21:04:43 -0400

    [ITensors] Remove combine-contract codepath (#1427)

--- src/deprecated.jl
+++ src/deprecated.jl
@@ -1,13 +1,10 @@
 
 # global_variables.jl
-@deprecate disable_combine_contract!() ITensors.disable_combine_contract()
 @deprecate disable_tblis!() ITensors.disable_tblis()
 @deprecate disable_warn_order!() ITensors.disable_warn_order()
-@deprecate enable_combine_contract!() ITensors.enable_combine_contract()
 @deprecate enable_tblis!() ITensors.enable_tblis()
 @deprecate reset_warn_order!() ITensors.reset_warn_order()
 @deprecate set_warn_order!(N) ITensors.set_warn_order(N)
-@deprecate use_combine_contract() ITensors.using_combine_contract()
 @deprecate use_debug_checks() ITensors.using_debug_checks()
 
 # index.jl
