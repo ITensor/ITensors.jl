e19fc959582078b1639e147cce3f0c13492f1aae
Karl Pierce <kmp5@vt.edu>
2024-05-31 13:46:11 -0400

    Add contract tests for BlockSparseDiag

--- NDTensors/test/test_diagblocksparse.jl
+++ NDTensors/test/test_diagblocksparse.jl
@@ -1,16 +1,20 @@
 @eval module $(gensym())
 using Dictionaries: Dictionary
+using GPUArraysCore: @allowscalar
 using NDTensors:
   NDTensors,
   Block,
   BlockSparseTensor,
+  dense,
   DiagBlockSparse,
+  Diag,
+  inds,
   Tensor,
   blockoffsets,
   contract,
   nzblocks
 using Random: randn!
-using Test: @test, @test_throws, @testset
+using Test: @test, @test_broken, @test_throws, @testset
 @testset "UniformDiagBlockSparseTensor basic functionality" begin
   NeverAlias = NDTensors.NeverAlias
   AllowAlias = NDTensors.AllowAlias
@@ -47,5 +51,35 @@ end
   for (labels1, labels2) in (((1, -1), (-1, 2)), ((-1, -2), (-1, -2)))
     @test_throws ErrorException contract(a1, labels1, a2, labels2)
   end
+end
+
+include("NDTensorsTestUtils/NDTensorsTestUtils.jl")
+using .NDTensorsTestUtils: devices_list
+@testset "DiagBlockSparse contract" for dev in devices_list(copy(ARGS))
+  elt = dev == NDTensors.mtl ? Float32 : Float64
+  A = dev(BlockSparseTensor{elt}([(1, 1), (2, 2)], [2, 2], [2, 2]))
+  randn!(A)
+  t = Tensor(DiagBlockSparse(one(elt),blockoffsets(A)), inds(A))
+  tdense = Tensor(Diag(one(elt)), inds(A))
+ 
+  a = dense(contract(A, (1, -2), t, (3,-2)))
+  b = contract(dense(A), (1, -2), tdense, (3,-2))
+  @test @allowscalar a ≈ b
+  
+  a = dense(contract(A, (-2, 1), t, (-2,3)))
+  b = contract(dense(A), (-2, 1), tdense, (-2,3))
+  @test @allowscalar a ≈ b
+  
+  a = contract(A, (-1,-2), t, (-1,-2))[]
+  b = contract(dense(A), (-1,-2), tdense, (-1,-2))[]
+  @test @allowscalar a ≈ b
+
+  ## TODO fix these kinds of contractions
+  A = BlockSparseTensor{elt}([(1, 1), (2, 2)], [3, 2, 3], [2, 2])
+  randn!(A)
+  t = Tensor(DiagBlockSparse(one(elt),blockoffsets(A)), inds(A))
+  @test_broken dense(contract(A, (1, -2), (t), (3,-2))) ≈ contract(dense(A), (1, -2), dense(t), (3,-2))
+  @test_broken dense(contract(A, (-2, 1), t, (-2,3))) ≈ contract(dense(A), (-2, 1), dense(t), (-2,3))
+  @test_broken contract(dev(A), (-1,-2), dev(t), (-1,-2))[] ≈ contract(dense(A), (-1,-2), dense(t), (-1,-2))[]
 end
 end
