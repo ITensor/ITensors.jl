297f01c670dc021cf9f8e249b1a578543d31f21a
kmp5VT <kmp5@vt.edu>
2024-06-05 16:55:56 -0400

    Create contract function to only copy data when necessary.

--- NDTensors/ext/NDTensorscuTENSORExt/contract.jl
+++ NDTensors/ext/NDTensorscuTENSORExt/contract.jl
@@ -2,6 +2,11 @@ using NDTensors: NDTensors, DenseTensor, array, data
 using NDTensors.Expose: Exposed, unexpose
 using cuTENSOR: cuTENSOR, CuArray, CuTensor
 
+struct CuArrayOffset{N} end
+function CuArrayOffset(V)
+  return CuArrayOffset{V}()
+end
+
 function NDTensors.contract!(
   R::Exposed{<:CuArray,<:DenseTensor},
   labelsR,
@@ -12,12 +17,54 @@ function NDTensors.contract!(
   α::Number=one(Bool),
   β::Number=zero(Bool),
 )
-  ## TODO for now a hack to get cuTENSOR working with blocksparse
-  R = unexpose(R)
-  cuR = CuTensor(copy(array(R)), collect(labelsR))
-  cuT1 = CuTensor(copy(array(unexpose(T1))), collect(labelsT1))
-  cuT2 = CuTensor(copy(array(unexpose(T2))), collect(labelsT2))
+  R, T1, T2 = unexpose.((R, T1, T2))
+  ## Instance the contract! function off the 
+  ## offset on the CuArrays
+  offR = CuArrayOffset(data(R).offset)
+  offT1 = CuArrayOffset(data(T1).offset)
+  offT2 = CuArrayOffset(data(T2).offset)
+  return contract!(offR, R, labelsR, offT1, T1, labelsT1, offT2, T2, labelsT2, α, β)
+end
+
+function contract!(
+  ::CuArrayOffset{0},
+  R,
+  labelsR,
+  ::CuArrayOffset{0},
+  T1,
+  labelsT1,
+  ::CuArrayOffset{0},
+  T2,
+  labelsT2,
+  α,
+  β,
+)
+  cuR = CuTensor(array(R), collect(labelsR))
+  cuT1 = CuTensor(array(T1), collect(labelsT1))
+  cuT2 = CuTensor(array(T2), collect(labelsT2))
   cuTENSOR.mul!(cuR, cuT1, cuT2, α, β)
-  copyto!(data(R), cuR.data)
+  return R
+end
+
+## TODO Should I always copy all of them if any are non-zero
+## Or should I create a function for every possible non-zero case?
+function contract!(
+  ::CuArrayOffset,
+  R,
+  labelsR,
+  ::CuArrayOffset,
+  T1,
+  labelsT1,
+  ::CuArrayOffset,
+  T2,
+  labelsT2,
+  α,
+  β,
+)
+  cuR = CuTensor(copy(array(R)), collect(labelsR))
+  cuT1 = CuTensor(copy(array(T1)), collect(labelsT1))
+  cuT2 = CuTensor(copy(array(T2)), collect(labelsT2))
+  cuTENSOR.mul!(cuR, cuT1, cuT2, α, β)
+  array(R) .= cuR.data
   return R
 end
