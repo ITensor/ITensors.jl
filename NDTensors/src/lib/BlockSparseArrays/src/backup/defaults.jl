7a6e342418437a0deb5a11f3b7f977b1a5fc91fa
Matt Fishman <mtfishman@users.noreply.github.com>
2024-06-13 13:28:59 -0400

    [GradedAxes] [BlockSparseArrays] Upgrade to BlockArrays.jl v1 (#1495)
    * [GradedAxes] [BlockSparseArrays] Upgrade to BlockArrays.jl v1
    
    * [NDTensors] Bump to v0.3.28

--- NDTensors/src/lib/BlockSparseArrays/src/backup/defaults.jl
+++
@@ -1,3 +0,0 @@
-using ..SparseArrayDOKs: SparseArrayDOK
-
-default_sparsearray_type() = SparseArrayDOK
--- NDTensors/src/lib/GradedAxes/src/blockedunitrange.jl
+++ NDTensors/src/lib/GradedAxes/src/blockedunitrange.jl
@@ -1,5 +1,6 @@
 using BlockArrays:
   BlockArrays,
+  AbstractBlockedUnitRange,
   Block,
   BlockIndexRange,
   BlockRange,
@@ -23,7 +24,7 @@ end
 # TODO: Rename this. `BlockArrays.findblock(a, k)` finds the
 # block of the value `k`, while this finds the block of the index `k`.
 # This could make use of the `BlockIndices` object, i.e. `block(BlockIndices(a)[index])`.
-function blockedunitrange_findblock(a::BlockedUnitRange, index::Integer)
+function blockedunitrange_findblock(a::AbstractBlockedUnitRange, index::Integer)
   @boundscheck index in 1:length(a) || throw(BoundsError(a, index))
   return @inbounds findblock(a, index + first(a) - 1)
 end
@@ -32,7 +33,7 @@ end
 # TODO: Rename this. `BlockArrays.findblockindex(a, k)` finds the
 # block index of the value `k`, while this finds the block index of the index `k`.
 # This could make use of the `BlockIndices` object, i.e. `BlockIndices(a)[index]`.
-function blockedunitrange_findblockindex(a::BlockedUnitRange, index::Integer)
+function blockedunitrange_findblockindex(a::AbstractBlockedUnitRange, index::Integer)
   @boundscheck index in 1:length(a) || throw(BoundsError())
   return @inbounds findblockindex(a, index + first(a) - 1)
 end
@@ -47,7 +48,7 @@ end
 # `blocked_getindex`. See the discussion here:
 # https://github.com/JuliaArrays/BlockArrays.jl/issues/347
 function blockedunitrange_getindices(
-  a::BlockedUnitRange, indices::AbstractUnitRange{<:Integer}
+  a::AbstractBlockedUnitRange, indices::AbstractUnitRange{<:Integer}
 )
   first_blockindex = blockedunitrange_findblockindex(a, first(indices))
   last_blockindex = blockedunitrange_findblockindex(a, last(indices))
@@ -70,46 +71,51 @@ function blockedunitrange_getindices(
 end
 
 # TODO: Move this to a `BlockArraysExtensions` library.
-function blockedunitrange_getindices(a::BlockedUnitRange, indices::BlockIndexRange)
+function blockedunitrange_getindices(a::AbstractBlockedUnitRange, indices::BlockIndexRange)
   return a[block(indices)][only(indices.indices)]
 end
 
 # TODO: Move this to a `BlockArraysExtensions` library.
-function blockedunitrange_getindices(a::BlockedUnitRange, indices::BlockSlice)
+function blockedunitrange_getindices(a::AbstractBlockedUnitRange, indices::BlockSlice)
   # TODO: Is this a good definition? It ignores `indices.indices`.
   return a[indices.block]
 end
 
 # TODO: Move this to a `BlockArraysExtensions` library.
-function blockedunitrange_getindices(a::BlockedUnitRange, indices::Vector{<:Integer})
+function blockedunitrange_getindices(
+  a::AbstractBlockedUnitRange, indices::Vector{<:Integer}
+)
   return map(index -> a[index], indices)
 end
 
-# TODO: Move this to a `BlockArraysExtensions` library.
-# TODO: Make a special definition for `BlockedVector{<:Block{1}}` in order
-# to merge blocks.
-function blockedunitrange_getindices(
-  a::BlockedUnitRange, indices::AbstractVector{<:Union{Block{1},BlockIndexRange{1}}}
-)
-  # Without converting `indices` to `Vector`,
-  # mapping `indices` outputs a `BlockVector`
-  # which is harder to reason about.
-  blocks = map(index -> a[index], Vector(indices))
-  # We pass `length.(blocks)` to `mortar` in order
-  # to pass block labels to the axes of the output,
-  # if they exist. This makes it so that
-  # `only(axes(a[indices])) isa `GradedUnitRange`
-  # if `a isa `GradedUnitRange`, for example.
-  return mortar(blocks, length.(blocks))
-end
+## # TODO: Bring back this definition and remove the one in
+## # BlockSparseArraysGradedAxesExt once
+## # https://github.com/JuliaArrays/BlockArrays.jl/pull/405 is merged.
+## # TODO: Move this to a `BlockArraysExtensions` library.
+## # TODO: Make a special definition for `BlockedVector{<:Block{1}}` in order
+## # to merge blocks.
+## function blockedunitrange_getindices(
+##   a::AbstractBlockedUnitRange, indices::AbstractVector{<:Union{Block{1},BlockIndexRange{1}}}
+## )
+##   # Without converting `indices` to `Vector`,
+##   # mapping `indices` outputs a `BlockVector`
+##   # which is harder to reason about.
+##   blocks = map(index -> a[index], Vector(indices))
+##   # We pass `length.(blocks)` to `mortar` in order
+##   # to pass block labels to the axes of the output,
+##   # if they exist. This makes it so that
+##   # `only(axes(a[indices])) isa `GradedUnitRange`
+##   # if `a isa `GradedUnitRange`, for example.
+##   return mortar(blocks, length.(blocks))
+## end
 
 # TODO: Move this to a `BlockArraysExtensions` library.
-function blockedunitrange_getindices(a::BlockedUnitRange, indices::Block{1})
+function blockedunitrange_getindices(a::AbstractBlockedUnitRange, indices::Block{1})
   return a[indices]
 end
 
 # TODO: Move this to a `BlockArraysExtensions` library.
-function blockedunitrange_getindices(a::BlockedUnitRange, indices)
+function blockedunitrange_getindices(a::AbstractBlockedUnitRange, indices)
   return error("Not implemented.")
 end
 
