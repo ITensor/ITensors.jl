721d0f00eb6d79e1d9db91a8a1b8da49d6e7276c
Karl Pierce <kmp5@vt.edu>
2024-06-13 07:39:03 -0400

    format

--- NDTensors/ext/NDTensorsGPUArraysCoreExt/permutedims.jl
+++ NDTensors/ext/NDTensorsGPUArraysCoreExt/permutedims.jl
@@ -2,8 +2,8 @@ using NDTensors: NDTensors, array
 using GPUArraysCore: @allowscalar, AbstractGPUArray
 using NDTensors.Adapt
 function NDTensors.permutedims!(
-  Rexposed::Exposed{<:AbstractGPUArray, <:DiagTensor},
-  texposed::Exposed{<:AbstractGPUArray, <:DiagTensor},
+  Rexposed::Exposed{<:AbstractGPUArray,<:DiagTensor},
+  texposed::Exposed{<:AbstractGPUArray,<:DiagTensor},
   perm::NTuple{N,Int},
   f::Function=(r, t) -> t,
 ) where {N}
