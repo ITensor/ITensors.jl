822411cb08bab3e7ce211bdc7336079c5c4a128c
Karl Pierce <kmp5@vt.edu>
2024-06-22 19:41:03 +0200

    Fix spelling

--- NDTensors/ext/NDTEnsorsJLArraysExt/set_types.jl
+++ NDTensors/ext/NDTEnsorsJLArraysExt/set_types.jl
@@ -1,10 +1,7 @@
 # TypeParameterAccessors definitions
 using NDTensors.TypeParameterAccessors: TypeParameterAccessors, Position
-using NDTensors.GPUArraysCoreExtensions: storagemode
-using JLArrays: JLArrays, JLArray
+using JLArrays: JLArray
 
 function TypeParameterAccessors.default_type_parameters(::Type{<:JLArray})
   return (Float64, 1)
 end
-TypeParameterAccessors.position(::Type{<:JLArray}, ::typeof(eltype)) = Position(1)
-TypeParameterAccessors.position(::Type{<:JLArray}, ::typeof(ndims)) = Position(2)
