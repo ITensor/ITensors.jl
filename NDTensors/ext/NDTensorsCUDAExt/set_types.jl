e213bff9efe701465c4b6542fc0527170a8e098f
Karl Pierce <kmp5@vt.edu>
2024-06-22 19:41:32 +0200

    remove unnecessary functions

--- NDTensors/ext/NDTensorsCUDAExt/set_types.jl
+++ NDTensors/ext/NDTensorsCUDAExt/set_types.jl
@@ -3,12 +3,6 @@ using CUDA: CUDA, CuArray
 using NDTensors.TypeParameterAccessors: TypeParameterAccessors, Position
 using NDTensors.GPUArraysCoreExtensions: storagemode
 
-function TypeParameterAccessors.position(::Type{<:CuArray}, ::typeof(eltype))
-  return Position(1)
-end
-function TypeParameterAccessors.position(::Type{<:CuArray}, ::typeof(ndims))
-  return Position(2)
-end
 function TypeParameterAccessors.position(::Type{<:CuArray}, ::typeof(storagemode))
   return Position(3)
 end
