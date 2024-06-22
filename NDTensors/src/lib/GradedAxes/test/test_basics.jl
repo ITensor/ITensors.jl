50b4511472ecfaf28c6c61cee83e6757ea68af25
mtfishman <mfishman@flatironinstitute.org>
2024-06-21 21:33:47 -0400

    Fix tests

--- NDTensors/src/lib/GradedAxes/test/test_basics.jl
+++ NDTensors/src/lib/GradedAxes/test/test_basics.jl
@@ -9,7 +9,6 @@ using BlockArrays:
   blocklength,
   blocklengths,
   blocks
-using NDTensors.BlockSparseArrays: BlockSparseVector
 using NDTensors.GradedAxes: GradedOneTo, GradedUnitRange, blocklabels, gradedrange
 using NDTensors.LabelledNumbers: LabelledUnitRange, islabelled, label, labelled, unlabel
 using Test: @test, @test_broken, @testset
@@ -159,14 +158,7 @@ using Test: @test, @test_broken, @testset
 
   x = gradedrange(["x" => 2, "y" => 3, "z" => 4])
   a = x[[Block(3), Block(2)]]
-  # This is a BlockSparseArray since BlockArray
-  # doesn't support axes with general integer
-  # element types. That is being fixed in:
-  # https://github.com/JuliaArrays/BlockArrays.jl/pull/405
-  # TODO: Change to `BlockVector` once we update
-  # `GradedAxes` once the axes of `BlockArray`
-  # are generalized.
-  @test a isa BlockSparseVector
+  @test a isa BlockVector
   @test length(a) == 7
   @test blocklength(a) == 2
   # TODO: `BlockArrays` doesn't define `blocklengths`
@@ -187,14 +179,7 @@ using Test: @test, @test_broken, @testset
 
   x = gradedrange(["x" => 2, "y" => 3, "z" => 4])
   a = x[[Block(3)[2:3], Block(2)[2:3]]]
-  # This is a BlockSparseArray since BlockArray
-  # doesn't support axes with general integer
-  # element types. That is being fixed in:
-  # https://github.com/JuliaArrays/BlockArrays.jl/pull/405
-  # TODO: Change to `BlockVector` once we update
-  # `GradedAxes` once the axes of `BlockArray`
-  # are generalized.
-  @test a isa BlockSparseVector
+  @test a isa BlockVector
   @test length(a) == 4
   @test blocklength(a) == 2
   # TODO: `BlockArrays` doesn't define `blocklengths`
