a1b07306f674c063180de13f0b9b5b2894407b70
mtfishman <mfishman@flatironinstitute.org>
2024-07-01 15:29:35 -0400

    Fix for Julia 1.6

--- NDTensors/src/lib/GradedAxes/src/gradedunitrange.jl
+++ NDTensors/src/lib/GradedAxes/src/gradedunitrange.jl
@@ -37,6 +37,13 @@ function Base.OrdinalRange{T,T}(a::GradedOneTo{<:LabelledInteger{T}}) where {T}
   return unlabel_blocks(a)
 end
 
+# This is only needed in certain Julia versions below 1.10
+# (for example Julia 1.6).
+# TODO: Delete this once we drop Julia 1.6 support.
+function Base.UnitRange{T}(r::GradedOneTo{<:LabelledInteger{T}}) where {T}
+  return unlabel_blocks(a)
+end
+
 # TODO: See if this is needed.
 function Base.AbstractUnitRange{T}(a::GradedOneTo{<:LabelledInteger{T}}) where {T}
   return unlabel_blocks(a)
