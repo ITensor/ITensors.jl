38092d8110a6b7f7176c1419e9338822a58df4f0
Karl Pierce <kmp5@vt.edu>
2024-09-24 16:02:43 -0400

    AbstractGPUArrays -> AbstractArray

--- NDTensors/ext/NDTensorsAMDGPUExt/set_types.jl
+++ NDTensors/ext/NDTensorsAMDGPUExt/set_types.jl
@@ -3,10 +3,9 @@ using NDTensors.TypeParameterAccessors:
   TypeParameterAccessors, Position, default_type_parameters
 using NDTensors.GPUArraysCoreExtensions: storagemode
 using AMDGPU: AMDGPU, ROCArray
-using GPUArraysCore: AbstractGPUArray
 
 function TypeParameterAccessors.default_type_parameters(::Type{<:ROCArray})
-  return (default_type_parameters(AbstractGPUArray)..., AMDGPU.Mem.HIPBuffer)
+  return (default_type_parameters(AbstractArray)..., AMDGPU.Mem.HIPBuffer)
 end
 
 TypeParameterAccessors.position(::Type{<:ROCArray}, ::typeof(storagemode)) = Position(3)
--- NDTensors/ext/NDTensorsCUDAExt/set_types.jl
+++ NDTensors/ext/NDTensorsCUDAExt/set_types.jl
@@ -3,12 +3,11 @@ using CUDA: CUDA, CuArray
 using NDTensors.TypeParameterAccessors:
   TypeParameterAccessors, Position, default_type_parameters
 using NDTensors.GPUArraysCoreExtensions: storagemode
-using GPUArraysCore: AbstractGPUArray
 
 function TypeParameterAccessors.position(::Type{<:CuArray}, ::typeof(storagemode))
   return Position(3)
 end
 
 function TypeParameterAccessors.default_type_parameters(::Type{<:CuArray})
-  return (default_type_parameters(AbstractGPUArray)..., CUDA.Mem.DeviceBuffer)
+  return (default_type_parameters(AbstractArray)..., CUDA.Mem.DeviceBuffer)
 end
