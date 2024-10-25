6020affe2ffc149eb17dd4e4f75f7d5f1d43736f
mtfishman <mfishman@flatironinstitute.org>
2024-10-24 20:23:59 -0400

    Merge conflict

--- NDTensors/test/runtests.jl
+++ NDTensors/test/runtests.jl
@@ -5,7 +5,7 @@ using SafeTestsets: @safetestset
   using NDTensors: NDTensors
   @testset "$(@__DIR__)" begin
     filenames = filter(readdir(@__DIR__)) do f
-      return startswith("test_")(f) && endswith(".jl")(f)
+      startswith("test_")(f) && endswith(".jl")(f)
     end
     for dir in ["lib"]
       push!(filenames, joinpath(dir, "runtests.jl"))
