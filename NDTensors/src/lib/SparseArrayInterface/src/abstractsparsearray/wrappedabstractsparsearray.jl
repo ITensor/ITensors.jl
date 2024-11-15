d1a5b53106c03387fc1db3b0a6614b44465e1c5c
Lukas Devos <ldevos98@gmail.com>
2024-11-15 09:32:08 -0500

    Add `sparse_storage` for `Adjoint` and `Transpose`

--- NDTensors/src/lib/SparseArrayInterface/src/abstractsparsearray/wrappedabstractsparsearray.jl
+++ NDTensors/src/lib/SparseArrayInterface/src/abstractsparsearray/wrappedabstractsparsearray.jl
@@ -13,8 +13,10 @@ function stored_indices(a::Adjoint)
   return Iterators.map(I -> CartesianIndex(reverse(Tuple(I))), stored_indices(parent(a)))
 end
 stored_length(a::Adjoint) = stored_length(parent(a))
+sparse_storage(a::Adjoint) = Iterators.map(adjoint, sparse_storage(parent(a)))
 
 function stored_indices(a::Transpose)
   return Iterators.map(I -> CartesianIndex(reverse(Tuple(I))), stored_indices(parent(a)))
 end
 stored_length(a::Transpose) = stored_length(parent(a))
+sparse_storage(a::Transpose) = Iterators.map(transpose, sparse_storage(parent(a)))
