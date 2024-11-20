bcde70def53d2526925610b446dcb13f05ffbbb7
mtfishman <mfishman@flatironinstitute.org>
2024-11-20 14:02:53 -0500

    [NDTensors] Excise libraries

--- NDTensors/src/lib/SmallVectors/src/BaseExt/insertstyle.jl
+++
@@ -1,9 +0,0 @@
-# Trait determining the style of inserting into a structure
-abstract type InsertStyle end
-struct IsInsertable <: InsertStyle end
-struct NotInsertable <: InsertStyle end
-struct FastCopy <: InsertStyle end
-
-# Assume is insertable
-@inline InsertStyle(::Type) = IsInsertable()
-@inline InsertStyle(x) = InsertStyle(typeof(x))
