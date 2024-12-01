4c8982608eb5480c7cc6e3c03e11c24d5e495d79
Zheng Zhou <goodwill.zhou@gmail.com>
2024-12-01 15:14:13 -0500

    vary the length of read QNs

--- ext/ITensorsHDF5Ext/qn.jl
+++ ext/ITensorsHDF5Ext/qn.jl
@@ -1,5 +1,5 @@
 using HDF5: HDF5, attributes, create_group, open_group, read, write
-using ITensors: maxQNs, modulus, name, QN, QNVal, val
+using ITensors: maxQNs, modulus, name, QN, QNVal, val, maxQNs_read
 
 function HDF5.write(parent::Union{HDF5.File,HDF5.Group}, gname::AbstractString, q::QN)
   g = create_group(parent, gname)
@@ -13,7 +13,7 @@ function HDF5.write(parent::Union{HDF5.File,HDF5.Group}, gname::AbstractString, 
   return write(g, "mods", mods)
 end
 
-function HDF5.read(parent::Union{HDF5.File,HDF5.Group}, name::AbstractString, ::Type{QN} ; max_qn = maxQNs)
+function HDF5.read(parent::Union{HDF5.File,HDF5.Group}, name::AbstractString, ::Type{QN})
   g = open_group(parent, name)
   if read(attributes(g)["type"]) != "QN"
     error("HDF5 group or file does not contain QN data")
@@ -21,6 +21,6 @@ function HDF5.read(parent::Union{HDF5.File,HDF5.Group}, name::AbstractString, ::
   names = read(g, "names")
   vals = read(g, "vals")
   mods = read(g, "mods")
-  mqn = ntuple(n -> QNVal(names[n], vals[n], mods[n]), max_qn)
+  mqn = ntuple(n -> QNVal(names[n], vals[n], mods[n]), length(names))
   return QN(mqn)
 end
