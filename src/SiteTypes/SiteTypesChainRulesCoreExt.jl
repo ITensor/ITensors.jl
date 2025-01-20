1001c8a407944c93a8b802e399a34f227143d79e
mtfishman <mfishman@flatironinstitute.org>
2025-01-19 22:27:44 -0500

    Delete SiteTypes, LazyApply, Ops modules

--- src/SiteTypes/SiteTypesChainRulesCoreExt.jl
+++
@@ -1,7 +0,0 @@
-module SiteTypesChainRulesCoreExt
-using ChainRulesCore: @non_differentiable
-using ..SiteTypes: SiteType, _sitetypes, has_fermion_string
-@non_differentiable has_fermion_string(::AbstractString, ::Any)
-@non_differentiable SiteType(::Any)
-@non_differentiable _sitetypes(::Any)
-end
