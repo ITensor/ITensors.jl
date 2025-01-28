afad3c0174c4610a471cb79a8aa5dc434ad45995
mtfishman <mfishman@flatironinstitute.org>
2025-01-28 16:00:27 -0500

    Remove unneeded functions

--- src/ITensors.jl
+++ src/ITensors.jl
@@ -23,7 +23,7 @@ using ITensorBase:
   unsettag
 
 # Quirks, decide where or if to define.
-using ITensorBase: OneITensor, dag, dim, factorize, hasqns, itensor, onehot, order, permute
+using ITensorBase: dag, dim, factorize, hasqns, onehot
 
 # TODO: Used in `ITensorMPS.jl`, define in `BackendSelection.jl` or `AlgorithmSelection.jl`.
 struct Algorithm{algname} end
@@ -32,40 +32,18 @@ macro Algorithm_str(algname)
 end
 
 # TODO: Used in `ITensorMPS.jl`, decide where or if to define it.
-# Maybe define in `TensorAlgebra.jl`.
-function outer end
-
-# TODO: Used in `ITensorMPS.jl`, decide where or if to define it.
-struct Apply{Args}
-  args::Args
-end
-
-# TODO: Used in `ITensorMPS.jl`, decide where or if to define it.
 # Probably define in `ITensorBase.jl` as a shorthand for
 # constructing a set of tags.
 macro ts_str(tags) end
 
-# TODO: Used in `ITensors.SiteTypes`, need to define.
-function product end
-
 # TODO: Used in `ITensorMPS.jl`, define in `ITensorBase.jl`.
-function removetags end
 function replaceprime end
-function replacetags end
 function setprime end
-function settags end
 function swapprime end
 
-# TODO: Delete these in-place versions, only define
-# them in `ITensorMPS.jl`.
-function addtags! end
-function noprime! end
-function prime! end
-function removetags! end
-function replaceprime! end
-function replacetags! end
-function setprime! end
-function settags! end
-function swapprime! end
+# TODO: Update tag functions for tagdict.
+function removetags end
+function replacetags end
+function settags end
 
 end
