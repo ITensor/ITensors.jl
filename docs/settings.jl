3bc652e6c2a24ab430970771c84ebd5f1c02728f
mtfishman <mfishman@flatironinstitute.org>
2025-03-24 11:32:43 -0400

    Fix link

--- docs/settings.jl
+++ docs/settings.jl
@@ -8,7 +8,7 @@ DocMeta.setdocmeta!(ITensors, :DocTestSetup, :(using ITensors); recursive=true)
 readme_str = read(joinpath(@__DIR__, "..", "README.md"), String)
 write(
   joinpath(@__DIR__, "src", "index.md"),
-  replace(readme_str, "docs/src/assets/CCQ.png" => "assets/CCQ.png"),
+  replace(readme_str, "docs/src/assets/CCQ.png" => "src/assets/CCQ.png"),
 )
 
 sitename = "ITensors.jl"
