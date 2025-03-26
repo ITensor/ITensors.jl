faa79969b918cb578224b54090ca5c4a83dad8ee
Matt Fishman <mtfishman@users.noreply.github.com>
2025-03-26 16:11:51 -0400

    [Docs] Update logos (#1638)

--- docs/settings.jl
+++ docs/settings.jl
@@ -5,10 +5,16 @@ using ITensors
 # https://github.com/JuliaDocs/Documenter.jl/issues/1734
 DocMeta.setdocmeta!(ITensors, :DocTestSetup, :(using ITensors); recursive=true)
 
-readme_ccq_logo = """<img src="docs/src/assets/CCQ.png" width="20%" alt="Flatiron Center for Computational Quantum Physics logo.">"""
+readme_ccq_logo = """
+  <picture>
+    <source media="(prefers-color-scheme: dark)" width="20%" srcset="docs/src/assets/CCQ-dark.png">
+    <img alt="Flatiron Center for Computational Quantum Physics logo." width="20%" src="docs/src/assets/CCQ.png">
+  </picture>
+  """
 index_ccq_logo = """
   ```@raw html
-  <img src="assets/CCQ.png" width="20%" alt="Flatiron Center for Computational Quantum Physics logo.">
+  <img class="display-light-only" src="assets/CCQ.png" width="20%" alt="Flatiron Center for Computational Quantum Physics logo."/>
+  <img class="display-dark-only" src="assets/CCQ-dark.png" width="20%" alt="Flatiron Center for Computational Quantum Physics logo."/>
   ```
   """
 
@@ -46,7 +52,8 @@ settings = Dict(
       "HDF5 File Formats" => "HDF5FileFormats.md",
     ],
   ],
-  :format => Documenter.HTML(; assets=["assets/favicon.ico"], prettyurls=false),
+  :format =>
+    Documenter.HTML(; assets=["assets/favicon.ico", "assets/extras.css"], prettyurls=false),
   :doctest => true,
   :checkdocs => :none,
 )
